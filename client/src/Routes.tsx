4f1673daf9bd5812955c8cd950ee140728f5c620
sdevmarc <suanieeee@yahoo.com>
2024-04-27 18:38:04 +0800

    Update Routes.tsx to include new page components and routes

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -1,18 +1,24 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
-import Dashboard from './pages/Dashboard'
-import Alumni from './pages/Alumni'
-import GraduateStudies from './pages/StudentAttrition'
-import AttritionRate from './pages/AttritionRate'
-import ManageAlumni from './pages/AlumniTracer'
+import Dashboard from './pages/Dashboard/Dashboard'
+import Alumni from './pages/Alumni/Alumni'
+import GraduateStudies from './pages/Student/StudentAttrition'
+import AttritionRate from './pages/Student/AttritionRate'
+import ManageAlumni from './pages/Alumni/AlumniTracer'
+import EnrollRate from './pages/Student/EnrollRate'
+import SurvivalRate from './pages/Student/SurvivalRate'
 
 const App = createBrowserRouter([
     { path: '/', element: <Home /> },
     { path: '/dashboard', element: <Dashboard /> },
+
     { path: '/alumni', element: <Alumni /> },
     { path: '/alumni/alumnitracer', element: <ManageAlumni /> },
+
     { path: '/studentattrition', element: <GraduateStudies /> },
-    { path: '/studentattrition/attritionrate', element: <AttritionRate /> }
+    { path: '/studentattrition/attritionrate', element: <AttritionRate /> },
+    { path: '/studentattrition/attritionrate/enrollrate', element: <EnrollRate /> },
+    { path: '/studentattrition/attritionrate/survivalrate', element: <SurvivalRate /> },
 ])
 
 export default App
