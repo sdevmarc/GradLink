161b5d700d04c571e776b42308007ce6148795e7
sdevmarc <suanieeee@yahoo.com>
2024-04-27 19:46:30 +0800

    Refactor Alumni page component to include sidebar and navbar functionality

--- client/src/pages/Alumni/Alumni.tsx
+++ client/src/pages/Alumni/Alumni.tsx
@@ -16,40 +16,43 @@ export default function Alumni() {
 
     return (
         <>
-        <div className="flex flex-col">
-            <Navbar onSelectSidebar={(item) => toggleSidebar(item)} />
-            <div className="flex">
-                <Sidebar SidebarStatus={isSidebar} />
-                <div className={`${isSidebar ? 'w-[100%]' : 'w-[82.5%]'} h-[93vh] p-[1rem] flex flex-col gap-2`}>
-                    <h1
-                        className='font-[600]'
-                    >
-                        ALUMNI PROFILE
-                    </h1>
-                    <div className="w-full h-full flex justify-start items-start gap-5">
-                        <Link
-                            to='/alumni/alumnitracer'
-                            className="w-[20rem] h-[9rem] border border-solid rounded-lg flex flex-col justify-end items-end p-[1rem]">
+            <div className="flex flex-col">
+                <Navbar onSelectSidebar={(item) => toggleSidebar(item)} />
+                <div className="flex">
+                    <Sidebar SidebarStatus={isSidebar} />
+                    <div className={`${isSidebar ? 'w-[100%]' : 'w-[82.5%]'} h-[93vh] p-[1rem] flex flex-col gap-2`}>
+                        <div className="w-full h-[5%] flex justify-start items-center">
                             <h1
                                 className='font-[600]'
                             >
-                                Alumni Tracer
+                                ALUMNI PROFILE
                             </h1>
-                        </Link>
+                        </div>
 
-                        <Link
-                            to='/alumni/alumnitracer'
-                            className="w-[20rem] h-[9rem] border border-solid rounded-lg flex flex-col justify-end items-end p-[1rem]">
-                            <h1
-                                className='font-[600]'
-                            >
-                                Manage Alumni
-                            </h1>
-                        </Link>
+                        <div className="w-full h-[95%] flex justify-start items-start gap-5">
+                            <Link
+                                to='/alumni/alumnitracer'
+                                className="w-[20rem] h-[9rem] border border-solid rounded-lg flex flex-col justify-end items-end p-[1rem]">
+                                <h1
+                                    className='font-[600]'
+                                >
+                                    Alumni Tracer
+                                </h1>
+                            </Link>
+
+                            <Link
+                                to='/alumni/alumnitracer'
+                                className="w-[20rem] h-[9rem] border border-solid rounded-lg flex flex-col justify-end items-end p-[1rem]">
+                                <h1
+                                    className='font-[600]'
+                                >
+                                    Manage Alumni
+                                </h1>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </>
+        </>
     )
 }
