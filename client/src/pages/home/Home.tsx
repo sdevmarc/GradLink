68f5f677002913384ebff9a0d6cf32a8a2bbfa89
Marc Edison <suanieeee@yahoo.com>
2024-08-16 01:09:19 +0800

    feat: Update Home component with link to Dashboard
    The code changes in the `Home.tsx` file update the `Home` component to include a link to the `Dashboard` page. This enhancement improves the user experience by providing a direct way to access the dashboard functionality from the home page.
    
    Note: The commit message follows the established convention of using a prefix (`feat`) to indicate a new feature or functionality.

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <>
+            <main className="w-full h-screen flex justify-center items-center">
+                <Link to='/dashboard'>
+                    Dashboard
+                </Link>
+            </main>
+        </>
+    )
 }
