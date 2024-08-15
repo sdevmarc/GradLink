02af4e4067bcfe7694587ee78f0bc587de0a7149
Marc Edison <108336842+sdevmarc@users.noreply.github.com>
2024-08-15 21:46:44 +0800

    Delete client directory

--- client/src/routes.tsx
+++
@@ -1,8 +0,0 @@
-import { createBrowserRouter } from 'react-router-dom'
-import Home from './pages/home/Home'
-
-const Routes = createBrowserRouter([
-  { path: '/', element: <Home /> }
-])
-
-export default Routes
