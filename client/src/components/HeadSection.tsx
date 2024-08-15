004453b53ff1544462223e72619e0196704c46ce
Marc Edison <suanieeee@yahoo.com>
2024-08-16 01:08:14 +0800

    feat: Add HeadSection component
    The commit adds a new `HeadSection` component to the `client/src/components/HeadSection.tsx` file. This component provides a section for displaying an overview of the application. It includes a title and a description. This addition enhances the user interface by providing a clear and informative section at the top of the page.
    
    Note: The commit message follows the established convention of using a prefix (`feat`) to indicate a new feature or functionality.

---
+++ client/src/components/HeadSection.tsx
@@ -0,0 +1,14 @@
+export default function HeadSection() {
+    return (
+        <>
+            <section className="w-[98%] h-[100px] flex flex-col justify-center items-start gap-2 px-8 border-b-[0.7px] border-black/20">
+                <h1 className="text-text font-semibold text-xl">
+                    Overview
+                </h1>
+                <p className="text-text font-light text-sm">
+                    A dashboard providing analytics and insights into alumni data, student, system health and trends.
+                </p>
+            </section>
+        </>
+    )
+}
