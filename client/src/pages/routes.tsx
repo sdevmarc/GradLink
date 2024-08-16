6ebea0eef8b31196ee1d20625cde6dbfeffc61e2
sdevmarc <suanieeee@yahoo.com>
2024-08-16 17:19:06 +0800

    feat: Add Tracer component to dashboard

--- client/src/pages/routes.tsx
+++ client/src/pages/routes.tsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from "react-router-dom"
 import Home from "./home/Home"
 import Dashboard from "./dashboard/Dashboard"
+import Tracer from "./dashboard/Tracer"
 
 const Routes = createBrowserRouter([
     { path: '/', element: <Home /> },
 
     //DASHBOARD
-    { path: '/dashboard', element: <Dashboard /> }
+    { path: '/dashboard', element: <Dashboard /> },
+    { path: '/tracer', element: <Tracer /> }
 ])
 
 export default Routes
