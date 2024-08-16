e82e8b550af564382d7a21c9a847c54e688c39c4
Marc Edison <suanieeee@yahoo.com>
2024-08-17 01:09:22 +0800

    refactor: Add FunctionComponents.type.ts
    The code changes in the `FunctionComponents.type.ts` file add the `IHeadSectionChildren`, `ISidebar`, and `ISidebarNavs` interfaces. These interfaces define the structure and types for the head section children, sidebar, and sidebar navigation links respectively. This addition improves the modularity and reusability of the codebase.

---
+++ client/src/interface/FunctionComponents.type.ts
@@ -0,0 +1,13 @@
+export interface IHeadSectionChildren {
+    title: string
+    description: string
+}
+
+export interface ISidebar {
+    children: React.ReactNode
+}
+
+export interface ISidebarNavs {
+    title: string
+    link: string
+}
