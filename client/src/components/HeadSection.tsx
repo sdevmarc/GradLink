7805cca59847a65d8e28cb3105f60d1576556e3b
Marc Edison <suanieeee@yahoo.com>
2024-08-17 01:08:48 +0800

    refactor: Update HeadSection component to accept title and description props
    The code changes in the `HeadSection.tsx` file refactor the component to accept `title` and `description` props. This allows for dynamic rendering of the title and description, improving the flexibility and reusability of the component.

--- client/src/components/HeadSection.tsx
+++ client/src/components/HeadSection.tsx
@@ -1,12 +1,14 @@
-export default function HeadSection() {
+import { IHeadSectionChildren } from "@/interface/FunctionComponents.type";
+
+export default function HeadSection({ title, description }: IHeadSectionChildren) {
     return (
         <>
             <section className="w-[98%] h-[100px] flex flex-col justify-center items-start gap-2 px-8 border-b-[0.7px] border-black/20">
                 <h1 className="text-text font-semibold text-xl">
-                    Overview
+                    {title}
                 </h1>
                 <p className="text-text font-light text-sm">
-                    A dashboard providing analytics and insights into alumni data, student, system health and trends.
+                    {description}
                 </p>
             </section>
         </>
