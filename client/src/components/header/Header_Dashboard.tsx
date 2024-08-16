9dcdd3a89f13b24f5aeefa7b21282477a226b56b
sdevmarc <suanieeee@yahoo.com>
2024-08-16 16:34:45 +0800

    refactor: Adjust header height in Header_Dashboard.tsx
    The code changes in the `Header_Dashboard.tsx` file adjust the height of the header component by modifying the `h-[8dvh]` class in the `className` prop. This refactor improves the visual appearance and layout of the header, providing a more balanced and aesthetically pleasing design.
    
    Note: The commit message follows the established convention of using a prefix (`refactor`) to indicate code refactoring or optimization.

--- client/src/components/header/Header_Dashboard.tsx
+++ client/src/components/header/Header_Dashboard.tsx
@@ -4,9 +4,9 @@ import { UserAvatar } from '@/components/UserAvatar'
 export default function Header_Dashboard() {
     return (
         <>
-            <header className="sticky top-0 left-0 w-full h-[60px] bg-background border-[0.7px] border-black/20 flex justify-center items-center">
+            <header className="sticky top-0 left-0 w-full h-[8dvh] bg-background border-[0.7px] border-black/20 flex justify-center items-center">
                 <div className="w-full max-w-[90rem] h-full flex justify-between items-center px-8">
-                    <nav className="flex justify-center items-center gap-4">
+                    <nav className="flex items-center gap-4">
                         <Link to={`/dashboard`} className='text-sm text-text font-medium'>
                             LOGO
                         </Link>
@@ -32,7 +32,7 @@ export default function Header_Dashboard() {
                             Form
                         </Link>
                     </nav>
-                    <nav className="h-full flex justify-center items-center gap-4">
+                    <nav className="flex items-center gap-4">
                         <h1 className='text-text font-medium text-sm'>
                             Welcome, John Doe
                         </h1>
