139d865f08179170fd132abb77d4649babf900ab
Marc Edison <suanieeee@yahoo.com>
2024-08-17 01:09:26 +0800

    feat: Add Student component
    The code changes in the `routes.tsx` file add a new route for the Student component. This component represents a page for displaying student information. This addition improves the functionality and navigation of the application.

--- client/src/pages/routes.tsx
+++ client/src/pages/routes.tsx
@@ -2,13 +2,15 @@ import { createBrowserRouter } from "react-router-dom"
 import Home from "./home/Home"
 import Dashboard from "./dashboard/Dashboard"
 import Tracer from "./dashboard/Tracer"
+import Student from "./dashboard/Student"
 
 const Routes = createBrowserRouter([
     { path: '/', element: <Home /> },
 
     //DASHBOARD
     { path: '/dashboard', element: <Dashboard /> },
-    { path: '/tracer', element: <Tracer /> }
+    { path: '/tracer', element: <Tracer /> },
+    { path: '/student', element: <Student /> },
 ])
 
 export default Routes
