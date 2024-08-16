9e145c941e881140feee960673ca3d54d524c89a
Marc Edison <suanieeee@yahoo.com>
2024-08-17 01:09:08 +0800

    feat: Add Table component
    The code changes in the `Table.tsx` file add a new component called Table. This component represents a table with two sections: a header section and a content section. The header section is a black background container, while the content section is a rounded border container. This addition improves the modularity and reusability of the codebase.

---
+++ client/src/components/Table.tsx
@@ -0,0 +1,15 @@
+
+export default function Table() {
+    return (
+        <>
+            <div className="w-[80%] h-full p-4 flex flex-col justify-between">
+                <div className="w-full h-[calc(12%-4%)] bg-black">
+
+                </div>
+                <div className="w-full h-[calc(92%-2%)] rounded-xl border-[0.7px] border-black/10">
+
+                </div>
+            </div>
+        </>
+    )
+}
