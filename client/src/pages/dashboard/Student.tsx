5303b317ba6f4b23f7d63d57e97db5245481f617
sdevmarc <suanieeee@yahoo.com>
2024-08-17 18:15:51 +0800

    refactor: Adjust aside padding in Student page
    The code changes in the `Student.tsx` file adjust the padding of the aside element in the Student page. This change sets the top padding to 3.2rem, improving the spacing and alignment of the content. This refactor enhances the visual appearance and consistency of the page, providing a better user experience.

--- client/src/pages/dashboard/Student.tsx
+++ client/src/pages/dashboard/Student.tsx
@@ -9,7 +9,7 @@ export default function Student() {
             <div className="flex flex-col min-h-screen items-center">
                 <Header_Dashboard />
                 <div className="w-full max-w-[90rem] flex flex-col">
-                    <aside className="px-4 pb-4 pt-20">
+                    <aside className="px-4 pb-4 pt-[3.2rem]">
                         <HeadSection
                             title="RECORD OF ENROLLED STUDENTS"
                             description="Here's a list of enrolled students."
