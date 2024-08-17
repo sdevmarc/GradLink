b9a84b3e10ff4f8634f2a9400ed9691d39da979a
sdevmarc <suanieeee@yahoo.com>
2024-08-17 18:15:40 +0800

    refactor: Adjust Search component height for better visibility
    The code changes in the `Search.tsx` file adjust the height of the Search component to improve visibility. This change sets the height of the component to 2.3rem, ensuring that the input field and buttons are more visible and accessible to users. This enhancement improves the usability and user experience of the search functionality.

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import { Button } from './ui/button'
 
 export default function Search() {
     return (
         <>
-            <div className="w-full h-full flex justify-between items-center">
+            <div className="w-full h-[2.3rem] flex justify-between items-center">
                 <div className="h-full flex justify-center items-center gap-2">
                     <input type="text" placeholder='Search by keywords' className='w-[20rem] h-full rounded-md px-2 bg-input text-text text-sm placeholder:text-black/40 placeholder:font-normal placeholder:text-sm' />
                     <Button variant={`default`} className='bg-muted text-text font-normal'>
