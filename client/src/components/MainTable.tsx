f1ec07dd0395261a0656db28ebfe2abe1a42db53
sdevmarc <suanieeee@yahoo.com>
2024-08-19 21:34:29 +0800

    ```text refactor: Update MainTable component to use DataTable and fetch data asynchronously
    The code changes in the `MainTable.tsx` file refactor the MainTable component to use the DataTable component for rendering the table and fetch data asynchronously. This change improves the performance and user experience by lazy loading the data and displaying it in a more efficient way.

--- client/src/components/MainTable.tsx
+++ client/src/components/MainTable.tsx
@@ -1,103 +1,34 @@
-import Search from "./Search";
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableFooter,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
+import { columns } from "@/components/table/Columns"
+import { DataTable } from "@/components/table/DataTable"
+import { Payment } from "@/interface/Table.type"
+import { useEffect, useState } from "react"
 
-export default function MainTable() {
-    return (
-        <>
-            <div className="w-[80%] h-full p-2 flex flex-col justify-between">
-                <div className="py-2">
-                    <Search />
-                </div>
-                <div className="w-full h-[calc(100%-12%)] rounded-xl border-[0.7px] border-black/10 p-3">
-                    <TableDemo />
-                </div>
-            </div>
-        </>
-    )
+async function getData(): Promise<Payment[]> {
+    return Array.from({ length: 50 }, (_, i) => ({
+        id: `728ed52f-${i}`,
+        amount: 100,
+        status: "pending",
+        email: `m${i}@example.com`,
+    }));
 }
 
-const invoices = [
-    {
-        invoice: "INV001",
-        paymentStatus: "Paid",
-        totalAmount: "$250.00",
-        paymentMethod: "Credit Card",
-    },
-    {
-        invoice: "INV002",
-        paymentStatus: "Pending",
-        totalAmount: "$150.00",
-        paymentMethod: "PayPal",
-    },
-    {
-        invoice: "INV003",
-        paymentStatus: "Unpaid",
-        totalAmount: "$350.00",
-        paymentMethod: "Bank Transfer",
-    },
-    {
-        invoice: "INV004",
-        paymentStatus: "Paid",
-        totalAmount: "$450.00",
-        paymentMethod: "Credit Card",
-    },
-    {
-        invoice: "INV005",
-        paymentStatus: "Paid",
-        totalAmount: "$550.00",
-        paymentMethod: "PayPal",
-    },
-    {
-        invoice: "INV006",
-        paymentStatus: "Pending",
-        totalAmount: "$200.00",
-        paymentMethod: "Bank Transfer",
-    },
-    {
-        invoice: "INV007",
-        paymentStatus: "Unpaid",
-        totalAmount: "$300.00",
-        paymentMethod: "Credit Card",
-    },
-]
+export default function MainTable() {
+    const [data, setData] = useState<Payment[]>([])
 
-export function TableDemo() {
+    useEffect(() => {
+        const fetchData = async () => {
+            const mydata = await getData()
+            setData(mydata)
+        }
+
+        fetchData()
+    }, [])
+
     return (
-        <Table>
-            <TableCaption>A list of your recent invoices.</TableCaption>
-            <TableHeader>
-                <TableRow>
-                    <TableHead className="w-[100px] text-label font-normal">Invoice</TableHead>
-                    <TableHead className="w-[100px] text-label font-normal">Status</TableHead>
-                    <TableHead className="w-[100px] text-label font-normal">Method</TableHead>
-                    <TableHead className="text-right text-label">Amount</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {invoices.map((invoice) => (
-                    <TableRow key={invoice.invoice}>
-                        <TableCell className="font-medium">{invoice.invoice}</TableCell>
-                        <TableCell>{invoice.paymentStatus}</TableCell>
-                        <TableCell>{invoice.paymentMethod}</TableCell>
-                        <TableCell className="text-right">{invoice.totalAmount}</TableCell>
-                    </TableRow>
-                ))}
-            </TableBody>
-            <TableFooter>
-                <TableRow>
-                    <TableCell colSpan={3}>Total</TableCell>
-                    <TableCell className="text-right">$2,500.00</TableCell>
-                </TableRow>
-            </TableFooter>
-        </Table>
+        <>
+            <div className="w-[80%] h-full flex flex-col justify-between">
+                <DataTable columns={columns} data={data} />
+            </div>
+        </>
     )
 }
