3218f210c4dd8ecd54f32684c16a0391bd011a3c
Marc Edison <suanieeee@yahoo.com>
2024-08-20 01:26:03 +0800

    refactor: Update DataTable components to improve code cleanliness and remove unused components

---
+++ client/src/interface/index.ts
@@ -0,0 +1,11 @@
+export interface IAvatar {
+    image: string
+    initials: string
+} 
+
+export interface IFCChildren {
+    title?: string
+    link?: string
+    description?: string
+    children?: React.ReactNode
+}
