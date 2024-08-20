6ff521865b8a2d2a7a47d50e120d3ae7043a08fc
sdevmarc <suanieeee@yahoo.com>
2024-08-20 17:07:14 +0800

    ```text refactor: Update HeadSection component to use full width
    The code changes in HeadSection.tsx update the HeadSection component to use the full width of the container. This refactor improves the layout consistency and responsiveness of the component by ensuring it spans the entire width of the page.

--- client/src/components/HeadSection.tsx
+++ client/src/components/HeadSection.tsx
@@ -3,7 +3,7 @@ import { IFCChildren } from "@/interface/index";
 export default function HeadSection({ title, description }: IFCChildren) {
     return (
         <>
-            <section className="w-[98%] h-[100px] flex flex-col justify-center items-start gap-2 px-8 border-b-[0.7px] border-black/20">
+            <section className="w-full h-[100px] flex flex-col justify-center items-start gap-2 px-8 border-b-[0.7px] border-black/20">
                 <h1 className="text-text font-semibold text-xl">
                     {title}
                 </h1>
