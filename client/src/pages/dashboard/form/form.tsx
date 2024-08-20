e85e0d02685e64932ebb8fb96f3ad34c86ca4e50
sdevmarc <suanieeee@yahoo.com>
2024-08-20 19:12:07 +0800

    feat: Add Form page
    This commit adds the Form page to the project. The Form page displays a centralized list showcasing all created Google Forms for easy access and management. It includes a header section with a title and description, a sidebar for navigation, and a main table section. This addition enhances the functionality and organization of the project.

---
+++ client/src/pages/dashboard/form/form.tsx
@@ -0,0 +1,34 @@
+import FormTable from '@/components/form-table/form-table'
+import Header_Dashboard from '@/components/header-dashboard'
+import HeadSection, { SubHeadSectionDetails } from '@/components/head-section'
+import MainTable from '@/components/main-table'
+import { Sidebar, SidebarNavs } from '@/components/sidebar'
+
+export default function Form() {
+    return (
+        <>
+            <div className="flex flex-col min-h-screen items-center">
+                <Header_Dashboard />
+                <div className="w-full max-w-[90rem] flex flex-col">
+                    <aside className="px-4 pb-4 pt-[5rem]">
+                        <HeadSection>
+                            <SubHeadSectionDetails
+                                title="FORMS"
+                                description="A centralized list showcasing all created Google Forms for easy access and management."
+                            />
+                        </HeadSection>
+                    </aside>
+                    <main className="flex">
+                        <Sidebar>
+                            <SidebarNavs title="List" link="/form" />
+                            <SidebarNavs title="Trash" link="/" />
+                        </Sidebar>
+                        <MainTable>
+                            <FormTable />
+                        </MainTable>
+                    </main>
+                </div>
+            </div>
+        </>
+    )
+}
