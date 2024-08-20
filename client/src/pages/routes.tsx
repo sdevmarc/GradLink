435fdb8329073ab08de78b903c06ab1507cf1250
sdevmarc <suanieeee@yahoo.com>
2024-08-20 12:25:04 +0800

    refactor: Add Program and Alumni routes
    This commit adds the Program and Alumni routes to the Routes component in routes.tsx. The Program route is associated with the Program component, and the Alumni route is associated with the Alumni component. This addition enhances the functionality of the application by providing navigation paths for the Program and Alumni pages.

--- client/src/pages/routes.tsx
+++ client/src/pages/routes.tsx
@@ -3,6 +3,9 @@ import Home from "./home/Home"
 import Dashboard from "./dashboard/Dashboard"
 import Tracer from "./dashboard/Tracer"
 import Student from "./dashboard/Student"
+import Program from "./dashboard/Program"
+import Alumni from "./dashboard/Alumni"
+import Form from "./dashboard/Form"
 
 const Routes = createBrowserRouter([
     { path: '/', element: <Home /> },
@@ -10,7 +13,10 @@ const Routes = createBrowserRouter([
     //DASHBOARD
     { path: '/dashboard', element: <Dashboard /> },
     { path: '/tracer', element: <Tracer /> },
+    { path: '/alumni', element: <Alumni /> },
     { path: '/student', element: <Student /> },
+    { path: '/program', element: <Program /> },
+    { path: '/form', element: <Form /> },
 ])
 
 export default Routes
