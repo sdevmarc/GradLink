bffe2226a05f460258a9554e8a05c9de7f7c66f1
sdevmarc <suanieeee@yahoo.com>
2024-08-20 19:12:30 +0800

    feat: Add Student page
    This commit adds the Student page to the project. The Student page displays a table of enrolled students using a data table component. It includes a header section with a title and description, a sidebar for navigation, and a main table section. This addition enhances the functionality and organization of the project.

---
+++ client/src/pages/dashboard/student/student.tsx
@@ -0,0 +1,45 @@
+import MainTable from "@/components/main-table"
+import Header_Dashboard from "@/components/header-dashboard"
+import HeadSection, { SubHeadSectionDetails } from "@/components/head-section"
+import { Sidebar, SidebarNavs } from "@/components/sidebar"
+import { DataTable } from "@/components/data-table-components/data-table"
+import { columns } from "@/components/data-table-components/columns"
+import { useState } from "react"
+import expense from '@/components/data-table-components/data.json'
+import { Expense } from "@/components/data-table-components/schema"
+
+export default function Student() {
+    const modifiedExpense = expense.map(item => ({ id: generateId(), ...item, type: item.type as "income" | "expense" }));
+    const [data, setData] = useState<Expense[]>(modifiedExpense)
+
+    return (
+        <>
+            <div className="flex flex-col min-h-screen items-center">
+                <Header_Dashboard />
+                <div className="w-full max-w-[90rem] flex flex-col">
+                    <aside className="px-4 pb-4 pt-[5rem]">
+                        <HeadSection>
+                            <SubHeadSectionDetails
+                                title="RECORD OF ENROLLED STUDENTS"
+                                description="Here's a list of enrolled students."
+                            />
+                        </HeadSection>
+                    </aside>
+                    <main className="flex">
+                        <Sidebar>
+                            <SidebarNavs title="Table" link="/student" />
+                            <SidebarNavs title="Trash" link="/" />
+                        </Sidebar>
+                        <MainTable>
+                            <DataTable columns={columns} data={data} />
+                        </MainTable>
+                    </main>
+                </div>
+            </div>
+        </>
+    )
+}
+
+function generateId() {
+    return '1';
+}
