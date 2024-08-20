7324d3136ca0ebcd528bd0cc0d148df11bf1c4f2
sdevmarc <suanieeee@yahoo.com>
2024-08-20 19:13:56 +0800

    refactor: Update routes file to use lowercase component filenames

--- client/src/pages/routes.tsx
+++ client/src/pages/routes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom"
 import Home from "./home/Home"
-import Dashboard from "./dashboard/dashboard"
+import Dashboard from "./dashboard/dashboard/dashboard"
 import Tracer from "./dashboard/tracer/tracer"
 import Student from "./dashboard/student/student"
 import Program from "./dashboard/program/program"
@@ -8,7 +8,7 @@ import Alumni from "./dashboard/alumni/alumni"
 import PostForm from "./dashboard/form/post-form"
 import Form from "./dashboard/form/form"
 import Profile from "./dashboard/settings/profile"
-import Mail from "./dashboard/mail/Mail"
+import Mail from "./dashboard/mail/mail"
 
 const Routes = createBrowserRouter([
     { path: '/', element: <Home /> },
