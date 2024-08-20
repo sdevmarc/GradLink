6c6827e67fc03bfbd8d0cccdd4980138ae0bf422
sdevmarc <suanieeee@yahoo.com>
2024-08-20 17:06:58 +0800

    feat: Add ComboBox component for dropdown selection
    This commit adds the ComboBox component to the project. The ComboBox component is a reusable UI component that provides a dropdown selection functionality. It allows users to select an option from a list of predefined options. This addition enhances the user interface by providing an intuitive and interactive way for users to make selections.

---
+++ client/src/components/combo-box.tsx
@@ -0,0 +1,95 @@
+"use client"
+
+import * as React from "react"
+import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+} from "@/components/ui/command"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import { IFCChildren } from "@/interface"
+
+const frameworks = [
+    {
+        value: "next.js",
+        label: "Next.js",
+    },
+    {
+        value: "sveltekit",
+        label: "SvelteKit",
+    },
+    {
+        value: "nuxt.js",
+        label: "Nuxt.js",
+    },
+    {
+        value: "remix",
+        label: "Remix",
+    },
+    {
+        value: "astro",
+        label: "Astro",
+    },
+]
+
+export function ComboBox({ title }: IFCChildren) {
+    const [open, setOpen] = React.useState(false)
+    const [value, setValue] = React.useState("")
+
+    return (
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={open}
+                    className="h-8 px-2 lg:px-3 justify-between"
+                >
+                    {value
+                        ? frameworks.find((framework) => framework.value === value)?.label
+                        : title}
+                    <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+                <Command>
+                    <CommandInput placeholder={`Search for keywords...`} className="h-9" />
+                    <CommandList>
+                        <CommandEmpty>No ${title} found.</CommandEmpty>
+                        <CommandGroup>
+                            {frameworks.map((framework) => (
+                                <CommandItem
+                                    key={framework.value}
+                                    value={framework.value}
+                                    onSelect={(currentValue) => {
+                                        setValue(currentValue === value ? "" : currentValue)
+                                        setOpen(false)
+                                    }}
+                                >
+                                    {framework.label}
+                                    <CheckIcon
+                                        className={cn(
+                                            "ml-auto h-4 w-4",
+                                            value === framework.value ? "opacity-100" : "opacity-0"
+                                        )}
+                                    />
+                                </CommandItem>
+                            ))}
+                        </CommandGroup>
+                    </CommandList>
+                </Command>
+            </PopoverContent>
+        </Popover>
+    )
+}
