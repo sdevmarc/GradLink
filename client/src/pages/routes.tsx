1a31b7178aa23df6089818de4ce64b5b062d16a6
sdevmarc <suanieeee@yahoo.com>
2024-08-20 19:11:56 +0800

    refactor: Update routes file to use lowercase component filenames
    This commit updates the routes file to use lowercase component filenames. It refactors the imports for the Dashboard, Tracer, Student, Program, Alumni, Form, and Mail components to match their new filenames. This change improves the consistency and readability of the codebase.

--- client/src/pages/routes.tsx
+++ client/src/pages/routes.tsx
@@ -1,22 +1,42 @@
 import { createBrowserRouter } from "react-router-dom"
 import Home from "./home/Home"
-import Dashboard from "./dashboard/Dashboard"
-import Tracer from "./dashboard/Tracer"
-import Student from "./dashboard/Student"
-import Program from "./dashboard/Program"
-import Alumni from "./dashboard/Alumni"
-import Form from "./dashboard/Form"
+import Dashboard from "./dashboard/dashboard"
+import Tracer from "./dashboard/tracer/tracer"
+import Student from "./dashboard/student/student"
+import Program from "./dashboard/program/program"
+import Alumni from "./dashboard/alumni/alumni"
+import PostForm from "./dashboard/form/post-form"
+import Form from "./dashboard/form/form"
+import Profile from "./dashboard/settings/profile"
+import Mail from "./dashboard/mail/Mail"
 
 const Routes = createBrowserRouter([
     { path: '/', element: <Home /> },
 
     //DASHBOARD
     { path: '/dashboard', element: <Dashboard /> },
+
+    //Mail
+    { path: '/mail', element: <Mail /> },
+
+    //Tracer
     { path: '/tracer', element: <Tracer /> },
+
+    //Alumni
     { path: '/alumni', element: <Alumni /> },
+
+    //Student
     { path: '/student', element: <Student /> },
+
+    //Program
     { path: '/program', element: <Program /> },
+
+    //Form
     { path: '/form', element: <Form /> },
+    { path: '/form/create-form', element: <PostForm /> },
+
+    //Settings
+    { path: '/profile', element: <Profile /> },
 ])
 
 export default Routes
