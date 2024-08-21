941e77512bf0c364f797fc56dc9e56855a28b6fc
sdevmarc <suanieeee@yahoo.com>
2024-08-21 19:30:29 +0800

    feat: Add form configuration section to PostForm page
    This commit adds a form configuration section to the PostForm page. The section includes input fields for form name, brief description, and data entries. It also includes a button to create the form. This addition enhances the functionality and organization of the project.

--- client/src/pages/dashboard/form/post-form.tsx
+++ client/src/pages/dashboard/form/post-form.tsx
@@ -1,13 +1,17 @@
 import Header_Dashboard from '@/components/header-dashboard'
 import HeadSection, { BackHeadSection, SubHeadSectionDetails } from '@/components/head-section'
 import { Sidebar, SidebarNavs } from '@/components/sidebar'
+import { Textarea } from '@/components/ui/textarea'
+import { Input } from '@/components/ui/input'
+import { ComboBox } from '@/components/combo-box'
+import { Button } from '@/components/ui/button'
 
 export default function PostForm() {
     return (
         <>
             <div className="flex flex-col min-h-screen items-center">
                 <Header_Dashboard />
-                <div className="w-full max-w-[90rem] flex flex-col">
+                <div className="w-full max-w-[90rem] flex flex-col pb-[20rem]">
                     <aside className="px-4 pb-4 pt-[5rem]">
                         <HeadSection>
                             <BackHeadSection />
@@ -30,21 +34,50 @@ export default function PostForm() {
     )
 }
 
+const Lists = [
+    { value: 'text', label: 'Text' },
+    { value: 'combobox', label: 'Combo Box' },
+    { value: 'number', label: 'Number' },
+]
 
 const CreateForm = () => {
     return (
         <>
-            <div className="w-[80%] flex flex-col justify-between rounded-lg border">
+            <form className="w-[80%] flex flex-col justify-between rounded-lg border">
                 <div className="w-full px-4 py-3 border-b">
                     <h1 className='text-text font-semibold text-lg'>Configure Form</h1>
                 </div>
-                <div className="w-full py-2 flex flex-col gap-2">
-                    <div className="flex flex-col px-4">
-                        <h1 className='text[1rem]'>Form Name</h1>
-                        <input type="text" placeholder='eg. Alumni Graduates Survey' className='border placeholder:text-sm px-4 py-2 rounded-md' />
+                <div className="w-full py-2 flex flex-col gap-4">
+                    <div className="flex flex-col px-4 gap-1">
+                        <h1 className='text-[.9rem]'>Form Name</h1>
+                        <Input type='text' placeholder='eg. Alumni Graduates Survey' required />
+                    </div>
+                    <div className="flex flex-col px-4 gap-1">
+                        <h1 className='text-[.9rem]'>Brief Description</h1>
+                        <Textarea placeholder="Type your message here." required />
+                    </div>
+                    <div className="flex flex-col px-4 gap-1">
+                        <h1 className='text-[.9rem]'>Data Entries</h1>
+                        <div className="flex justify-between items-center">
+                            <div className="w-full flex gap-2 items-center">
+                                <Input type='text' placeholder='eg. What is the biggest country?' className='w-[70%]' />
+                                <ComboBox
+                                    title='Type'
+                                    lists={Lists}
+                                />
+                            </div>
+                            <Button variant={`outline`} type='button'>
+                                Add Row
+                            </Button>
+                        </div>
                     </div>
                 </div>
-            </div>
+                <div className="w-full flex justify-end p-5">
+                    <Button variant={`default`} type='submit'>
+                        Create Form
+                    </Button>
+                </div>
+            </form>
         </>
     )
 }
