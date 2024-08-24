f1a7343f0be8b2f345752f7bf5b6b4bc5263423c
sdevmarc <suanieeee@yahoo.com>
2024-08-24 18:57:45 +0800

    feat: Add FormsModule to AppModule imports
    This commit adds the FormsModule to the imports array in the AppModule file. The FormsModule is required for the proper functioning of the forms in the application. This addition enhances the functionality and usability of the forms module.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FormsModule } from './forms/forms.module';
 
 @Module({
-  imports: [],
+  imports: [
+    FormsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
