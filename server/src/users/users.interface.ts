841e60cb0c1d16ba5f0e08a614330450ac9cfaed
sdevmarc <suanieeee@yahoo.com>
2024-09-03 19:00:25 +0800

    feat: Update users.interface.ts with additional properties
    The users.interface.ts file was modified to include additional properties such as id, role, and isactive. This update allows for more comprehensive user data representation and supports future functionality enhancements.

--- server/src/users/users.interface.ts
+++ server/src/users/users.interface.ts
@@ -1,4 +1,14 @@
 export interface IUsers {
-    email: string
-    password: string
+    id?: string
+    email?: string
+    password?: string
+    role?: string
+    isactive?: string
+}
+
+export interface IPromiseUser {
+    success: boolean
+    message: string
+    role?: string
+    data?: IUsers[] | {}
 }
