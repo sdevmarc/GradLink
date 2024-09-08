3cc349454d2dcd233f2230abc182eaa3ebc377b8
sdevmarc <suanieeee@yahoo.com>
2024-09-08 19:09:51 +0800

    refactor: Simplify FormsSchema in forms.schema.ts

--- server/src/forms/forms.schema.ts
+++ server/src/forms/forms.schema.ts
@@ -1,16 +1,5 @@
 import mongoose from "mongoose";
 
 export const FormsSchema = new mongoose.Schema({
-    formId: {
-        type: String,
-        required: true
-    },
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    }
+    form: []
 }, { timestamps: true })
