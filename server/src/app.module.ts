6af8cb78674f97eb55568b9eb5d9a23f03a388c2
Marc Edison <suanieeee@yahoo.com>
2024-09-09 01:30:29 +0800

    Refactor app.module.ts to include JwtModule for global JWT configuration and add ProgramsModule to the imports

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,27 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FormsModule } from './forms/forms.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { StudentModule } from './student/student.module';
 import { AuditlogService } from './auditlog/auditlog.service';
 import { OtpService } from './otp/otp.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { OtpModule } from './otp/otp.module';
 import { AuditlogModule } from './auditlog/auditlog.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ProgramsModule } from './programs/programs.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true, }),
-    MongooseModule.forRoot(process.env.MONGODB_URI),
-    FormsModule,
-    UsersModule,
-    StudentModule,
-    OtpModule,
-    AuditlogModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService, AuditlogService, OtpService],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true, }),
+        JwtModule.registerAsync({
+            global: true,
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.get<string>('JWT_SECRET'),
+                signOptions: { expiresIn: '10s' },
+            }),
+        }),
+        MongooseModule.forRoot(process.env.MONGODB_URI),
+        UsersModule,
+        StudentModule,
+        OtpModule,
+        AuditlogModule,
+        ProgramsModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService, AuditlogService, OtpService],
 })
 export class AppModule { }
