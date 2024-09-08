565e42dba0b946694915c96e7b48fdb61a1dfb1c
sdevmarc <suanieeee@yahoo.com>
2024-09-08 19:09:55 +0800

    feat: Add createUser and loginUser endpoints to UsersController

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { IUsers } from './users.interface';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(
+        private readonly usersService: UsersService
+    ) { }
+
+    @Post('create-user')
+    async CreateUser(
+        @Body() { email, password }: IUsers
+    ) {
+        return await this.usersService.InsertUser({ email, password })
+    }
+
+    @Post('login-user')
+    async LoginUser(
+        @Body() { email, password }: IUsers
+    ) {
+        return await this.usersService.ReadLoginUser({ email, password })
+    }
+}
