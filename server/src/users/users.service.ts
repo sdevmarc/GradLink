2dbedf21ff4f36cd39a6aaa5c3a27e69d8680aac
sdevmarc <suanieeee@yahoo.com>
2024-09-08 19:10:03 +0800

    refactor: Update error handling in UsersService methods
    Simplify error handling in the GetRole, findAll, findOne, InsertUser, and ReadLoginUser methods of the UsersService class. Instead of returning a generic error message, throw an HttpException with a specific error message and the appropriate HTTP status code.
    
    This improves the clarity and consistency of error handling in the UsersService class.

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { IUsers, IPromiseUser } from './users.interface';
@@ -12,37 +12,67 @@ export class UsersService {
 
     async GetRole({ id }: IUsers)
         : Promise<IPromiseUser> {
-        const isuser = await this.UserModel.findById(id)
-        if (!isuser) return { success: false, message: 'User does not exist.' }
-        return { success: true, message: 'User role retrieved successfully.', role: isuser.role }
+        try {
+            const isuser = await this.UserModel.findById(id)
+            if (!isuser) return { success: false, message: 'User does not exist.' }
+            return { success: true, message: 'User role retrieved successfully.', role: isuser.role }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'User role failed to retrieved.' }, HttpStatus.BAD_REQUEST)
+        }
     }
 
     async findAll()
         : Promise<IPromiseUser> {
-        const response = await this.UserModel.find()
-        return { success: true, message: 'Users retrieved successfully.', data: response }
+        try {
+            const response = await this.UserModel.find()
+            return { success: true, message: 'Users retrieved successfully.', data: response }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Users failed to retrieved.' }, HttpStatus.BAD_REQUEST)
+        }
     }
 
     async findOne({ email }: IUsers)
         : Promise<IPromiseUser> {
-        const response = await this.UserModel.findOne({ email })
-        return { success: true, message: 'User retrieved successfully.', data: response }
+        try {
+            const response = await this.UserModel.findOne({ email })
+            return { success: true, message: 'User retrieved successfully.', data: response }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'User email failed to retrieved.' }, HttpStatus.BAD_REQUEST)
+        }
     }
 
-    async InsertUser({ email, password }: IUsers) {
-        const salt = await bcrypt.genSalt();
-        const hashedpassword = await bcrypt.hash(password, salt);
+    async InsertUser({ email, password }: IUsers)
+        : Promise<IPromiseUser> {
+        try {
+            const isemail = await this.UserModel.findOne({ email })
+            if (isemail) return { success: false, message: 'User email already exists.' }
 
-        await this.UserModel.create({ email, password: hashedpassword })
+            let role: string
+            const hasusers = await this.UserModel.find()
+            hasusers.length <= 0 ? role = 'admin' : role = 'user'
+
+            const salt = await bcrypt.genSalt();
+            const hashedpassword = await bcrypt.hash(password, salt);
+
+            await this.UserModel.create({ email, password: hashedpassword, role })
+            return { success: true, message: 'User successfully created.' }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'User failed to create.' }, HttpStatus.BAD_REQUEST)
+        }
     }
 
     async ReadLoginUser({ email, password }: IUsers)
         : Promise<IPromiseUser> {
-        const isemail = await this.UserModel.findOne({ email })
+        try {
+            const isemail = await this.UserModel.findOne({ email })
 
-        if (!isemail) return { success: false, message: 'User does not exist.' }
+            if (!isemail) return { success: false, message: 'User email does not exist.' }
+            const ispassword = await bcrypt.compare(password, isemail.password);
 
-        const ispassword = await bcrypt.compare(password, isemail.password);
-        if (ispassword) return { success: true, message: 'Logged in successful.' }
+            if (!ispassword) return { success: false, message: 'Password is incorrect.' }
+            return { success: true, message: 'Logged in successful.', }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'User failed to login.' }, HttpStatus.BAD_REQUEST)
+        }
     }
 }
