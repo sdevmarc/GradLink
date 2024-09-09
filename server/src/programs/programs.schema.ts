4bbaa32afb55c22d13f5d849d5943c9ad06e4b5b
sdevmarc <suanieeee@yahoo.com>
2024-09-09 16:33:13 +0800

    Refactor ProgramSchema in programs.schema.ts

--- server/src/programs/programs.schema.ts
+++ server/src/programs/programs.schema.ts
@@ -1,5 +1,27 @@
 import mongoose from "mongoose";
 
 export const ProgramSchema = new mongoose.Schema({
-    program: []
+    code: {
+        type: String,
+        required: true
+    },
+    descriptiveTitle: {
+        type: String,
+        required: true
+    },
+    units: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    enrolled: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    residency: {
+        type: Number,
+        required: true,
+        default: 0
+    }
 }, { timestamps: true })
