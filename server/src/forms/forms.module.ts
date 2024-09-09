f496c5ea840f2d69099d9ec8ce055ec91f1e20b0
sdevmarc <suanieeee@yahoo.com>
2024-09-09 19:53:00 +0800

    Add FormsModule to server/src/forms/forms.module.ts

---
+++ server/src/forms/forms.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { FormsService } from './forms.service';
+import { FormsController } from './forms.controller';
+
+@Module({
+    providers: [
+        FormsService
+    ],
+    controllers: [FormsController]
+})
+export class FormsModule { }
