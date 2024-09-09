feeb3c7a9f8f113397daa90eca4733c7910c7cfc
sdevmarc <suanieeee@yahoo.com>
2024-09-09 16:33:06 +0800

    Add MongooseModule for ProgramSchema in ProgramsModule

--- server/src/programs/programs.module.ts
+++ server/src/programs/programs.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProgramsService } from './programs.service';
 import { ProgramsController } from './programs.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProgramSchema } from './programs.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([
+    { name: 'Program', schema: ProgramSchema }
+  ])],
   providers: [ProgramsService],
   controllers: [ProgramsController]
 })
-export class ProgramsModule {}
+export class ProgramsModule { }
