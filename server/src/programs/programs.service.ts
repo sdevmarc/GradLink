80527402c2f8871f9b75c18baf98c82ec64ae60d
sdevmarc <suanieeee@yahoo.com>
2024-09-10 20:54:50 +0800

    Refactor ProgramsService to include CRUD endpoints for programs

--- server/src/programs/programs.service.ts
+++ server/src/programs/programs.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { IPrograms, IPromisePrograms } from './programs.interface';
 
 @Injectable()
-export class ProgramsService {}
+export class ProgramsService {
+    constructor(
+        @InjectModel('Program') private readonly ProgramModel: Model<IPrograms>
+    ) { }
+
+    async findAll(): Promise<IPromisePrograms> {
+        try {
+            const response = await this.ProgramModel.find()
+            return { success: true, message: 'Programs fetched successfully.', data: response }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Failed to fetch all program.', error }, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    async findOne({ code }: IPrograms): Promise<IPromisePrograms> {
+        try {
+            const response = await this.ProgramModel.findOne({ code })
+            return { success: true, message: 'Program fetched successfully', data: response }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Failed to fetch all program.', error }, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    async upsert({ code, descriptiveTitle, units, enrolled, residency }: IPrograms)
+        : Promise<IPromisePrograms> {
+        try {
+            await this.ProgramModel.findOneAndUpdate(
+                { code },
+                { code, descriptiveTitle, units, enrolled, residency },
+                { new: true, upsert: true }
+            )
+            return { success: true, message: 'Program successfully upserted' }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Failed to upsert a program.', error }, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    async delete({ pid }: IPrograms)
+        : Promise<IPromisePrograms> {
+        try {
+            await this.ProgramModel.findByIdAndDelete(pid)
+            return { success: true, message: 'Program deleted successfully.' }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Failed to delete a program.', error }, HttpStatus.BAD_REQUEST)
+        }
+    }
+}
