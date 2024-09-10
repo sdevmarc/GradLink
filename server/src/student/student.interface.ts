2413d57103cea10204c7f9bd52e1b0d6d2f883f0
sdevmarc <suanieeee@yahoo.com>
2024-09-10 17:11:24 +0800

    Refactor student interface to include additional properties and interfaces

--- server/src/student/student.interface.ts
+++ server/src/student/student.interface.ts
@@ -1,1 +1,36 @@
-export interface Student {}
+export interface IStudentGeneralInformation {
+
+}
+
+export interface IStudentEducationalBackground {
+    
+}
+
+export interface IStudentTrainingAndAdvanceStudies {
+    
+}
+
+export interface IStudentCourse {
+    courseno: string
+    units: number
+}
+
+export interface IStudentPrograms {
+    code: {}
+    course: IStudentCourse[]
+}
+
+export interface IStudent {
+    sid?: string
+    generalInformation: {}
+    educationalBackground: {}
+    trainingAdvanceStudies: {}
+    programs: IStudentPrograms[]
+    status: string
+    progress: string
+}
+
+export interface IPromiseStudent {
+    success: boolean
+    message: string
+}
