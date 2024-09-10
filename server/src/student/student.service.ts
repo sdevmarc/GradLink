c451f21e984ad574084bcdec93114a197c1e07ca
sdevmarc <suanieeee@yahoo.com>
2024-09-10 17:11:28 +0800

    Refactor StudentService to include UpsertStudent endpoint

--- server/src/student/student.service.ts
+++ server/src/student/student.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { IPromiseStudent, IStudent } from './student.interface';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+    constructor(
+        @InjectModel('Student') private readonly studentModel: Model<IStudent>
+    ) { }
+
+    async UpsertStudent(
+        { sid, generalInformation, educationalBackground, trainingAdvanceStudies, programs, status, progress }: IStudent
+    )
+        : Promise<IPromiseStudent> {
+        try {
+            await this.studentModel.findOneAndUpdate(
+                { sid },
+                {
+                    sid,
+                    generalInformation,
+                    educationalBackground,
+                    trainingAdvanceStudies,
+                    programs,
+                    status,
+                    progress
+                },
+                { new: true, upsert: true }
+            )
+            return { success: true, message: 'Student successfully updated' }
+        } catch (error) {
+            throw new HttpException({ success: false, message: error }, HttpStatus.BAD_REQUEST)
+        }
+    }
+}
