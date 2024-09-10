d7dbf3dd32c75e43f05b0043058621f1157d4ca2
sdevmarc <suanieeee@yahoo.com>
2024-09-10 17:11:20 +0800

    Refactor StudentController to include UpsertStudent endpoint

--- server/src/student/student.controller.ts
+++ server/src/student/student.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { StudentService } from './student.service';
+import { IStudent } from './student.interface';
 
 @Controller('student')
-export class StudentController {}
+export class StudentController {
+    constructor(
+        private readonly studentService: StudentService
+    ) { }
+
+    @Post('upsert')
+    async UpsertStudent(
+        @Body() { sid, generalInformation, educationalBackground, trainingAdvanceStudies, programs, status, progress }: IStudent
+    ) {
+        return this.studentService.UpsertStudent(
+            {
+                sid,
+                generalInformation,
+                educationalBackground,
+                trainingAdvanceStudies,
+                programs,
+                status,
+                progress
+            }
+        )
+    }
+}
