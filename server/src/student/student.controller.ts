6e2faa8e0407fad2f7b6a57adf9ae9d5dfd8fbac
sdevmarc <suanieeee@yahoo.com>
2024-09-10 19:20:56 +0800

    Refactor student controller to include create-student endpoint and use FormsService

--- server/src/student/student.controller.ts
+++ server/src/student/student.controller.ts
@@ -1,26 +1,37 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { IStudent } from './student.interface';
+import { FormsService } from 'src/forms/forms.service';
 
 @Controller('student')
 export class StudentController {
     constructor(
-        private readonly studentService: StudentService
+        private readonly studentService: StudentService,
+        private readonly formsService: FormsService
     ) { }
+
+    @Post('create-student')
+    async InsertStudent(
+        @Body() { sid, programs }: IStudent
+    ) {
+        return this.studentService.UpsertStudent(
+            {
+                sid,
+                programs,
+            }
+        )
+    }
 
     @Post('upsert')
     async UpsertStudent(
-        @Body() { sid, generalInformation, educationalBackground, trainingAdvanceStudies, programs, status, progress }: IStudent
+        @Body() { sid, generalInformation, educationalBackground, trainingAdvanceStudies }: IStudent
     ) {
         return this.studentService.UpsertStudent(
             {
                 sid,
                 generalInformation,
                 educationalBackground,
-                trainingAdvanceStudies,
-                programs,
-                status,
-                progress
+                trainingAdvanceStudies
             }
         )
     }
