5ef4f96b97d363367d29532ba7f1131f46514486
sdevmarc <suanieeee@yahoo.com>
2024-09-10 20:54:46 +0800

    Refactor programs interface to include additional fields and types

--- server/src/programs/programs.interface.ts
+++ server/src/programs/programs.interface.ts
@@ -1,1 +1,16 @@
-export interface IPrograms {}
+import mongoose from "mongoose"
+
+export interface IPrograms {
+    pid?: mongoose.Schema.Types.ObjectId
+    code?: string
+    descriptiveTitle?: string
+    units?: number
+    enrolled?: number
+    residency?: number
+}
+
+export interface IPromisePrograms {
+    success: boolean
+    message: string
+    data?: IPrograms[] | {}
+}
