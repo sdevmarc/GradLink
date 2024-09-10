0f487a4833bff046bbf9f319ddcf9d42d6ef4e5f
sdevmarc <suanieeee@yahoo.com>
2024-09-10 20:54:41 +0800

    Refactor programs controller to include CRUD endpoints for programs

--- server/src/programs/programs.controller.ts
+++ server/src/programs/programs.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ProgramsService } from './programs.service';
+import { IPrograms } from './programs.interface';
 
 @Controller('programs')
-export class ProgramsController {}
+export class ProgramsController {
+    constructor(
+        private readonly programService: ProgramsService
+    ) { }
+
+    @Get()
+    async findAllPrograms() {
+        return await this.programService.findAll()
+    }
+
+    @Get(':code')
+    async findOneProgram(@Param() { code }: IPrograms) {
+        return await this.programService.findOne({ code })
+    }
+
+    @Post('upsert')
+    async upsertProgram(
+        @Body() { code, descriptiveTitle, units, enrolled, residency }: IPrograms
+    ) {
+        return await this.programService.upsert({ code, descriptiveTitle, units, enrolled, residency })
+    }
+
+    @Post(':pid')
+    async deleteProgram(@Param() { pid }: IPrograms) {
+        return this.programService.delete({ pid })
+    }
+}
