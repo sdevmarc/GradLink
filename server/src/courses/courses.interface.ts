76e26d678740a2b2df9386d54066158e63d4315c
sdevmarc <suanieeee@yahoo.com>
2024-09-11 15:25:01 +0800

    Refactor courses interface to include additional fields and types

--- server/src/courses/courses.interface.ts
+++ server/src/courses/courses.interface.ts
@@ -1,1 +1,13 @@
-export interface Courses {}
+export interface ICourses {
+    cid?: string
+    courseno?: string
+    descriptiveTitle?: string
+    degree?: string
+    units?: number
+}
+
+export interface IPromiseCourse {
+    success: boolean
+    message: string
+    data?: ICourses[] | {}
+}
