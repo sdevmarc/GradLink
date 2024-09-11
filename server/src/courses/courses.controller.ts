d3147b239e0f3940baf7b0996bc6cb3fe58b5de0
sdevmarc <suanieeee@yahoo.com>
2024-09-11 15:24:55 +0800

    Refactor courses controller to include CRUD endpoints for courses

--- server/src/courses/courses.controller.ts
+++ server/src/courses/courses.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CoursesService } from './courses.service';
+import { ICourses } from './courses.interface';
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+    constructor(
+        private readonly courseService: CoursesService
+    ) { }
+
+    @Get()
+    async findAllCourses() {
+        return this.courseService.findAll()
+    }
+
+    @Get(':courseno')
+    async findOneCourse(@Param() { courseno }: ICourses) {
+        return this.courseService.findOne({ courseno })
+    }
+
+    @Post('upsert-course')
+    async upsertCourse(@Body() { courseno, descriptiveTitle, degree, units }: ICourses) {
+        return this.courseService.upsert({ courseno, descriptiveTitle, degree, units })
+    }
+
+    @Post(':cid')
+    async deleteCourse(@Param() { cid }: ICourses) {
+        return this.courseService.delete({ cid })
+    }
+}
