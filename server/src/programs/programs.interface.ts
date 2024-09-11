d304b463da6dfc75c857b45f1bff22e5dbd4089d
sdevmarc <suanieeee@yahoo.com>
2024-09-11 15:25:23 +0800

    Refactor programs interface to remove 'units' and 'enrolled' fields

--- server/src/programs/programs.interface.ts
+++ server/src/programs/programs.interface.ts
@@ -4,8 +4,6 @@ export interface IPrograms {
     pid?: mongoose.Schema.Types.ObjectId
     code?: string
     descriptiveTitle?: string
-    units?: number
-    enrolled?: number
     residency?: number
 }
 
