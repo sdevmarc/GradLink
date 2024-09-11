5926ee00cadbea82eb47d8ee07fbfd7b09221430
sdevmarc <suanieeee@yahoo.com>
2024-09-11 22:10:27 +0800

    Refactor routes.tsx to update Alumni component import path

--- client/src/pages/routes.tsx
+++ client/src/pages/routes.tsx
@@ -4,11 +4,11 @@ import Dashboard from "./dashboard/dashboard/dashboard"
 import Tracer from "./dashboard/tracer/tracer"
 import Student from "./dashboard/student/student"
 import Program from "./dashboard/program/program"
-import Alumni from "./dashboard/alumni/alumni"
 import PostForm from "./dashboard/form/post-form"
 import Form from "./dashboard/form/form"
 import Profile from "./dashboard/settings/profile"
 import Courses from "./dashboard/program/courses"
+import Alumni from "./dashboard/student/alumni"
 
 const Routes = createBrowserRouter([
     { path: '/', element: <Home /> },
@@ -19,11 +19,9 @@ const Routes = createBrowserRouter([
     //Tracer
     { path: '/tracer', element: <Tracer /> },
 
-    //Alumni
-    { path: '/alumni', element: <Alumni /> },
-
     //Student
     { path: '/student', element: <Student /> },
+    { path: '/student/alumni', element: <Alumni /> },
 
     //Program
     { path: '/program', element: <Program /> },
