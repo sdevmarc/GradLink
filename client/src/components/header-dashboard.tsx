843fff254aa93fe7f89c25da0969b8c76c21ab2f
sdevmarc <suanieeee@yahoo.com>
2024-09-11 20:08:20 +0800

    Refactor header-dashboard component to add Semester link

--- client/src/components/header-dashboard.tsx
+++ client/src/components/header-dashboard.tsx
@@ -26,6 +26,9 @@ export default function Header_Dashboard() {
                         <NavLink to={`/program`} className='text-[.8rem] text-text font-normal rounded-full px-3 py-1'>
                             Program
                         </NavLink>
+                        <NavLink to={`/semester`} className='text-[.8rem] text-text font-normal rounded-full px-3 py-1'>
+                            Semester
+                        </NavLink>
                         <NavLink to={`/form`} className='text-[.8rem] text-text font-normal rounded-full px-3 py-1'>
                             Form
                         </NavLink>
