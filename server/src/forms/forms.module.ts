d34844054fa18079771ca12e502f2c2a401f9e7f
sdevmarc <suanieeee@yahoo.com>
2024-09-12 13:22:44 +0800

    Refactor forms module to remove unused dependencies

--- server/src/forms/forms.module.ts
+++ server/src/forms/forms.module.ts
@@ -2,19 +2,14 @@ import { Module } from '@nestjs/common';
 import { FormsService } from './forms.service';
 import { FormsController } from './forms.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { StudentSchema } from 'src/student/student.schema';
-import { StudentService } from 'src/student/student.service';
 import { FormSchema } from './forms.schema';
 
 @Module({
     imports: [MongooseModule.forFeature([
-        { name: 'Student', schema: StudentSchema },
         { name: 'Form', schema: FormSchema },
     ])],
     providers: [
-        FormsService,
-        StudentService
-    ],
+        FormsService,],
     controllers: [FormsController]
 })
 export class FormsModule { }
