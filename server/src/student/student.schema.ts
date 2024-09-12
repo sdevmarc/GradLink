fccca386e1120ac3d9457b6f516917d41a94c3c4
sdevmarc <suanieeee@yahoo.com>
2024-09-12 19:55:47 +0800

    Refactor student schema to include enrollments field

--- server/src/student/student.schema.ts
+++ server/src/student/student.schema.ts
@@ -8,29 +8,37 @@ export const StudentSchema = new mongoose.Schema({
     generalInformation: {},
     educationalBackground: {},
     trainingAdvanceStudies: {},
-    // programs: [{
-    //     code: {
-    //         type: String,
-    //         required: true
-    //     },
-    //     course: [{
-    //         courseno: {
-    //             type: String,
-    //            ref: 'Course',
-    //             required: true
-    //         }
-    //     }]
-    // }],
+    enrollments: [{
+        semester: {
+            type: Number,
+            enum: [1, 2, 3],
+            required: true,
+        },
+        progress: {
+            type: String,
+            enum: ['ongoing', 'done', 'dropout'],
+            required: true,
+            default: 'ongoing'
+        },
+        academic_year: {
+            type: String,
+            required: true
+        },
+        courses: [{
+            courseno: {
+                type: String,
+                ref: 'Course',
+                required: true
+            }
+        }]
+    }],
     status: {
         type: String,
         required: true,
         enum: ['student', 'alumni'],
         default: 'student'
     },
-    progress: {
-        type: String,
-        required: true,
-        enum: ['ongoing', 'done', 'dropout'],
-        default: 'ongoing'
+    graduation_date: {
+        type: String
     }
 }, { timestamps: true })
