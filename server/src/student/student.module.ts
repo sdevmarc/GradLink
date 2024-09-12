3bdb11c6db855581971de95f46150064dae0d7d8
sdevmarc <suanieeee@yahoo.com>
2024-09-12 19:55:42 +0800

    Refactor student module to include FormSchema in imports

--- server/src/student/student.module.ts
+++ server/src/student/student.module.ts
@@ -4,11 +4,13 @@ import { StudentController } from './student.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { StudentSchema } from './student.schema';
 import { FormsService } from 'src/forms/forms.service';
+import { FormSchema } from 'src/forms/forms.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: 'Student', schema: StudentSchema },
+      { name: 'Form', schema: FormSchema }
     ])
   ],
   providers: [StudentService, FormsService],
