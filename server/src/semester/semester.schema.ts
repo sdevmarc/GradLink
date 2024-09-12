f55bcc87e6a0ca58ef0deb1ab17ad097d146d389
sdevmarc <suanieeee@yahoo.com>
2024-09-12 19:55:19 +0800

    Refactor semester interface to include academic year field

--- server/src/semester/semester.schema.ts
+++ server/src/semester/semester.schema.ts
@@ -6,23 +6,8 @@ export const SemesterSchema = new mongoose.Schema({
         enum: [1, 2, 3],
         required: true
     },
-    programs: [{
-        sid: {
-            type: String,
-            ref: 'Student',
-            required: true
-        },
-        code: {
-            type: String,
-            ref: 'Program',
-            required: true
-        },
-        courses: [{
-            courseno: {
-                type: String,
-                ref: 'Course',
-                required: true
-            }
-        }]
-    }]
+    academic_year: {
+        type: String,
+        required: true
+    }
 }, { timestamps: true })
