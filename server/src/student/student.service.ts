6d1e04bfb75603984afc207743aa61d285277e7b
sdevmarc <suanieeee@yahoo.com>
2024-09-12 13:23:14 +0800

    Refactor student service to add CRUD operations for students

--- server/src/student/student.service.ts
+++ server/src/student/student.service.ts
@@ -9,26 +9,82 @@ export class StudentService {
         @InjectModel('Student') private readonly studentModel: Model<IStudent>
     ) { }
 
-    async UpsertStudent(
-        { sid, generalInformation, educationalBackground, trainingAdvanceStudies, status, progress }: IStudent
+    async findAll()
+        : Promise<IPromiseStudent> {
+        try {
+            const response = await this.studentModel.find()
+            return { success: true, message: 'Students sucessfully fetched', data: response }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Students failed to fetch.', error }, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    async findOne()
+        : Promise<IPromiseStudent> {
+        try {
+            const response = await this.studentModel.findOne()
+            return { success: true, message: 'Student successfully ', data: response }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Student failed to fetch.', error }, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    async create(
+        { idNumber, status, progress }: IStudent
+    ): Promise<IPromiseStudent> {
+        try {
+            await this.studentModel.create({ idNumber, status, progress })
+            return { success: true, message: 'Student successfully created.' }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Failed to create student.', error }, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    async findByIdAndUpdate(
+        { idNumber, status, progress }: IStudent
     )
         : Promise<IPromiseStudent> {
         try {
+            const isstudent = await this.studentModel.findOne({ idNumber })
+            if (!isstudent) return { success: false, message: 'Student do not exist.' }
+
             await this.studentModel.findOneAndUpdate(
-                { sid },
+                { idNumber },
                 {
-                    sid,
-                    generalInformation,
-                    educationalBackground,
-                    trainingAdvanceStudies,
+                    idNumber,
                     status,
                     progress
                 },
-                { new: true, upsert: true }
+                { new: true }
             )
             return { success: true, message: 'Student successfully updated' }
         } catch (error) {
-            throw new HttpException({ success: false, message: error }, HttpStatus.BAD_REQUEST)
+            throw new HttpException({ success: false, message: 'Failed to update student information', error }, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    async formUpdateStudent(
+        { idNumber, generalInformation, educationalBackground, trainingAdvanceStudies }: IStudent
+    )
+        : Promise<IPromiseStudent> {
+        try {
+            const isstudent = await this.studentModel.findOne({ idNumber })
+            if (!isstudent) return { success: false, message: 'Student do not exist.' }
+
+            await this.studentModel.findOneAndUpdate(
+                { idNumber },
+                {
+                    generalInformation,
+                    educationalBackground,
+                    trainingAdvanceStudies,
+                    status: 'alumni',
+                    progress: 'done'
+                },
+                { new: true }
+            )
+            return { success: true, message: 'Student successfully updated' }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Failed to update student graduate', error }, HttpStatus.BAD_REQUEST)
         }
     }
 }
