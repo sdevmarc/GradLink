18655c9987008130f3e426cb4e14e4ca529b8cae
sdevmarc <suanieeee@yahoo.com>
2024-09-16 12:47:14 +0800

    Refactor DataTable to include FormColumns component

---
+++ client/src/components/data-table-components/columns/student-columns.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { Expense } from "@/components/data-table-components/schema";
+import { DataTableRowActions } from "@/components/data-table-components/data-table-row-actions";
+import { DataTableColumnHeader } from "../data-table-column-header";
+
+export const StudentColumns: ColumnDef<Expense>[] = [
+    {
+        accessorKey: "idNumber",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="ID" className="text-text" />
+        ),
+        cell: ({ row }) => (
+            <div className="w-[2rem] capitalize">{row.getValue("idNumber")}</div>
+        ),
+        enableSorting: false,
+        enableHiding: false
+    },
+    {
+        accessorKey: "name",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Full Name" className="text-text" />
+        ),
+        cell: ({ row }) => {
+            return (
+                <div className="flex space-x-2">
+                    <span className="max-w-[500px] truncate capitalize font-medium">
+                        {row.getValue("name")}
+                    </span>
+                </div>
+            );
+        }
+    },
+    {
+        accessorKey: "email",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Email Address" className="text-text" />
+        ),
+        cell: ({ row }) => {
+            return (
+                <div className="flex w-[100px] items-center">
+                    <span> {row.getValue("email")}</span>
+                </div>
+            );
+        },
+        filterFn: (row, id, value) => {
+            return value.includes(row.getValue(id));
+        }
+    },
+    {
+        accessorKey: "status",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Status" className="text-text" />
+        ),
+        cell: ({ row }) => {
+            return (
+                <div className="flex w-[100px] items-center">
+                    <span className="capitalize"> {row.getValue("status")}</span>
+                </div>
+            );
+        },
+        filterFn: (row, id, value) => {
+            return value.includes(row.getValue(id));
+        }
+    },
+    {
+        accessorKey: "status",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Progress" className="text-text" />
+        ),
+        cell: ({ row }) => {
+            return (
+                <div className="flex w-[100px] items-center">
+                    <span className="capitalize">No backend yet</span>
+                </div>
+            );
+        },
+        filterFn: (row, id, value) => {
+            return value.includes(row.getValue(id));
+        }
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => <DataTableRowActions row={row} />
+    }
+];
