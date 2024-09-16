e673902efe40b2acea8f4a2bbb41b03c7560f7b2
sdevmarc <suanieeee@yahoo.com>
2024-09-16 12:47:09 +0800

    Refactor DataTable to include FormColumns component

---
+++ client/src/components/data-table-components/columns/form-columns.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { Expense } from "@/components/data-table-components/schema";
+import { DataTableRowActions } from "@/components/data-table-components/data-table-row-actions";
+import { DataTableColumnHeader } from "../data-table-column-header";
+
+export const FormColumns: ColumnDef<Expense>[] = [
+    {
+        accessorKey: "courseno",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Course No." className="text-text" />
+        ),
+        cell: ({ row }) => (
+            <div className="w-[2rem] capitalize">{row.getValue("courseno")}</div>
+        ),
+        enableSorting: false,
+        enableHiding: false
+    },
+    {
+        accessorKey: "descriptiveTitle",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Descriptive Title" className="text-text" />
+        ),
+        cell: ({ row }) => {
+            return (
+                <div className="flex space-x-2">
+                    <span className="max-w-[200px] truncate capitalize font-medium">
+                        {row.getValue("descriptiveTitle")}
+                    </span>
+                </div>
+            );
+        }
+    },
+    {
+        accessorKey: "degree",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Degree" className="text-text" />
+        ),
+        cell: ({ row }) => {
+            return (
+                <div className="flex space-x-2">
+                    <span className="max-w-[60px] truncate capitalize font-medium">
+                        {row.getValue("degree")}
+                    </span>
+                </div>
+            );
+        }
+    },
+    {
+        accessorKey: "units",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Units" className="text-text" />
+        ),
+        cell: ({ row }) => {
+            return (
+                <div className="flex w-[60px] items-center">
+                    <span className="capitalize"> {row.getValue("units")}</span>
+                </div>
+            );
+        },
+        filterFn: (row, id, value) => {
+            return value.includes(row.getValue(id));
+        }
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => <DataTableRowActions row={row} />
+    }
+];
