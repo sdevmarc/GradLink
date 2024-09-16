a22f69c0fa139a73fb27c5be37d09ad80cce5bf0
sdevmarc <suanieeee@yahoo.com>
2024-09-16 12:47:32 +0800

    Refactor Form component to use DataTable and ProgramColumns

--- client/src/pages/dashboard/form/form.tsx
+++ client/src/pages/dashboard/form/form.tsx
@@ -1,19 +1,15 @@
-import FormTable from '@/components/form-table/form-table'
 import Header_Dashboard from '@/components/header-dashboard'
 import HeadSection, { SubHeadSectionDetails } from '@/components/head-section'
 import MainTable from '@/components/main-table'
 import { Sidebar, SidebarNavs } from '@/components/sidebar'
+import { DataTable } from '@/components/data-table-components/data-table'
+import { ProgramColumns } from '@/components/data-table-components/columns/program-columns'
 
 export default function Form() {
     return (
         <>
             <div className="flex flex-col min-h-screen items-center">
                 <Header_Dashboard />
-                <div className="w-full h-screen fixed top-0 bg-black/40 flex justify-center items-center">
-                    <h1 className='text-white font-semibold text-[7rem]'>
-                        IN RENOVATION
-                    </h1>
-                </div>
                 <div className="w-full max-w-[90rem] flex flex-col">
                     <aside className="px-4 pb-4 pt-[5rem]">
                         <HeadSection>
@@ -25,11 +21,12 @@ export default function Form() {
                     </aside>
                     <main className="flex">
                         <Sidebar>
-                            <SidebarNavs title="List" link="/form" />
+                            <SidebarNavs title="Mail" link="/form" />
+                            <SidebarNavs title="Pending Forms" link="/form" />
                             <SidebarNavs title="Trash" link="/" />
                         </Sidebar>
                         <MainTable>
-                            <FormTable />
+                            <DataTable columns={ProgramColumns} data={program.data || []} toolbar='student' />
                         </MainTable>
                     </main>
                 </div>
