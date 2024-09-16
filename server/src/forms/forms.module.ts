44759dfb65a9686c0f58cd48f7e36c0e41cc78a1
sdevmarc <suanieeee@yahoo.com>
2024-09-16 12:48:16 +0800

    Refactor FormsController to use ConstantsService for form ID

--- server/src/forms/forms.module.ts
+++ server/src/forms/forms.module.ts
@@ -3,13 +3,16 @@ import { FormsService } from './forms.service';
 import { FormsController } from './forms.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FormSchema } from './forms.schema';
+import { ConstantsService } from 'src/constants/constants.service';
 
 @Module({
     imports: [MongooseModule.forFeature([
         { name: 'Form', schema: FormSchema },
     ])],
     providers: [
-        FormsService,],
+        FormsService,
+        ConstantsService
+    ],
     controllers: [FormsController]
 })
 export class FormsModule { }
