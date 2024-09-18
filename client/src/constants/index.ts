ec34fa4eb105be2c55a4f22d8d6d5beaf3c5ce35
sdevmarc <suanieeee@yahoo.com>
2024-09-18 23:10:56 +0800

    Refactor constants file to add route paths

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,3 +1,16 @@
 const { VITE_API_HOST } = import.meta.env
 
 export const HOST = VITE_API_HOST
+
+export const ROUTES = {
+    HOME: '/',
+    LOGIN: '/login',
+    OVERVIEW: '/overview',
+    TRACER: '/tracer',
+    REGISTERED_STUDENTS: '/student',
+    ALUMNI_GRADUATES: '/student/alumni',
+    CREATE_STUDENT: '/student/create',
+    REGISTERED_PROGRAMS: '/program',
+    REGISTERED_COURSES: '/program/courses',
+    GOOGLE_FORM: '/form'
+}
