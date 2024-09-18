0a9141ffa2c4d9766bba24b15fd08b22d0c133d5
sdevmarc <suanieeee@yahoo.com>
2024-09-18 23:11:12 +0800

    Refactor dashboard form component to remove unused imports and update layout

--- client/src/pages/dashboard/form/form.tsx
+++ client/src/pages/dashboard/form/form.tsx
@@ -1,4 +1,3 @@
-import Header_Dashboard from '@/components/header-dashboard'
 import HeadSection, { SubHeadSectionDetails } from '@/components/head-section'
 import MainTable from '@/components/main-table'
 import { Sidebar, SidebarNavs } from '@/components/sidebar'
@@ -9,7 +8,6 @@ export default function Form() {
     return (
         <>
             <div className="flex flex-col min-h-screen items-center">
-                <Header_Dashboard />
                 <div className="w-full max-w-[90rem] flex flex-col">
                     <aside className="px-4 pb-4 pt-[5rem]">
                         <HeadSection>
