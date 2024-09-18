2b02247ef448975f5f2f2be5ad4bb7e104c36404
sdevmarc <suanieeee@yahoo.com>
2024-09-18 23:11:03 +0800

    Refactor routes file to update page paths and add header layouts

--- client/src/pages/routes.tsx
+++ client/src/pages/routes.tsx
@@ -1,40 +1,54 @@
 import { createBrowserRouter } from "react-router-dom"
-import Home from "./home/Home"
 import Dashboard from "./dashboard/dashboard/dashboard"
 import Tracer from "./dashboard/tracer/tracer"
 import Student from "./dashboard/student/student"
 import Program from "./dashboard/program/program"
-import PostForm from "./dashboard/form/post-form"
-import Form from "./dashboard/form/form"
 import Profile from "./dashboard/settings/profile"
 import Courses from "./dashboard/program/courses"
 import Alumni from "./dashboard/student/alumni"
 import CreateStudent from "./dashboard/student/create-student"
+import LoginPage from "./home/login"
+import { HeaderLayout, HomeHeaderLayout, TracerHeaderLayout } from "@/components/header"
+import Home from "./home/home"
 
 const Routes = createBrowserRouter([
-    { path: '/', element: <Home /> },
+    {
+        element: <HeaderLayout />,
+        children: [
 
-    //DASHBOARD
-    { path: '/dashboard', element: <Dashboard /> },
+            //DASHBOARD
+            { path: '/overview', element: <Dashboard /> },
 
-    //Tracer
-    { path: '/tracer', element: <Tracer /> },
+            //Student
+            { path: '/student', element: <Student /> },
+            { path: '/student/create', element: <CreateStudent /> },
+            { path: '/student/alumni', element: <Alumni /> },
 
-    //Student
-    { path: '/student', element: <Student /> },
-    { path: '/student/create', element: <CreateStudent /> },
-    { path: '/student/alumni', element: <Alumni /> },
+            //Program
+            { path: '/program', element: <Program /> },
+            { path: '/program/courses', element: <Courses /> },
 
-    //Program
-    { path: '/program', element: <Program /> },
-    { path: '/program/courses', element: <Courses /> },
+            //Form
+            // { path: '/form', element: <Form /> },
+            // { path: '/form/create-form', element: <PostForm /> },
 
-    //Form
-    { path: '/form', element: <Form /> },
-    { path: '/form/create-form', element: <PostForm /> },
-
-    //Settings
-    { path: '/profile', element: <Profile /> },
+            //Settings
+            { path: '/profile', element: <Profile /> },
+        ]
+    },
+    {
+        element: <TracerHeaderLayout />,
+        children: [
+            { path: '/tracer', element: <Tracer /> },
+        ]
+    },
+    {
+        element: <HomeHeaderLayout />,
+        children: [
+            { path: '/', element: <Home /> },
+            { path: '/login', element: <LoginPage /> },
+        ]
+    }
 ])
 
 export default Routes
