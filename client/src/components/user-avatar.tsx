2f706e77379487fa96352b678399562de972a4b1
sdevmarc <suanieeee@yahoo.com>
2024-09-18 23:10:07 +0800

    Refactor UserAvatar component to add account settings and logout functionality

--- client/src/components/user-avatar.tsx
+++ client/src/components/user-avatar.tsx
@@ -3,15 +3,28 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import {
     DropdownMenu,
     DropdownMenuContent,
+    DropdownMenuGroup,
     DropdownMenuItem,
     DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { ROUTES } from "@/constants"
 
 import { IAvatar } from "@/interface/index"
+import { Link, useNavigate } from "react-router-dom"
 
 export const UserAvatar = ({ image, initials }: IAvatar) => {
+    const navigate = useNavigate()
+
+    const handleAccountSettings = () => {
+        navigate(ROUTES.OVERVIEW)
+    }
+
+    const handleLogout = () => {
+        navigate(ROUTES.HOME)
+    }
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger>
@@ -22,13 +35,29 @@ export const UserAvatar = ({ image, initials }: IAvatar) => {
                     </AvatarFallback>
                 </Avatar>
             </DropdownMenuTrigger>
-            <DropdownMenuContent>
+            <DropdownMenuContent className="w-[13rem]">
                 <DropdownMenuLabel>My Account</DropdownMenuLabel>
                 <DropdownMenuSeparator />
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Billing</DropdownMenuItem>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuItem>Subscription</DropdownMenuItem>
+                <DropdownMenuGroup>
+                    <DropdownMenuItem onClick={handleAccountSettings}>
+                        <Link to={`/overview`}>
+                            Account Settings
+                        </Link>
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuSeparator />
+                <DropdownMenuItem asChild>
+                    <a href="https://www.linkedin.com/in/sdevmarc/" target="_blank">
+                        Contact Support
+                    </a>
+                </DropdownMenuItem>
+                {/* <DropdownMenuItem>Support</DropdownMenuItem> */}
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={handleLogout}>
+                    Log out
+                    {/* <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut> */}
+                </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
     )
