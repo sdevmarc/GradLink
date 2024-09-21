cd7056be9cf02aa4c2a929b6a1493f7dd6db958d
sdevmarc <suanieeee@yahoo.com>
2024-09-21 14:50:06 +0800

    Refactor API_STUDENT_CREATE to include isenrolled parameter

--- client/src/api/student/index.ts
+++ client/src/api/student/index.ts
@@ -7,9 +7,14 @@ export const API_STUDENT_FINDALL = async () => {
     return response.data
 }
 
-export const API_STUDENT_CREATE = async ({ idNumber, name, email, enrollments, status }: IAPIStudents) => {
+export const API_STUDENT_FINDALL_ENROLLED = async () => {
+    const response = await axios.get(`${HOST}/student/enrolled`)
+    return response.data
+}
+
+export const API_STUDENT_CREATE = async ({ idNumber, name, email, enrollments, isenrolled = true }: IAPIStudents) => {
     const response = await axios.post(`${HOST}/student/create`, {
-        idNumber, name, email, enrollments, status
+        idNumber, name, email, enrollments, isenrolled
     })
     return response.data
 }
