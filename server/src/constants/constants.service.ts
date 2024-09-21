6e889dd4e5d1e6e8955903eae5dc445402eb0988
sdevmarc <suanieeee@yahoo.com>
2024-09-21 19:15:03 +0800

    Refactor ConstantsService to include email transport settings

--- server/src/constants/constants.service.ts
+++ server/src/constants/constants.service.ts
@@ -4,12 +4,30 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class ConstantsService {
     private readonly form_id: string
+    private readonly mail_username: string
+    private readonly mail_password: string
+    private readonly mail_host: string
 
     constructor(private configService: ConfigService) {
-        this.form_id = this.configService.get<string>('FORM_ID');
+        this.form_id = this.configService.get<string>('FORM_ID')
+        this.mail_username = this.configService.get<string>('EMAIL_USERNAME')
+        this.mail_password = this.configService.get<string>('EMAIL_PASSWORD')
+        this.mail_host = this.configService.get<string>('EMAIL_HOST');
     }
 
     getFormId(): string {
         return this.form_id
+    }
+
+    getMailUsername(): string {
+        return this.mail_username
+    }
+
+    getMailPassword(): string {
+        return this.mail_password
+    }
+
+    getMailHost(): string {
+        return this.mail_host
     }
 }
