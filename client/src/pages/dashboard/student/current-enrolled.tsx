c7a69f4081390dedc71516430ce547e8049eb8fc
sdevmarc <suanieeee@yahoo.com>
2024-09-21 14:51:21 +0800

    Refactor current-enrolled.tsx to update title and description

--- client/src/pages/dashboard/student/current-enrolled.tsx
+++ client/src/pages/dashboard/student/current-enrolled.tsx
@@ -2,15 +2,15 @@ import MainTable from "@/components/main-table"
 import HeadSection, { SubHeadSectionDetails } from "@/components/head-section"
 import { Sidebar, SidebarNavs } from "@/components/sidebar"
 import { DataTable } from "@/components/data-table-components/data-table"
-import { StudentColumns } from "@/components/data-table-components/columns/student-columns"
 import { useQuery } from "@tanstack/react-query"
-import { API_STUDENT_FINDALL } from "@/api/student"
+import { API_STUDENT_FINDALL_ENROLLED } from "@/api/student"
 import { ROUTES } from "@/constants"
+import { StudentCourseColumns } from "./student-data-table-components/student/columns"
 
 export default function CurrentEnrolledStudent() {
     const { data: students, isLoading: studentLoading, isFetched: studentFetched } = useQuery({
-        queryFn: () => API_STUDENT_FINDALL(),
-        queryKey: ['students']
+        queryFn: () => API_STUDENT_FINDALL_ENROLLED(),
+        queryKey: ['enrolled_students']
     })
 
     return (
@@ -20,19 +20,19 @@ export default function CurrentEnrolledStudent() {
                     <aside className="px-4 pb-4 pt-[8rem]">
                         <HeadSection>
                             <SubHeadSectionDetails
-                                title="RECORD OF CURRENTLY ENROLLED STUDENTS"
-                                description="Here's a list of enrolled students."
+                                title="CURRENTLY ENROLLED STUDENTS"
+                                description="Here's a list of currently enrolled students this semester."
                             />
                         </HeadSection>
                     </aside>
                     <main className="flex">
                         <Sidebar>
-                            <SidebarNavs bg='bg-muted' title="Currently Enrolled" link={ROUTES.CURRENTLY_ENROLLED}/>
-                            <SidebarNavs title="List of Students" link={ROUTES.LIST_OF_STUDENTS}/>
+                            <SidebarNavs bg='bg-muted' title="Currently Enrolled" link={ROUTES.CURRENTLY_ENROLLED} />
+                            <SidebarNavs title="List of Students" link={ROUTES.LIST_OF_STUDENTS} />
                             <SidebarNavs title="Alumni Graduates" link={ROUTES.ALUMNI_GRADUATES} />
                         </Sidebar>
                         <MainTable>
-                            {studentFetched && <DataTable toolbar="student" columns={StudentColumns} data={students.data || []} />}
+                            {studentFetched && <DataTable toolbar="current_student" columns={StudentCourseColumns} data={students.data || []} />}
                         </MainTable>
                     </main>
                 </div>
