4080892654222e42bde094159bb90b37f2274123
sdevmarc <suanieeee@yahoo.com>
2024-09-21 15:52:29 +0800

    Refactor enroll-student.tsx to add student course enrollment functionality

---
+++ client/src/pages/dashboard/student/student-data-table-components/enroll-student/columns-student-enroll.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import {
+    ColumnDef,
+} from "@tanstack/react-table"
+
+import { IAPICourse } from '@/interface/course.interface'
+import { Checkbox } from "@/components/ui/checkbox";
+
+export const StudentCourseColumns: ColumnDef<IAPICourse>[] = [
+    {
+        id: "select",
+        header: ({ table }) => (
+            <Checkbox
+                checked={
+                    table.getIsAllPageRowsSelected() ||
+                    (table.getIsSomePageRowsSelected() && "indeterminate")
+                }
+                onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+                aria-label="Select all"
+            />
+        ),
+        cell: ({ row }) => (
+            <Checkbox
+                checked={row.getIsSelected()}
+                onCheckedChange={(value) => row.toggleSelected(!!value)}
+                aria-label="Select row"
+            />
+        ),
+        enableSorting: false,
+        enableHiding: false,
+    },
+    {
+        accessorKey: "courseno",
+        header: "Course No.",
+        cell: ({ row }) => (
+            <div>{row.getValue("courseno")}</div>
+        ),
+    },
+    {
+        accessorKey: "descriptiveTitle",
+        header: 'Descriptive Title',
+        cell: ({ row }) => <div className="capitalize">{row.getValue("descriptiveTitle")}</div>,
+    },
+    {
+        accessorKey: "units",
+        header: 'Units',
+        cell: ({ row }) => (
+            <div>{row.getValue("units")}</div>
+        ),
+    }
+]
