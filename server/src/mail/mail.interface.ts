f4b04a7dcb5df190ba54b36414e49c0f37c4c2b6
sdevmarc <suanieeee@yahoo.com>
2024-09-22 19:07:19 +0800

    Refactor mail interface to include additional fields

--- server/src/mail/mail.interface.ts
+++ server/src/mail/mail.interface.ts
@@ -1,3 +1,10 @@
+export interface IMail {
+    send_to?: string
+    idNumber?: string
+    date_sent?: string
+    notes?: string
+}
+
 export interface IPromiseMail {
     success?: boolean
     message?: string
