91e087f23605d0d3c0cc9ab40be701a0b11b9116
sdevmarc <suanieeee@yahoo.com>
2024-09-22 19:23:00 +0800

    Refactor mail module to remove unused 'send_to' field and update imports

--- server/src/mail/mail.module.ts
+++ server/src/mail/mail.module.ts
@@ -3,12 +3,10 @@ import { MailService } from './mail.service';
 import { MailController } from './mail.controller';
 import { ConstantsService } from 'src/constants/constants.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { FormSchema } from 'src/forms/forms.schema';
 import { MailSchema } from './mail.schema';
 
 @Module({
   imports: [MongooseModule.forFeature([
-    { name: 'Form', schema: FormSchema },
     { name: 'Mail', schema: MailSchema },
   ])],
   providers: [MailService, ConstantsService],
