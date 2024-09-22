af4e911fb98b143ea38a3ae28f7584b37a21b463
sdevmarc <suanieeee@yahoo.com>
2024-09-22 18:30:48 +0800

    Refactor mail.module.ts to import FormSchema and MailSchema in MongooseModule

--- server/src/mail/mail.module.ts
+++ server/src/mail/mail.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { MailController } from './mail.controller';
 import { ConstantsService } from 'src/constants/constants.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FormSchema } from 'src/forms/forms.schema';
+import { MailSchema } from './mail.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([
+    { name: 'Form', schema: FormSchema },
+    { name: 'Mail', schema: MailSchema },
+  ])],
   providers: [MailService, ConstantsService],
   controllers: [MailController]
 })
