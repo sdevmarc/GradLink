ffc204398d32b356e0603843afd4f6961ead9bad
sdevmarc <suanieeee@yahoo.com>
2024-09-22 21:02:06 +0800

    Refactor form API to add endpoint for retrieving forms with unknown respondents

---
+++ client/src/api/form/index.ts
@@ -0,0 +1,7 @@
+import { HOST } from '@/constants'
+import axios from 'axios'
+
+export const API_FORM_FINDALL_UNKNOWN = async () => {
+    const response = await axios.get(`${HOST}/forms/unknown-respondents`)
+    return response.data
+}
