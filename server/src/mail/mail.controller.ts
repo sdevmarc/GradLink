468a96a2c2a60f1f90f960762454bb27c910d3df
sdevmarc <suanieeee@yahoo.com>
2024-09-22 19:22:39 +0800

    Refactor MailController to update sendMailer method to accept an array of email addresses

--- server/src/mail/mail.controller.ts
+++ server/src/mail/mail.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { MailService } from './mail.service';
-import { IMail } from './mail.interface';
 
 @Controller('mail')
 export class MailController {
     constructor(private readonly mailService: MailService) { }
 
     @Get(':email')
-    async sendMailer(@Param('email') send_to: IMail) {
+    async sendMailer(@Param('email') send_to: string[]) {
         return await this.mailService.sendMail(send_to);
     }
 }
