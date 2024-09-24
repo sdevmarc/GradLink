2d4ba7d20b778a5940a0f9ad859e611ace9a0640
sdevmarc <suanieeee@yahoo.com>
2024-09-24 18:40:06 +0800

    Refactor alumni API to fetch all alumni students and find one alumni student by ID number

--- client/src/api/alumni/index.ts
+++ client/src/api/alumni/index.ts
@@ -1,7 +1,13 @@
 import { HOST } from '@/constants'
+import { IAPIAlumni } from '@/interface/alumni.interface'
 import axios from 'axios'
 
 export const API_STUDENT_FINDALL_ALUMNI = async () => {
     const response = await axios.get(`${HOST}/student/alumni`)
+    return response.data
+}
+
+export const API_STUDENT_FINDONE_ALUMNI = async ({ idNumber }: IAPIAlumni) => {
+    const response = await axios.get(`${HOST}/student/findone/${idNumber}`)
     return response.data
 }
