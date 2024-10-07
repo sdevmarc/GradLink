019e0c2363d9bfbe9c7e58ecb49b8771a27c139b
sdevmarc <suanieeee@yahoo.com>
2024-10-07 21:52:48 +0800

    Refactor DataTableStudentGraduatingStudent component to add select checkbox, improve column headers, and update cell rendering

---
+++ client/src/pages/dashboard/student/student-data-table-components/graduating-student/data-table-graduating-student.tsx
@@ -0,0 +1,180 @@
+"use client"
+
+import { Button } from '@/components/ui/button'
+
+import * as React from "react"
+import {
+    ColumnDef,
+    ColumnFiltersState,
+    SortingState,
+    VisibilityState,
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    useReactTable,
+} from "@tanstack/react-table"
+
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { IAPIStudents } from '@/interface/student.interface'
+import { DataTableToolbarGraduatingStudent } from './data-table-toolbar-graduating-student'
+
+interface DataTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[]
+    data: TData[],
+    resetSelection: boolean
+    onResetComplete: () => void
+}
+
+export function DataTableStudentGraduatingStudent<TData, TValue>({
+    columns,
+    data,
+    resetSelection,
+    onResetComplete
+}: DataTableProps<TData, TValue>) {
+    const [sorting, setSorting] = React.useState<SortingState>([])
+    const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([])
+    const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({})
+    const [rowSelection, setRowSelection] = React.useState({})
+    const [isrows, setRows] = React.useState<boolean>(false)
+    const [selected, setSelected] = React.useState<IAPIStudents[]>([])
+
+    const table = useReactTable({
+        data,
+        columns,
+        onSortingChange: setSorting,
+        onColumnFiltersChange: setColumnFilters,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        onColumnVisibilityChange: setColumnVisibility,
+        onRowSelectionChange: setRowSelection,
+        state: {
+            sorting,
+            columnFilters,
+            columnVisibility,
+            rowSelection,
+        },
+    })
+
+    React.useEffect(() => {
+        const selectedRows = table.getFilteredSelectedRowModel().rows;
+        const current_enrolled = selectedRows.map(row => {
+            const original = row.original as IAPIStudents;
+            const { idNumber, name, email, status } = original;
+            return { idNumber, name, email, status };
+        });
+
+        if (selectedRows.length > 0) {
+            setRows(true)
+        } else {
+            setRows(false)
+        }
+        setSelected(current_enrolled)
+    }, [rowSelection, table]);
+
+    React.useEffect(() => {
+        if (resetSelection) {
+            table.resetRowSelection();
+            onResetComplete();
+        }
+    }, [resetSelection, table, onResetComplete]);
+
+    return (
+        <div className="w-full flex flex-col gap-4">
+            <DataTableToolbarGraduatingStudent
+                table={table}
+                isrows={isrows}
+                checks={selected}
+                onResetSelections={() => {
+                    setRows(false)
+                    table.resetRowSelection()
+                }
+                }
+            />
+            <div className="rounded-md border">
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead key={header.id}>
+                                            {header.isPlaceholder
+                                                ? null
+                                                : flexRender(
+                                                    header.column.columnDef.header,
+                                                    header.getContext()
+                                                )}
+                                        </TableHead>
+                                    )
+                                })}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow
+                                    key={row.id}
+                                    data-state={row.getIsSelected() && "selected"}
+                                >
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell key={cell.id}>
+                                            {flexRender(
+                                                cell.column.columnDef.cell,
+                                                cell.getContext()
+                                            )}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell
+                                    colSpan={columns.length}
+                                    className="h-24 text-center"
+                                >
+                                    No results.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+            <div className="flex items-center justify-end space-x-2 py-4">
+                <div className="flex-1 text-sm text-muted-foreground">
+                    {table.getFilteredSelectedRowModel().rows.length} of{" "}
+                    {table.getFilteredRowModel().rows.length} row(s) selected.
+                </div>
+                <div className="space-x-2">
+                    <Button
+                        variant="outline"
+                        size="sm"
+                        onClick={() => table.previousPage()}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        Previous
+                    </Button>
+                    <Button
+                        variant="outline"
+                        size="sm"
+                        onClick={() => table.nextPage()}
+                        disabled={!table.getCanNextPage()}
+                    >
+                        Next
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
