26eb42bc29cf7ad25f6845b37173b387008ca883
sdevmarc <suanieeee@yahoo.com>
2024-10-07 21:51:59 +0800

    Refactor DataTableStudentAlumni component to remove unnecessary code and improve readability

--- client/src/pages/dashboard/student/student-data-table-components/alumni/data-table-alumni.tsx
+++ client/src/pages/dashboard/student/student-data-table-components/alumni/data-table-alumni.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 
 import * as React from "react"
@@ -25,10 +24,7 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
-import { AlertDialogConfirmation } from '@/components/alert-dialog'
-import { DataTableViewOptions } from '@/components/data-table-components/data-table-view-options'
-import { Cross2Icon } from '@radix-ui/react-icons'
-import { useNavigate } from 'react-router-dom'
+import { DataTableToolbarAlumni } from './data-table-toolbar-alumni'
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[]
@@ -63,73 +59,9 @@ export function DataTableStudentAlumni<TData, TValue>({
         },
     })
 
-    const isFiltered = table.getState().columnFilters.length > 0;
-    // const [dateRange, setDateRange] = useState<{ from: Date; to: Date }>({
-    //     from: new Date(new Date().getFullYear(), 0, 1),
-    //     to: new Date()
-    // });
-
-    // const handleDateSelect = ({ from, to }: { from: Date; to: Date }) => {
-    //     setDateRange({ from, to });
-    //     // Filter table data based on selected date range
-    //     table.getColumn("date")?.setFilterValue([from, to]);
-    // };
-    const navigate = useNavigate()
-
     return (
         <div className="w-full flex flex-col gap-4">
-            <div className="flex flex-wrap items-center justify-between">
-                <div className="flex flex-1 flex-wrap items-center gap-2">
-                    <Input
-                        placeholder="Filter labels..."
-                        value={(table.getColumn("idNumber")?.getFilterValue() as string) ?? ""}
-                        onChange={(event) => {
-                            table.getColumn("idNumber")?.setFilterValue(event.target.value);
-                        }}
-                        className="h-8 w-[20rem] lg:w-[25rem] placeholder:text-muted"
-                    />
-
-                    {/* {table.getColumn("category") && (
-                    <DataTableFacetedFilter
-                        column={table.getColumn("category")}
-                        title="Category"
-                        options={categories}
-                    />
-                )}
-                {table.getColumn("type") && (
-                    <DataTableFacetedFilter
-                        column={table.getColumn("type")}
-                        title="Type"
-                        options={incomeType}
-                    />
-                )} */}
-                    {isFiltered && (
-                        <Button
-                            variant="ghost"
-                            onClick={() => table.resetColumnFilters()}
-                            className="h-8 px-2 lg:px-3"
-                        >
-                            Reset
-                            <Cross2Icon className="ml-2 h-4 w-4" />
-                        </Button>
-                    )}
-                    {/* <CalendarDatePicker
-                    date={dateRange}
-                    onDateSelect={handleDateSelect}
-                    className="w-[250px] h-8"
-                    variant="outline"
-                /> */}
-                </div>
-                <div className="flex gap-2 items-center">
-                    <AlertDialogConfirmation
-                        btnTitle="Export"
-                        title="Are you sure?"
-                        description={`This will export the current data you are viewing.`}
-                        btnContinue={() => navigate('/program')}
-                    />
-                    <DataTableViewOptions table={table} />
-                </div>
-            </div>
+            <DataTableToolbarAlumni table={table} />
             <div className="rounded-md border">
                 <Table>
                     <TableHeader>
