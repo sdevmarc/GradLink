dbd38826a8db589d5f80f41962d2c82e408e17a7
sdevmarc <suanieeee@yahoo.com>
2024-10-09 23:08:56 +0800

    Refactor program page title to be more concise

--- client/src/pages/dashboard/program/available-programs.tsx
+++ client/src/pages/dashboard/program/available-programs.tsx
@@ -19,7 +19,7 @@ export default function Program() {
                     <aside className="px-4 pb-4 pt-[8rem]">
                         <HeadSection>
                             <SubHeadSectionDetails
-                                title="RECORD OF REGISTERED PROGRAMS"
+                                title="REGISTERED PROGRAMS"
                                 description="Here's a list of registered programs."
                             />
                         </HeadSection>
