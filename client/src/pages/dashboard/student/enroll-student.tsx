3a3007fafcaf2fdaca12859d1e72c8b1bae6cc73
sdevmarc <suanieeee@yahoo.com>
2024-10-09 21:44:16 +0800

    Refactor CurrentEnrolledStudent component to add loading state

--- client/src/pages/dashboard/student/enroll-student.tsx
+++ client/src/pages/dashboard/student/enroll-student.tsx
@@ -2,18 +2,23 @@ import HeadSection, { BackHeadSection, SubHeadSectionDetails } from '@/component
 import { Sidebar, SidebarNavs } from '@/components/sidebar'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
-import { useMutation, useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { API_COURSE_FINDALL } from '@/api/courses'
 import * as React from "react"
-import { IAPICourse } from '@/interface/course.interface'
 import { ROUTES } from '@/constants'
 import { IAPIStudents, ICourses } from '@/interface/student.interface'
 import { ComboBox } from '@/components/combo-box'
-import { DataTableEnrollStudent } from './student-data-table-components/enroll-student/data-table-enroll-student'
 import { API_STUDENT_CREATE } from '@/api/student'
 import ContinueDialog from '@/components/continue-dialog'
-import { CircleCheck, CircleX } from 'lucide-react'
+import { CircleCheck, CircleX, Plus } from 'lucide-react'
+import { API_PROGRAM_FINDALL } from '@/api/program'
+import { CreateProgramColumns } from '../program/program-data-table-components/program/columns'
+import { IAPIPrograms } from '@/interface/program.interface'
+import { DataTableCreateProgram } from '../program/program-data-table-components/program/data-table-program-create'
+import { DataTableEnrollStudent } from './student-data-table-components/enroll-student/data-table-enroll-student'
 import { StudentCourseColumns } from './student-data-table-components/enroll-student/columns-student-enroll'
+import { IAPICourse } from '@/interface/course.interface'
+import Loading from '@/components/loading'
 
 export default function CreateStudent() {
     return (
@@ -51,54 +56,74 @@ const lists = [
 ]
 
 const CreateForm = () => {
-    const [resetSelection, setResetSelection] = React.useState(false);
+    const queryClient = useQueryClient()
+    const [selectedPrograms, setSelectedPrograms] = React.useState<string[]>([])
+    const [isAddAdditional, setIsAddAdditional] = React.useState<boolean>(false)
+    const [resetSelection, setResetSelection] = React.useState(false)
     const [dialogState, setDialogState] = React.useState({
         show: false,
         title: '',
         description: '',
         success: false
-    });
+    })
     const [student, setStudent] = React.useState<IAPIStudents>({
         idNumber: '',
         name: '',
         email: '',
         enrollments: {}
-    });
+    })
     const { data: course, isLoading: courseLoading, isFetched: courseFetched } = useQuery({
         queryFn: () => API_COURSE_FINDALL(),
         queryKey: ['course']
     })
 
+    const { data: dataProgram, isLoading: programLoading, isFetched: programFetched } = useQuery({
+        queryFn: () => API_PROGRAM_FINDALL(),
+        queryKey: ['program']
+    })
+
+
     const { mutateAsync: insertStudent, isPending: studentLoading } = useMutation({
         mutationFn: API_STUDENT_CREATE,
-        onSuccess: (data) => {
+        onSuccess: async (data) => {
             if (!data.success) {
                 setDialogState({ success: false, show: true, title: 'Uh, oh! Something went wrong.', description: data.message })
             } else {
-                setDialogState({ success: true, show: true, title: data.message, description: 'Do you want to continue creating course?' })
+                await queryClient.invalidateQueries({ queryKey: ['students'] })
+                await queryClient.refetchQueries({ queryKey: ['students'] })
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                })
+                setDialogState({ success: true, show: true, title: 'Yay, Success', description: data.message })
                 setResetSelection(true)
                 setStudent(prev => ({ ...prev, idNumber: '', name: '', email: '', enrollments: {} }))
             }
+        },
+        onError: (data) => {
+            setDialogState({ success: false, show: true, title: 'Uh, oh! Something went wrong.', description: data.message })
         }
     })
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        console.log(student)
+        if (selectedPrograms.length === 0) return setDialogState({ success: false, show: true, title: 'Uh, oh! Something went wrong.', description: 'You need to select a degree.' })
         const { idNumber, name, email, enrollments } = student
+
         if (!idNumber?.trim() || !name || !email?.trim() || !enrollments?.semester || !(enrollments.courses?.length || 0)) return alert('Please fill-up the required fields.')
         await insertStudent({ idNumber, name, email, enrollments })
     }
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setStudent((prev) => ({ ...prev, [name]: value }));
-    };
+        const { name, value } = e.target
+        setStudent((prev) => ({ ...prev, [name]: value }))
+    }
 
     const handleCoursesChange = (selectedCourses: IAPICourse[]) => {
-        const courses: ICourses[] = selectedCourses.map(({ courseno, descriptiveTitle }) => ({
+        const courses: ICourses[] = selectedCourses.map(({ courseno, descriptiveTitle, units }) => ({
             courseno: courseno || '',
-            descriptive_title: descriptiveTitle || ''
+            descriptive_title: descriptiveTitle || '',
+            units: Number(units || 0)
         }))
 
         setStudent(prev => ({
@@ -110,96 +135,150 @@ const CreateForm = () => {
         }))
     }
 
+    const handleProgramChange = (programs: IAPIPrograms[]) => {
+        const selectedCodes = programs?.map(item => {
+            const { code } = item
+            if (!code) return
+            return code
+        })
+        setSelectedPrograms(selectedCodes.filter((code): code is string => code !== undefined))
+    }
+
+    const handleOnClickAddAdditionalCourse = () => {
+        if (selectedPrograms.length === 0) return setDialogState({ success: false, show: true, title: 'Uh, oh! Something went wrong.', description: 'You need to select a degree first.' })
+        setIsAddAdditional(true)
+    }
+
     return (
-        <form onSubmit={handleSubmit} className="w-[80%] flex flex-col justify-start gap-4 rounded-lg border">
-            <ContinueDialog
-                icon={dialogState.success ? <CircleCheck color="#42a626" size={70} /> : <CircleX color="#880808" size={70} />}
-                trigger={dialogState.show}
-                title={dialogState.title}
-                description={dialogState.description}
-                onClose={() => { setDialogState(prev => ({ ...prev, show: false })) }}
-            />
-            <div className="w-full px-4 py-3 border-b">
-                <h1 className='text-text font-semibold text-lg'>Create a student</h1>
-            </div>
-            <div className="w-full py-2 flex flex-col justify-between">
-                <div className="w-full flex flex-col gap-4">
-                    <div className="flex flex-col px-4 gap-1">
-                        <h1 className='text-[.83rem]'>ID Number</h1>
-                        <Input
-                            value={student.idNumber}
-                            onChange={handleInputChange}
-                            name='idNumber'
-                            type='text'
-                            placeholder='eg. 000xxxxxx'
-                            required
+        <>
+            {
+                studentLoading ? <Loading />
+                    :
+                    <form onSubmit={handleSubmit} className="w-[80%] flex flex-col justify-start gap-4 rounded-lg border">
+                        <ContinueDialog
+                            icon={dialogState.success ? <CircleCheck color="#42a626" size={70} /> : <CircleX color="#880808" size={70} />}
+                            trigger={dialogState.show}
+                            title={dialogState.title}
+                            description={dialogState.description}
+                            onClose={() => { setDialogState(prev => ({ ...prev, show: false })) }}
                         />
-                    </div>
-                    <div className="flex flex-col px-4 gap-1">
-                        <h1 className='text-[.83rem]'>Full Name</h1>
-                        <Input
-                            value={student.name}
-                            onChange={handleInputChange}
-                            name='name'
-                            type='text'
-                            placeholder='eg. John Doe'
-                            required
-                        />
-                    </div>
-                    <div className="flex flex-col px-4 gap-1">
-                        <h1 className='text-[.83rem]'>Email Address</h1>
-                        <Input
-                            value={student.email}
-                            onChange={handleInputChange}
-                            name='email'
-                            type='text'
-                            placeholder='eg. m@example.com'
-                            required
-                        />
-                    </div>
-                    <div className="max-w-lg flex flex-col px-4 gap-1">
-                        <h1 className='text-[.83rem]'>Choose a semester</h1>
-                        <ComboBox
-                            type={
-                                (e) => {
-                                    setStudent(prev => ({
-                                        ...prev,
-                                        enrollments: { ...prev.enrollments, semester: e || '' }
-                                    }))
+                        <div className="w-full px-4 py-3 border-b">
+                            <h1 className='text-text font-semibold text-lg'>Create a student</h1>
+                        </div>
+                        <div className="w-full py-2 flex flex-col justify-between">
+                            <div className="w-full flex flex-col gap-4">
+                                <div className="flex flex-col px-4 gap-1">
+                                    <h1 className='text-[.83rem]'>ID Number</h1>
+                                    <Input
+                                        value={student.idNumber}
+                                        onChange={handleInputChange}
+                                        name='idNumber'
+                                        type='text'
+                                        placeholder='eg. 000xxxxxx'
+                                        required
+                                    />
+                                </div>
+                                <div className="flex flex-col px-4 gap-1">
+                                    <h1 className='text-[.83rem]'>Full Name</h1>
+                                    <Input
+                                        value={student.name}
+                                        onChange={handleInputChange}
+                                        name='name'
+                                        type='text'
+                                        placeholder='eg. John Doe'
+                                        required
+                                    />
+                                </div>
+                                <div className="flex flex-col px-4 gap-1">
+                                    <h1 className='text-[.83rem]'>Email Address</h1>
+                                    <Input
+                                        value={student.email}
+                                        onChange={handleInputChange}
+                                        name='email'
+                                        type='text'
+                                        placeholder='eg. m@example.com'
+                                        required
+                                    />
+                                </div>
+                                <div className="max-w-lg flex flex-col px-4 gap-1">
+                                    <h1 className='text-[.83rem]'>Choose a semester</h1>
+                                    <ComboBox
+                                        type={
+                                            (e) => {
+                                                setStudent(prev => ({
+                                                    ...prev,
+                                                    enrollments: { ...prev.enrollments, semester: e || '' }
+                                                }))
+                                            }
+                                        }
+                                        title='None'
+                                        lists={lists || []}
+                                        value={student.enrollments?.semester || ''}
+                                    />
+                                </div>
+                                <div className="flex flex-col px-4 gap-2">
+                                    <div className="flex flex-col gap-1">
+                                        <h1 className='text-[1.1rem] font-medium'>
+                                            Degree's Available
+                                        </h1>
+                                        <p className="text-sm">
+                                            Please check degree(s) that you wish to enroll the student.
+                                        </p>
+                                    </div>
+                                    <div className="w-full flex flex-col gap-2 justify-center items-start">
+                                        {programLoading && <div>Loading...</div>}
+                                        {
+                                            programFetched &&
+                                            <DataTableCreateProgram
+                                                data={dataProgram.data || []}
+                                                columns={CreateProgramColumns}
+                                                fetchCheck={handleProgramChange}
+                                                resetSelection={resetSelection}
+                                                onResetComplete={() => setResetSelection(false)}
+                                            />
+                                        }
+                                    </div>
+                                </div>
+
+                                <div className={`${!isAddAdditional && 'hidden'} flex flex-col px-4 gap-2`}>
+                                    <div className="flex flex-col gap-1">
+                                        <h1 className='text-[1.1rem] font-medium'>
+                                            Courses Available
+                                        </h1>
+                                        <p className="text-sm">
+                                            Please check the courses that you wish to enroll.
+                                        </p>
+                                    </div>
+                                    <div className="w-full flex flex-col gap-2 justify-center items-start">
+                                        {courseLoading && <div>Loading...</div>}
+                                        {courseFetched && <DataTableEnrollStudent
+                                            data={course.data || []}
+                                            columns={StudentCourseColumns}
+                                            onSubmit={handleCoursesChange}
+                                            resetSelection={resetSelection}
+                                            onResetComplete={() => setResetSelection(false)}
+                                            selectedPrograms={selectedPrograms}
+                                            isAdditional={(e) => setIsAddAdditional(e)}
+                                        />}
+                                    </div>
+                                </div>
+                                {
+                                    !isAddAdditional &&
+                                    <div className="flex px-4">
+                                        <Button variant={`outline`} size={`sm`} className="flex items-center gap-4" type="button" onClick={handleOnClickAddAdditionalCourse}>
+                                            <Plus color="#000000" size={18} /> Add Additional Course
+                                        </Button>
+                                    </div>
                                 }
-                            }
-                            title='None'
-                            lists={lists || []}
-                            value={student.enrollments?.semester || ''}
-                        />
-                    </div>
-                    <div className="flex flex-col px-4 gap-2">
-                        <div className="flex flex-col gap-1">
-                            <h1 className='text-[1.1rem] font-medium'>
-                                Courses Available
-                            </h1>
-                            <p className="text-sm">
-                                Please check the courses that you wish to enroll.
-                            </p>
+                                <div className="w-full flex items-center justify-end px-4">
+                                    <Button disabled={studentLoading} type='submit' variant={`default`} size={`sm`}>
+                                        {studentLoading ? 'Submitting...' : 'Submit Enrollment'}
+                                    </Button>
+                                </div>
+                            </div>
                         </div>
-                        <div className="w-full flex flex-col gap-2 justify-center items-start">
-                            {courseLoading && <div>Loading...</div>}
-                            {courseFetched && <DataTableEnrollStudent
-                                data={course.data || []}
-                                columns={StudentCourseColumns}
-                                onSubmit={handleCoursesChange}
-                                resetSelection={resetSelection}
-                                onResetComplete={() => setResetSelection(false)}
-                            />}
-                        </div>
-                    </div>
-                    <div className="w-full flex items-center justify-end px-4">
-                        <Button type='submit' variant={`default`} size={`default`}>
-                            SUBMIT ENROLLMENT
-                        </Button>
-                    </div>
-                </div>
-            </div>
-        </form>
+                    </form>
+            }
+        </>
     )
 }
