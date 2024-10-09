d89d29fbebc9b8945f8936f9e3124c1a26bb2e80
sdevmarc <suanieeee@yahoo.com>
2024-10-09 21:44:59 +0800

    Refactor DataTableToolbarListOfStudent component to add smaller size option for reset button

--- client/src/pages/dashboard/student/student-data-table-components/list-of-students/data-table-toolbar-list-of-students.tsx
+++ client/src/pages/dashboard/student/student-data-table-components/list-of-students/data-table-toolbar-list-of-students.tsx
@@ -59,6 +59,7 @@ export function DataTableToolbarListOfStudent<TData>({
                         variant="ghost"
                         onClick={() => table.resetColumnFilters()}
                         className="h-8 px-2 lg:px-3"
+                        size={`sm`}
                     >
                         Reset
                         <Cross2Icon className="ml-2 h-4 w-4" />
