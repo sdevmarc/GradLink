c8d244a23aa4ca03387044a2efeb6ff43eebfe7c
sdevmarc <suanieeee@yahoo.com>
2024-10-14 23:11:51 +0800

    Refactor available-programs.tsx to update UI and use specialized DataTable component

--- client/src/pages/dashboard/program/available-programs.tsx
+++ client/src/pages/dashboard/program/available-programs.tsx
@@ -1,10 +1,11 @@
-import { DataTable } from '@/components/data-table-components/data-table'
 import HeadSection, { SubHeadSectionDetails } from '@/components/head-section'
 import MainTable from '@/components/main-table'
 import { SidebarNavs, Sidebar } from '@/components/sidebar'
-import { ProgramColumns } from '@/components/data-table-components/columns/program-columns'
 import { useQuery } from '@tanstack/react-query'
 import { API_PROGRAM_FINDALL } from '@/api/program'
+import { AvailableProgramsColumns } from './program-data-table-components/program/available-programs/columns-available-programs'
+import { DataTableAvailablePrograms } from './program-data-table-components/program/available-programs/data-table-available-programs'
+import { ROUTES } from '@/constants'
 
 export default function Program() {
     const { data: program, isLoading: programLoading, isFetched: programFetched } = useQuery({
@@ -19,19 +20,20 @@ export default function Program() {
                     <aside className="px-4 pb-4 pt-[8rem]">
                         <HeadSection>
                             <SubHeadSectionDetails
-                                title="REGISTERED PROGRAMS"
-                                description="Here's a list of registered programs."
+                                title="AVAILABLE PROGRAMS"
+                                description="Here's a list of avalailable programs."
                             />
                         </HeadSection>
                     </aside>
                     <main className="flex">
                         <Sidebar>
-                            <SidebarNavs bg='bg-muted' title="Programs" link="/program" />
-                            <SidebarNavs title="Courses" link="/program/courses" />
+                            <SidebarNavs bg='bg-muted' title="Programs" link={ROUTES.AVAILABLE_PROGRAMS} />
+                            <SidebarNavs title="Courses" link={ROUTES.AVAILABLE_COURSES} />
+                            <SidebarNavs title="Curriculums" link={ROUTES.CURRICULUM} />
                         </Sidebar>
                         <MainTable>
                             {programLoading && <div>Loading...</div>}
-                            {programFetched && <DataTable columns={ProgramColumns} data={program.data || []} toolbar='program' />}
+                            {programFetched && <DataTableAvailablePrograms columns={AvailableProgramsColumns} data={program.data.programs || []} />}
                         </MainTable>
                     </main>
                 </div>
