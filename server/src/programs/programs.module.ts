4e909c575575433647841ade51487038d1659ae1
sdevmarc <suanieeee@yahoo.com>
2024-10-14 23:16:02 +0800

    Add support for curriculum integration in ProgramsModule

--- server/src/programs/programs.module.ts
+++ server/src/programs/programs.module.ts
@@ -3,12 +3,15 @@ import { ProgramsService } from './programs.service';
 import { ProgramsController } from './programs.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProgramSchema } from './programs.schema';
+import { CurriculumSchema } from 'src/curriculum/curriculum.schema';
+import { CurriculumService } from 'src/curriculum/curriculum.service';
 
 @Module({
   imports: [MongooseModule.forFeature([
-    { name: 'Program', schema: ProgramSchema }
+    { name: 'Program', schema: ProgramSchema },
+    { name: 'Curriculum', schema: CurriculumSchema }
   ])],
-  providers: [ProgramsService],
+  providers: [ProgramsService, CurriculumService],
   controllers: [ProgramsController]
 })
 export class ProgramsModule { }
