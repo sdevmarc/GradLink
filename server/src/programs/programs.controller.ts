c8fb2cb0c3986ee344b956a15d33c9f6767c0124
sdevmarc <suanieeee@yahoo.com>
2024-10-14 23:15:49 +0800

    Refactor ProgramsController to add support for curriculum integration

--- server/src/programs/programs.controller.ts
+++ server/src/programs/programs.controller.ts
@@ -1,39 +1,55 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ProgramsService } from './programs.service';
-import { IPrograms } from './programs.interface';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common'
+import { ProgramsService } from './programs.service'
+import { IRequestPrograms } from './programs.interface'
+import { CurriculumService } from 'src/curriculum/curriculum.service'
 
 @Controller('programs')
 export class ProgramsController {
     constructor(
-        private readonly programService: ProgramsService
+        private readonly programService: ProgramsService,
+        private readonly curriculumService: CurriculumService
     ) { }
 
     @Get()
     async findAllPrograms() {
-        return await this.programService.findAll()
+        return await this.programService.findAllActive()
     }
 
-    @Get(':code')
-    async findOneProgram(@Param() { code }: IPrograms) {
-        return await this.programService.findOne({ code })
-    }
+    // @Get(':code')
+    // async findOneProgram(@Param() { code }: IPrograms) {
+    //     return await this.programService.findOne({ code })
+    // }
 
     @Post('create')
     async createProgram(
-        @Body() { code, descriptiveTitle, residency }: IPrograms
+        @Body() { name, programs }: IRequestPrograms
     ) {
-        return await this.programService.create({ code, descriptiveTitle, residency })
+        const validationResult = await this.programService.validate({ programs })
+        if (!validationResult.success) return validationResult
+
+        const { data } = await this.curriculumService.insertNew({ name })
+        return await this.programService.insertNew({ programs, curriculumId: data.toString() })
     }
 
-    @Post('update')
-    async updateProgram(
-        @Body() { pid, code, descriptiveTitle, residency }: IPrograms
+    @Post('add-program-to-active-curriculum')
+    async AddToActiveCurriculum(
+        @Body() { programs }: IRequestPrograms
     ) {
-        return await this.programService.findByIdAndUpdate({ pid, code, descriptiveTitle, residency })
+        const validationResult = await this.programService.validate({ programs })
+        if (!validationResult.success) return validationResult
+
+        return await this.programService.addProgramToActiveCurriculum({ programs })
     }
 
-    @Post(':pid')
-    async deleteProgram(@Param() { pid }: IPrograms) {
-        return this.programService.delete({ pid })
-    }
+    // @Post('update')
+    // async updateProgram(
+    //     @Body() { pid, code, descriptiveTitle, residency }: IPrograms
+    // ) {
+    //     return await this.programService.findByIdAndUpdate({ pid, code, descriptiveTitle, residency })
+    // }
+
+    // @Post(':pid')
+    // async deleteProgram(@Param() { pid }: IPrograms) {
+    //     return this.programService.delete({ pid })
+    // }
 }
