e97c21baebca9a2f0b6a0db62002cec5d174798b
sdevmarc <suanieeee@yahoo.com>
2024-10-14 23:11:47 +0800

    Refactor available-courses.tsx to update UI and use specialized DataTable component

--- client/src/pages/dashboard/program/available-courses.tsx
+++ client/src/pages/dashboard/program/available-courses.tsx
@@ -1,4 +1,3 @@
-import { DataTable } from '@/components/data-table-components/data-table'
 import HeadSection, { SubHeadSectionDetails } from '@/components/head-section'
 import MainTable from '@/components/main-table'
 import { SidebarNavs, Sidebar } from '@/components/sidebar'
@@ -6,6 +5,7 @@ import { useQuery } from '@tanstack/react-query'
 import { CourseColumns } from '@/components/data-table-components/columns/course-columns'
 import { API_COURSE_FINDALL } from '@/api/courses'
 import { ROUTES } from '@/constants'
+import { DataTableAvailableCourses } from './program-data-table-components/courses/available-courses/data-table-available-courses'
 
 export default function Courses() {
     const { data: course, isLoading: courseLoading, isFetched: courseFetched } = useQuery({
@@ -29,10 +29,11 @@ export default function Courses() {
                         <Sidebar>
                             <SidebarNavs title="Programs" link={ROUTES.AVAILABLE_PROGRAMS}/>
                             <SidebarNavs bg='bg-muted' title="Courses" link={ROUTES.AVAILABLE_COURSES} />
+                            <SidebarNavs title="Curriculums" link={ROUTES.CURRICULUM} />
                         </Sidebar>
                         <MainTable>
                             {courseLoading && <div>Loading...</div>}
-                            {courseFetched && <DataTable columns={CourseColumns} data={course.data || []} toolbar='course' />}
+                            {courseFetched && <DataTableAvailableCourses columns={CourseColumns} data={course.data || []}/>}
                         </MainTable>
                     </main>
                 </div>
