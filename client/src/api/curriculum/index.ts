8ba92dba96925f0db5512281d780ea7fb7d8ede9
sdevmarc <suanieeee@yahoo.com>
2024-10-15 17:28:57 +0800

    Refactor API_CURRICULUM_FINDALL and API_PROGRAM_ADD_NEW_CURRICULUM functions

--- client/src/api/curriculum/index.ts
+++ client/src/api/curriculum/index.ts
@@ -1,9 +1,19 @@
 import { HOST } from '@/constants'
+import { IRequestCurriculum } from '@/interface/curriculum.interface'
 import axios from 'axios'
 
 export const API_CURRICULUM_FINDALL = async () => {
     try {
         const response = await axios.get(`${HOST}/curriculum`)
+        return response.data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const API_PROGRAM_ADD_NEW_CURRICULUM = async ({ name, programs }: IRequestCurriculum) => {
+    try {
+        const response = await axios.post(`${HOST}/programs/create`, { name, programs })
         return response.data
     } catch (error) {
         console.error(error)
