ca279b957b4382b07dfd619ffbd4f527f4096139
sdevmarc <suanieeee@yahoo.com>
2024-10-15 17:31:34 +0800

    Refactor CreateCurriculum component to handle curriculum creation and program addition

--- client/src/pages/dashboard/program/create-curriculum.tsx
+++ client/src/pages/dashboard/program/create-curriculum.tsx
@@ -7,14 +7,70 @@ import { useState } from 'react'
 import { DataTableCreateProgramInCurriculum } from './program-data-table-components/curriculum/create-curriculum/data-table-create-program-in-curriculum'
 import { CreateProgramInCurriculumColumns } from './program-data-table-components/curriculum/create-curriculum/columns-create-program-in-curriculum'
 import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
+import { AlertDialogConfirmation } from '@/components/alert-dialog'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import Loading from '@/components/loading'
+import ContinueDialog from '@/components/continue-dialog'
+import { CircleCheck, CircleX } from 'lucide-react'
+import { API_PROGRAM_ADD_NEW_CURRICULUM } from '@/api/curriculum'
 
 export default function CreateCurriculum() {
+    const queryClient = useQueryClient()
+    const [isreset, setReset] = useState<boolean>(false)
     const [programs, setPrograms] = useState<IAPIPrograms[]>([])
     const [hascurriculum, setCurriculum] = useState<string>('')
+    const [dialogState, setDialogState] = useState({
+        show: false,
+        title: '',
+        description: '',
+        success: false
+    })
+
+    const handleFetchProgramsAdded = (e: IAPIPrograms[]) => {
+        setPrograms(e)
+    }
+
+    const { mutateAsync: addcurriculum, isPending: addcurriculumLoading } = useMutation({
+        mutationFn: API_PROGRAM_ADD_NEW_CURRICULUM,
+        onSuccess: async (data) => {
+            if (!data.success) {
+                setDialogState({ success: false, show: true, title: 'Uh, oh! Something went wrong.', description: data.message })
+            } else {
+                await queryClient.invalidateQueries({ queryKey: ['curriculums'] })
+                await queryClient.refetchQueries({ queryKey: ['curriculums'] })
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                })
+                setDialogState({ success: true, show: true, title: data.message, description: 'Do you want to continue creating program?' })
+                setReset(true)
+                setCurriculum('')
+
+            }
+        },
+        onError: (data) => {
+            setDialogState({ success: false, show: true, title: 'Uh, oh! Something went wrong.', description: data.message })
+        }
+    })
+
+    const handleSubmit = async () => {
+        if (programs.length === 0 || hascurriculum === '') return setDialogState({ success: false, show: true, title: 'Uh, oh! Something went wrong.', description: 'Please fill-in the required fields.' })
+        await addcurriculum({ name: hascurriculum, programs })
+    }
+
+    const isLoading = addcurriculumLoading
+
     return (
         <>
             <div className="flex flex-col min-h-screen items-center">
+                {isLoading && <Loading />}
+                <ContinueDialog
+                    icon={dialogState.success ? <CircleCheck color="#42a626" size={70} /> : <CircleX color="#880808" size={70} />}
+                    trigger={dialogState.show}
+                    title={dialogState.title}
+                    description={dialogState.description}
+                    onClose={() => { setDialogState(prev => ({ ...prev, show: false })) }}
+                />
                 <div className="w-full max-w-[90rem] flex flex-col pb-[20rem]">
                     <aside className="px-4 pb-4 pt-[8rem]">
                         <HeadSection>
@@ -40,6 +96,7 @@ export default function CreateCurriculum() {
                                     <div className="flex flex-col gap-1">
                                         <h1 className='text-[.83rem]'>Curriculum Name</h1>
                                         <Input
+                                            disabled={isLoading}
                                             value={hascurriculum}
                                             type='text'
                                             placeholder='eg. New Curriculum 2020'
@@ -47,11 +104,21 @@ export default function CreateCurriculum() {
                                             required
                                         />
                                     </div>
-                                    <DataTableCreateProgramInCurriculum columns={CreateProgramInCurriculumColumns} data={programs || []} fetchAddedPrograms={(e) => setPrograms(e)} />
+                                    <DataTableCreateProgramInCurriculum
+                                        columns={CreateProgramInCurriculumColumns}
+                                        data={programs || []}
+                                        fetchAddedPrograms={handleFetchProgramsAdded}
+                                        isreset={isreset}
+                                    />
                                 </div>
-                                <Button variant={`default`} size={`sm`} className='my-3 py-5'>
-                                    Create New Curriculum
-                                </Button>
+                                <AlertDialogConfirmation
+                                    className='w-full my-3 py-5'
+                                    variant={'default'}
+                                    btnTitle="Create New Curriculum"
+                                    title="Are you sure?"
+                                    description={`This will create a new curriculum.`}
+                                    btnContinue={handleSubmit}
+                                />
                             </div>
 
 
