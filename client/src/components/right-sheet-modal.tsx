b16392decb999ccbe5bcc861df42764301222f7f
sdevmarc <suanieeee@yahoo.com>
2024-10-18 14:03:08 +0800

    Refactor RightSheetModal component to add a right sheet modal for displaying content

---
+++ client/src/components/right-sheet-modal.tsx
@@ -0,0 +1,45 @@
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+} from "@/components/ui/sheet"
+import { cn } from "@/lib/utils"
+import { useEffect, useState } from "react"
+
+interface RightModalProps {
+    isOpen: boolean
+    onOpenChange: (e: boolean) => void
+    title: string
+    description: string
+    content: React.ReactNode
+    className?: string
+}
+
+export function RightSheetModal({ isOpen, onOpenChange, title, description, content, className }: RightModalProps) {
+    const [open, setOpen] = useState<boolean>(isOpen)
+
+    useEffect(() => {
+        setOpen(isOpen)
+    }, [isOpen])
+
+    const handleOpenChange = (newOpen: boolean) => {
+        setOpen(newOpen)
+        onOpenChange(newOpen)
+    }
+
+    return (
+        <Sheet open={open} onOpenChange={handleOpenChange}>
+            <SheetContent className={cn("sm:max-w-none", className)}>
+                <SheetHeader>
+                    <SheetTitle>{title}</SheetTitle>
+                    <SheetDescription>
+                        {description}
+                    </SheetDescription>
+                </SheetHeader>
+                {content}
+            </SheetContent>
+        </Sheet>
+    )
+}
