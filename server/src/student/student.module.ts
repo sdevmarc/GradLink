5b6938ec6c42a1d85ef0f290580b320c4f0d9bb7
sdevmarc <suanieeee@yahoo.com>
2024-10-20 23:38:17 +0800

    Add Semester and Curriculum dependencies to StudentModule

--- server/src/student/student.module.ts
+++ server/src/student/student.module.ts
@@ -6,15 +6,21 @@ import { StudentSchema } from './student.schema';
 import { FormsService } from 'src/forms/forms.service';
 import { FormSchema } from 'src/forms/forms.schema';
 import { ConstantsService } from 'src/constants/constants.service';
+import { SemesterSchema } from 'src/semester/semester.schema';
+import { SemesterService } from 'src/semester/semester.service';
+import { CurriculumSchema } from 'src/curriculum/curriculum.schema';
+import { CurriculumService } from 'src/curriculum/curriculum.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: 'Student', schema: StudentSchema },
-      { name: 'Form', schema: FormSchema }
+      { name: 'Form', schema: FormSchema },
+      { name: 'Semester', schema: SemesterSchema },
+      { name: 'Curriculum', schema: CurriculumSchema }
     ])
   ],
-  providers: [StudentService, FormsService, ConstantsService],
+  providers: [StudentService, FormsService, ConstantsService, SemesterService, CurriculumService],
   controllers: [StudentController]
 })
 export class StudentModule { }
