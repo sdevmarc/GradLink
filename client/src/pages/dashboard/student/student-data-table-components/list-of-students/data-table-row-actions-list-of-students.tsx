e1477ff45d52d43a0aa8d68d3f79a8b22bea4919
sdevmarc <suanieeee@yahoo.com>
2024-10-21 00:23:56 +0800

    Refactor student alumni data table row actions to use separate component and add course details modal: Update student data table row actions to use a separate component and add a course details modal for viewing curriculum details.

--- client/src/pages/dashboard/student/student-data-table-components/list-of-students/data-table-row-actions-list-of-students.tsx
+++ client/src/pages/dashboard/student/student-data-table-components/list-of-students/data-table-row-actions-list-of-students.tsx
@@ -10,35 +10,51 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
-import { useNavigate } from "react-router-dom";
+import { RightSheetModal } from "@/components/right-sheet-modal";
+import { useState } from "react";
 
 interface DataTableRowActionsProps<TData> {
     row: Row<TData>;
 }
 
 export function DataTableRowActionsListOfStudent<TData>({ row }: DataTableRowActionsProps<TData>) {
-    const navigate = useNavigate()
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+
     const handleViewDetails = () => {
-        // console.log(row.original)
-        const { _id: id } = row.original as { _id: string }
-        const encoded_id = btoa(id)
-        navigate(`/student/details/${encoded_id}`)
+        setIsOpen(true)
+    }
+
+    const handleOpenChange = (open: boolean) => {
+        setIsOpen(open)
     }
 
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button
-                    variant="ghost"
-                    className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-                >
-                    <DotsHorizontalIcon className="h-4 w-4" />
-                    <span className="sr-only">Open menu</span>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="w-[160px]">
-                <DropdownMenuItem onClick={handleViewDetails}>View details</DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button
+                        variant="ghost"
+                        className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+                    >
+                        <DotsHorizontalIcon className="h-4 w-4" />
+                        <span className="sr-only">Open menu</span>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="w-[160px]">
+                    <DropdownMenuItem onClick={handleViewDetails}>View Details</DropdownMenuItem>
+                    <DropdownMenuItem>View Enrollees</DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+
+            <RightSheetModal
+                className="w-[60%]"
+                isOpen={isOpen}
+                onOpenChange={handleOpenChange}
+                title="Curriculum Details"
+                description="View details of the selected curriculum."
+                content={''}
+            />
+        </>
+
     );
 }
