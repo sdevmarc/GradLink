651702f4f7a6d1ecc586fd856a0dc98184c47efc
sdevmarc <suanieeee@yahoo.com>
2024-10-20 23:36:15 +0800

    Refactor DataTableToolbarAvailablePrograms component to add confirmation dialogs for creating a new program and exporting data

--- client/src/pages/dashboard/program/program-data-table-components/program/available-programs/data-table-toolbar-available-programs.tsx
+++ client/src/pages/dashboard/program/program-data-table-components/program/available-programs/data-table-toolbar-available-programs.tsx
@@ -64,6 +64,7 @@ export function DataTableToolbarAvailablePrograms<TData>({
             </div>
             <div className="flex gap-2 items-center">
                 <AlertDialogConfirmation
+                    type={`default`}
                     variant={'outline'}
                     btnTitle="New Program"
                     title="Are you sure?"
@@ -71,6 +72,7 @@ export function DataTableToolbarAvailablePrograms<TData>({
                     btnContinue={() => navigate(ROUTES.CREATE_PROGRAM)}
                 />
                 <AlertDialogConfirmation
+                    type={`default`}
                     variant={'outline'}
                     btnTitle="Export"
                     title="Are you sure?"
