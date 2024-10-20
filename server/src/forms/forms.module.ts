2eedb8dd4aae5335991c3f7d9a2acf1b8d982bc5
sdevmarc <suanieeee@yahoo.com>
2024-10-20 23:37:24 +0800

    Add SemesterSchema to forms.module.ts

--- server/src/forms/forms.module.ts
+++ server/src/forms/forms.module.ts
@@ -6,11 +6,15 @@ import { FormSchema } from './forms.schema';
 import { ConstantsService } from 'src/constants/constants.service';
 import { StudentSchema } from 'src/student/student.schema';
 import { StudentService } from 'src/student/student.service';
+import { SemesterService } from 'src/semester/semester.service';
+import { CurriculumSchema } from 'src/curriculum/curriculum.schema';
+import { SemesterSchema } from 'src/semester/semester.schema';
 
 @Module({
     imports: [MongooseModule.forFeature([
         { name: 'Form', schema: FormSchema },
         { name: 'Student', schema: StudentSchema },
+        { name: 'Semester', schema: SemesterSchema },
     ])],
     providers: [
         FormsService,
