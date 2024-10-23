da7313a0b5d05dd14e668df7895c16241dd9481d
sdevmarc <suanieeee@yahoo.com>
2024-10-23 17:43:58 +0800

    Refactor SelectProgramInCurriculumColumns to add "Code" column

---
+++ client/src/pages/dashboard/program/program-data-table-components/curriculum/create-curriculum/programs/columns-select-programs-in-curriculum.tsx
@@ -0,0 +1,77 @@
+"use client"
+
+import {
+    ColumnDef,
+} from "@tanstack/react-table"
+
+import { IAPICourse } from '@/interface/course.interface'
+import { DataTableColumnHeader } from "@/components/data-table-components/data-table-column-header"
+import { Checkbox } from "@/components/ui/checkbox"
+import { DataTableRowActionsSelectProgramInCurriculum } from "./data-table-row-actions-programs-in-curriculum"
+
+export const SelectProgramInCurriculumColumns: ColumnDef<IAPICourse>[] = [
+    {
+        id: "select",
+        header: () => null, // Remove header checkbox since we only want single select
+        cell: ({ row, table }) => (
+            <Checkbox
+                checked={row.getIsSelected()}
+                onCheckedChange={(value) => {
+                    // Deselect all rows first
+                    table.toggleAllRowsSelected(false)
+                    // Then select only the current row
+                    row.toggleSelected(!!value)
+                }}
+                aria-label="Select row"
+            />
+        ),
+        enableSorting: false,
+        enableHiding: false,
+    },
+    {
+        accessorKey: "code",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Code" className="text-text" />
+        ),
+        cell: ({ row }) => (
+            <div className="w-[2rem] capitalize">{row.getValue("code")}</div>
+        ),
+        enableSorting: false,
+        enableHiding: false
+    },
+    {
+        accessorKey: "descriptiveTitle",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Descriptive Title" className="text-text" />
+        ),
+        cell: ({ row }) => {
+            return (
+                <div className="flex space-x-2">
+                    <span className="max-w-[500px] truncate capitalize font-medium">
+                        {row.getValue("descriptiveTitle")}
+                    </span>
+                </div>
+            )
+        }
+    },
+    {
+        accessorKey: "residency",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Residency" className="text-text" />
+        ),
+        cell: ({ row }) => {
+            return (
+                <div className="flex w-[100px] items-center">
+                    <span className="capitalize"> {row.getValue("residency")}</span>
+                </div>
+            )
+        },
+        filterFn: (row, id, value) => {
+            return value.includes(row.getValue(id))
+        }
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => <DataTableRowActionsSelectProgramInCurriculum row={row} />
+    }
+]
