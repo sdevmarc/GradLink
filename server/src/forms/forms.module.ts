4a2d2d9392c4a144b1d9db5de08ab95c8f0b8d8e
sdevmarc <suanieeee@yahoo.com>
2024-10-23 17:46:27 +0800

    Refactor forms.module.ts to remove unused imports and commented out code

--- server/src/forms/forms.module.ts
+++ server/src/forms/forms.module.ts
@@ -1,20 +1,16 @@
-import { Module } from '@nestjs/common';
-import { FormsService } from './forms.service';
-import { FormsController } from './forms.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { FormSchema } from './forms.schema';
-import { ConstantsService } from 'src/constants/constants.service';
-import { StudentSchema } from 'src/student/student.schema';
-import { StudentService } from 'src/student/student.service';
-import { SemesterService } from 'src/semester/semester.service';
-import { CurriculumSchema } from 'src/curriculum/curriculum.schema';
-import { SemesterSchema } from 'src/semester/semester.schema';
+import { Module } from '@nestjs/common'
+import { FormsService } from './forms.service'
+import { FormsController } from './forms.controller'
+import { MongooseModule } from '@nestjs/mongoose'
+import { FormSchema } from './forms.schema'
+import { ConstantsService } from 'src/constants/constants.service'
+import { StudentSchema } from 'src/student/student.schema'
+import { StudentService } from 'src/student/student.service'
 
 @Module({
     imports: [MongooseModule.forFeature([
         { name: 'Form', schema: FormSchema },
         { name: 'Student', schema: StudentSchema },
-        { name: 'Semester', schema: SemesterSchema },
     ])],
     providers: [
         FormsService,
