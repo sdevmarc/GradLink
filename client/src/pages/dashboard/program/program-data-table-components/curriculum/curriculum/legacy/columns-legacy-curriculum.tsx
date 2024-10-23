9ebcb30c5b9f596d646961c3a341157a8612d178
sdevmarc <suanieeee@yahoo.com>
2024-10-23 17:45:02 +0800

    Refactor LegacyCurriculumColumns to add "Code" column

---
+++ client/src/pages/dashboard/program/program-data-table-components/curriculum/curriculum/legacy/columns-legacy-curriculum.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import {
+    ColumnDef,
+} from "@tanstack/react-table"
+
+import { IAPICourse } from '@/interface/course.interface'
+import { DataTableColumnHeader } from "@/components/data-table-components/data-table-column-header"
+import { Badge } from "@/components/ui/badge"
+import { DataTableRowActionsLegacyCurriculum } from "./data-table-row-actions-legacy-curriculum"
+
+export const LegacyCurriculumColumns: ColumnDef<IAPICourse>[] = [
+    {
+        accessorKey: "name",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Name" className="text-text" />
+        ),
+        cell: ({ row }) => (
+            <div className="w-full capitalize">{row.getValue("name")}</div>
+        ),
+        enableSorting: false,
+        enableHiding: false
+    },
+    {
+        accessorKey: "program",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Program" className="text-text" />
+        ),
+        cell: ({ row }) => (
+            <div className="w-full capitalize">{row.getValue("program")}</div>
+        )
+    },
+    {
+        accessorKey: "isActive",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Status" className="text-text" />
+        ),
+        cell: ({ row }) => {
+            return (
+                <div className="flex space-x-2">
+                    <span className="max-w-[500px] truncate capitalize font-medium">
+                        <Badge variant={row.getValue("isActive") === true ? "default" : "destructive"}>
+                            {row.getValue("isActive") === true ? 'Active' : 'Legacy'}
+                        </Badge>
+                    </span>
+                </div>
+            )
+        }
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => <DataTableRowActionsLegacyCurriculum row={row} />
+    }
+]
