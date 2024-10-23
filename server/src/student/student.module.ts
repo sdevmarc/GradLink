f043feaec8fe91b571d484fe60710aaff72a7095
sdevmarc <suanieeee@yahoo.com>
2024-10-23 17:47:37 +0800

    Refactor student.module.ts: remove unused dependencies and update imports

--- server/src/student/student.module.ts
+++ server/src/student/student.module.ts
@@ -1,26 +1,25 @@
-import { Module } from '@nestjs/common';
-import { StudentService } from './student.service';
-import { StudentController } from './student.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { StudentSchema } from './student.schema';
-import { FormsService } from 'src/forms/forms.service';
-import { FormSchema } from 'src/forms/forms.schema';
-import { ConstantsService } from 'src/constants/constants.service';
-import { SemesterSchema } from 'src/semester/semester.schema';
-import { SemesterService } from 'src/semester/semester.service';
-import { CurriculumSchema } from 'src/curriculum/curriculum.schema';
-import { CurriculumService } from 'src/curriculum/curriculum.service';
+import { Module } from '@nestjs/common'
+import { StudentService } from './student.service'
+import { StudentController } from './student.controller'
+import { MongooseModule } from '@nestjs/mongoose'
+import { StudentSchema } from './student.schema'
+import { FormsService } from 'src/forms/forms.service'
+import { FormSchema } from 'src/forms/forms.schema'
+import { ConstantsService } from 'src/constants/constants.service'
+import { CurriculumSchema } from 'src/curriculum/curriculum.schema'
+import { CurriculumService } from 'src/curriculum/curriculum.service'
+import { CoursesSchema } from 'src/courses/courses.schema'
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: 'Student', schema: StudentSchema },
       { name: 'Form', schema: FormSchema },
-      { name: 'Semester', schema: SemesterSchema },
-      { name: 'Curriculum', schema: CurriculumSchema }
+      { name: 'Curriculum', schema: CurriculumSchema },
+      { name: 'Course', schema: CoursesSchema },
     ])
   ],
-  providers: [StudentService, FormsService, ConstantsService, SemesterService, CurriculumService],
+  providers: [StudentService, FormsService, ConstantsService, CurriculumService],
   controllers: [StudentController]
 })
 export class StudentModule { }
