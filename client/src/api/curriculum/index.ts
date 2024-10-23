16feb086a518e487d6e3023ae7594dcd11a5b44d
sdevmarc <suanieeee@yahoo.com>
2024-10-23 17:42:12 +0800

    Refactor API_CURRICULUM_FINDALL function to separate active and legacy curriculum retrieval

--- client/src/api/curriculum/index.ts
+++ client/src/api/curriculum/index.ts
@@ -2,9 +2,18 @@ import { HOST } from '@/constants'
 import { IRequestCurriculum } from '@/interface/curriculum.interface'
 import axios from 'axios'
 
-export const API_CURRICULUM_FINDALL = async () => {
+export const API_CURRICULUM_FINDALL_ACTIVE = async () => {
     try {
-        const response = await axios.get(`${HOST}/curriculum`)
+        const response = await axios.get(`${HOST}/curriculum/active`)
+        return response.data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const API_CURRICULUM_FINDALL_LEGACY = async () => {
+    try {
+        const response = await axios.get(`${HOST}/curriculum/legacy`)
         return response.data
     } catch (error) {
         console.error(error)
