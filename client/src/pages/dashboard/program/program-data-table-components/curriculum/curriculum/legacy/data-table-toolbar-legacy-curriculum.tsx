a0cc12aff4eafd246ecffa5cb21a702a7e0d468d
sdevmarc <suanieeee@yahoo.com>
2024-10-23 17:45:17 +0800

    Refactor DataTableToolbarLegacyCurriculum component to add search functionality and reset button

---
+++ client/src/pages/dashboard/program/program-data-table-components/curriculum/curriculum/legacy/data-table-toolbar-legacy-curriculum.tsx
@@ -0,0 +1,86 @@
+"use client"
+
+import { Cross2Icon } from "@radix-ui/react-icons"
+import { Table } from "@tanstack/react-table"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { DataTableViewOptions } from "@/components/data-table-components/data-table-view-options"
+import { AlertDialogConfirmation } from "@/components/alert-dialog"
+import { useNavigate } from "react-router-dom"
+import { ROUTES } from "@/constants"
+
+interface DataTableToolbarProps<TData> {
+    table: Table<TData>
+}
+
+export function DataTableToolbarLegacyCurriculum<TData>({
+    table,
+}: DataTableToolbarProps<TData>) {
+    const isFiltered = table.getState().columnFilters.length > 0;
+    const navigate = useNavigate()
+
+    return (
+        <div className="flex flex-wrap items-center justify-between">
+            <div className="flex flex-1 flex-wrap items-center gap-2">
+                <Input
+                    placeholder="Search name..."
+                    value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+                    onChange={(event) => {
+                        table.getColumn("name")?.setFilterValue(event.target.value);
+                    }}
+                    className="h-8 w-[20rem] lg:w-[25rem] placeholder:text-muted"
+                />
+                {/* {table.getColumn("category") && (
+                    <DataTableFacetedFilter
+                        column={table.getColumn("category")}
+                        title="Categoryasdasd"
+                        options={categories}
+                    />
+                )}
+                {table.getColumn("type") && (
+                    <DataTableFacetedFilter
+                        column={table.getColumn("type")}
+                        title="Type"
+                        options={incomeType}
+                    />
+                )} */}
+                {isFiltered && (
+                    <Button
+                        variant="ghost"
+                        onClick={() => table.resetColumnFilters()}
+                        className="h-8 px-2 lg:px-3"
+                    >
+                        Reset
+                        <Cross2Icon className="ml-2 h-4 w-4" />
+                    </Button>
+                )}
+                {/* <CalendarDatePicker
+                    date={dateRange}
+                    onDateSelect={handleDateSelect}
+                    className="w-[250px] h-8"
+                    variant="outline"
+                /> */}
+            </div>
+            <div className="flex gap-2 items-center">
+                <AlertDialogConfirmation
+                    type={`default`}
+                    variant={'outline'}
+                    btnTitle="New Curriculum"
+                    title="Are you sure?"
+                    description={`You will be redirect to a page to create new curriculum.`}
+                    btnContinue={() => navigate(ROUTES.CREATE_CURRICULUM)}
+                />
+                <AlertDialogConfirmation
+                    type={`default`}
+                    variant={'outline'}
+                    btnTitle="Export"
+                    title="Are you sure?"
+                    description={`This will export the current data you are viewing.`}
+                    btnContinue={() => navigate('/program')}
+                />
+                <DataTableViewOptions table={table} />
+            </div>
+        </div>
+    )
+}
