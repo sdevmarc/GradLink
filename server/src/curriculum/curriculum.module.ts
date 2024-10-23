6aea7243571f266d55f11a60c0d71b2a0cdba5a7
sdevmarc <suanieeee@yahoo.com>
2024-10-23 17:46:07 +0800

    Refactor curriculum.module.ts to import CoursesSchema for the 'Course' model

--- server/src/curriculum/curriculum.module.ts
+++ server/src/curriculum/curriculum.module.ts
@@ -3,11 +3,13 @@ import { CurriculumController } from './curriculum.controller';
 import { CurriculumService } from './curriculum.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CurriculumSchema } from './curriculum.schema';
+import { CoursesSchema } from 'src/courses/courses.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: 'Curriculum', schema: CurriculumSchema }
+      { name: 'Curriculum', schema: CurriculumSchema },
+      { name: 'Course', schema: CoursesSchema },
     ])
   ],
   controllers: [CurriculumController],
