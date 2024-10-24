8001ffe223d573e3fac9a1ba0da018c7b952a2f5
sdevmarc <suanieeee@yahoo.com>
2024-10-24 14:05:10 +0800

    Refactor enrollment page: Add component for viewing courses offered in enrollment

---
+++ client/src/pages/dashboard/enrollment/enrollment-data-table-components/view-courses-offered/data-table-row-actions-enrollment.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { DotsHorizontalIcon } from "@radix-ui/react-icons";
+import { Row } from "@tanstack/react-table";
+
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import { RightSheetModal } from "@/components/right-sheet-modal";
+import { useState } from "react";
+
+interface DataTableRowActionsProps<TData> {
+    row: Row<TData>;
+}
+
+export function DataTableRowActionsCoursesOfferedInEnrollment<TData>({ row }: DataTableRowActionsProps<TData>) {
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+
+    const handleViewDetails = () => {
+        setIsOpen(true)
+    }
+
+    const handleOpenChange = (open: boolean) => {
+        setIsOpen(open)
+    }
+
+    return (
+        <>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button
+                        variant="ghost"
+                        className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+                    >
+                        <DotsHorizontalIcon className="h-4 w-4" />
+                        <span className="sr-only">Open menu</span>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="w-[160px]">
+                    <DropdownMenuItem onClick={handleViewDetails}>View Details</DropdownMenuItem>
+                    <DropdownMenuItem>View Enrollees</DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+
+            <RightSheetModal
+                className="w-[60%]"
+                isOpen={isOpen}
+                onOpenChange={handleOpenChange}
+                title="Student Details"
+                description="View details of the selected student."
+                content={''}
+            />
+        </>
+
+    );
+}
