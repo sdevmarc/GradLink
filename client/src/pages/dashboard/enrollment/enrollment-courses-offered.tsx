c0cff6d34f5b9c0e70ca63d7080f0a83254d0abd
sdevmarc <suanieeee@yahoo.com>
2024-10-26 00:34:45 +0800

    Refactor: Update enrollment courses offered page

--- client/src/pages/dashboard/enrollment/enrollment-courses-offered.tsx
+++ client/src/pages/dashboard/enrollment/enrollment-courses-offered.tsx
@@ -4,8 +4,14 @@ import { Sidebar, SidebarNavs } from "@/components/sidebar"
 import { ROUTES } from "@/constants"
 import { DataTableCoursesOfferedInEnrollment } from "./enrollment-data-table-components/view-courses-offered/data-table-courses-offered-in-enrollment"
 import { CoursesOfferedInEnrollmentColumns } from "./enrollment-data-table-components/view-courses-offered/columns-courses-offered-in-enrollment"
+import { useQuery } from "@tanstack/react-query"
+import { API_COURSE_FINDALL_COURSES_OFFERED } from "@/api/courses"
 
 export default function Enrollment() {
+    const { data: courses, isLoading: coursesLoading, isFetched: coursesFetched } = useQuery({
+        queryFn: () => API_COURSE_FINDALL_COURSES_OFFERED(),
+        queryKey: ['courses-offered']
+    })
 
     return (
         <div className="flex flex-col min-h-screen items-center">
@@ -24,7 +30,11 @@ export default function Enrollment() {
                         <SidebarNavs title="Current Enrolled" link={ROUTES.CURRENTLY_ENROLLED} />
                     </Sidebar>
                     <MainTable>
-                        <DataTableCoursesOfferedInEnrollment columns={CoursesOfferedInEnrollmentColumns} data={[]} />
+                        {coursesLoading && <div>Loading...</div>}
+                        {
+                            (!coursesLoading && coursesFetched) &&
+                            <DataTableCoursesOfferedInEnrollment columns={CoursesOfferedInEnrollmentColumns} data={courses?.data || []} />
+                        }
                     </MainTable>
                 </main>
             </div>
