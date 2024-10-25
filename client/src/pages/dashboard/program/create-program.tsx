1671e30d130623e73e15256fe69b9fb86ac0c2fd
sdevmarc <suanieeee@yahoo.com>
2024-10-25 12:52:49 +0800

    Refactor create-program.tsx: Update program creation form

--- client/src/pages/dashboard/program/create-program.tsx
+++ client/src/pages/dashboard/program/create-program.tsx
@@ -2,23 +2,20 @@ import HeadSection, { BackHeadSection, SubHeadSectionDetails } from '@/component
 import { Sidebar, SidebarNavs } from '@/components/sidebar'
 import { ROUTES } from '@/constants'
 import MainTable from '@/components/main-table'
-import { CreateProgramColumns } from './program-data-table-components/program/create-program/columns-create-program'
-import { DataTableCreateProgram } from './program-data-table-components/program/create-program/data-table-create-program'
 import { useState } from 'react'
-import { IAPIPrograms } from '@/interface/program.interface'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { CircleCheck, CircleX } from 'lucide-react'
-import { API_PROGRAM_ADD_PROGRAM } from '@/api/program'
 import { AlertDialogConfirmation } from '@/components/alert-dialog'
-import { toast } from "sonner"
 import Loading from '@/components/loading'
 import { useNavigate } from 'react-router-dom'
+import { Input } from '@/components/ui/input'
+import { IAPIPrograms } from '@/interface/program.interface'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { API_PROGRAM_NEW_PROGRAM } from '@/api/program'
 
 export default function CreateProgram() {
     const queryClient = useQueryClient()
     const navigate = useNavigate()
-    const [isreset, setReset] = useState<boolean>(false)
-    const [programs, setPrograms] = useState<IAPIPrograms[]>([])
+    const [isvalid, setValid] = useState<boolean>(false)
     const [dialogsubmit, setDialogSubmit] = useState<boolean>(false)
     const [alertdialogstate, setAlertDialogState] = useState({
         show: false,
@@ -26,18 +23,26 @@ export default function CreateProgram() {
         description: '',
         success: false
     })
+    const [values, setValues] = useState<IAPIPrograms>({
+        code: '',
+        descriptiveTitle: '',
+        residency: ''
+    })
 
-    const fetchProgramsAdded = (e: IAPIPrograms[]) => {
-        setPrograms(e)
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target
+        setValues(prev => ({
+            ...prev,
+            [name]: value
+        }))
     }
 
     const { mutateAsync: addprogram, isPending: addprogramLoading } = useMutation({
-        mutationFn: API_PROGRAM_ADD_PROGRAM,
+        mutationFn: API_PROGRAM_NEW_PROGRAM,
         onSuccess: async (data) => {
             if (!data.success) {
                 setDialogSubmit(false)
                 setAlertDialogState({ success: false, show: true, title: "Uh, oh. Something went wrong!", description: data.message })
-                toast("Uh, oh. Something went wrong!", { description: data.message })
                 return
             } else {
                 await queryClient.invalidateQueries({ queryKey: ['programs'] })
@@ -46,26 +51,38 @@ export default function CreateProgram() {
                     top: 0,
                     behavior: 'smooth'
                 })
-                setAlertDialogState({ success: true, show: true, title: "Yay, success! 🎉", description: data.message })
-                setReset(true)
+                setValid(true)
                 setDialogSubmit(false)
-                toast("Yay, success! 🎉", { description: data.message })
+                setAlertDialogState({ success: true, show: true, title: "Yay, success! 🎉", description: data.message })
+                setValues({ code: '', descriptiveTitle: '', residency: '' })
                 return
             }
         },
         onError: (data) => {
+            setDialogSubmit(false)
             setAlertDialogState({ success: false, show: true, title: 'Uh, oh! Something went wrong.', description: data.message })
         }
     })
 
     const handleSubmit = async () => {
-        if (programs.length === 0) {
+        const { code, descriptiveTitle, residency } = values
+        const upperCode = (code ?? '').replace(/\s+/g, '').toUpperCase()
+        const noSpaceResidency = (residency ?? '').replace(/\s+/g, '').toUpperCase()
+
+        if (upperCode === '' || !descriptiveTitle || noSpaceResidency === '') {
             setDialogSubmit(false)
-            setAlertDialogState({ success: false, show: true, title: 'Uh, oh! Something went wrong.', description: 'Please add at least one program to submit.' })
+            setValid(false)
+            setAlertDialogState({ success: false, show: true, title: 'Uh, oh! Something went wrong.', description: 'Please fill-up the required fields.' })
             return
         }
-        await addprogram({ programs })
-        setDialogSubmit(false)
+
+        if (isNaN(Number(residency))) {
+            setDialogSubmit(false)
+            setValid(false)
+            setAlertDialogState({ success: false, show: true, title: 'Uh, oh! Something went wrong.', description: 'Residency should be a number.' })
+            return
+        }
+        await addprogram({ code: upperCode, descriptiveTitle, residency: noSpaceResidency })
     }
 
     const isLoading = addprogramLoading
@@ -84,10 +101,9 @@ export default function CreateProgram() {
                 variant={`default`}
                 btnContinue={() => {
                     setAlertDialogState(prev => ({ ...prev, show: false }))
-                    navigate(-1)
+                    if (isvalid) return navigate(-1)
                 }}
             />
-
 
             <div className="flex flex-col min-h-screen items-center">
                 {isLoading && <Loading />}
@@ -112,31 +128,64 @@ export default function CreateProgram() {
                                 <div className="w-full px-4 py-3 border-b">
                                     <h1 className='text-text font-semibold text-lg'>Configuration</h1>
                                 </div>
-                                <div className="px-4">
-                                    <DataTableCreateProgram
-                                        columns={CreateProgramColumns}
-                                        data={programs || []}
-                                        fetchAddedPrograms={fetchProgramsAdded}
-                                        isreset={isreset}
-                                    />
-                                    <AlertDialogConfirmation
-                                        isDialog={dialogsubmit}
-                                        setDialog={(open) => setDialogSubmit(open)}
-                                        type={`default`}
-                                        disabled={isLoading}
-                                        className='w-full my-3 py-5'
-                                        variant={'default'}
-                                        btnTitle="Add Program to Current Curriculum"
-                                        title="Are you sure?"
-                                        description={`This will add new programs to the current curriculum.`}
-                                        btnContinue={handleSubmit}
-                                    />
+                                <div className="w-full py-2 flex flex-col justify-between">
+                                    <div className="w-full flex flex-col gap-4">
+                                        <div className="flex flex-col px-4 gap-1">
+                                            <h1 className='text-[.83rem]'>Code</h1>
+                                            <Input
+                                                value={values.code}
+                                                disabled={isLoading}
+                                                name='code'
+                                                type='text'
+                                                placeholder='eg. MLIS'
+                                                onChange={handleOnChange}
+                                                required
+                                            />
+                                        </div>
+                                        <div className="flex flex-col px-4 gap-1">
+                                            <h1 className='text-[.83rem]'>Descriptive Title</h1>
+                                            <Input
+                                                value={values.descriptiveTitle}
+                                                disabled={isLoading}
+                                                name='descriptiveTitle'
+                                                type='text'
+                                                placeholder='eg. Library Information System'
+                                                onChange={handleOnChange}
+                                                required
+                                            />
+                                        </div>
+                                        <div className="flex flex-col px-4 gap-1">
+                                            <h1 className='text-[.83rem]'>Residency</h1>
+                                            <Input
+                                                value={values.residency}
+                                                disabled={isLoading}
+                                                name='residency'
+                                                type='text'
+                                                placeholder='eg. 123'
+                                                onChange={handleOnChange}
+                                                required
+                                            />
+                                        </div>
+
+                                        <AlertDialogConfirmation
+                                            isDialog={dialogsubmit}
+                                            setDialog={(open) => setDialogSubmit(open)}
+                                            type={`default`}
+                                            disabled={isLoading}
+                                            className='w-full my-3 py-5'
+                                            variant={'default'}
+                                            btnTitle="Add Program to Current Curriculum"
+                                            title="Are you sure?"
+                                            description={`This will add new programs to the current curriculum.`}
+                                            btnContinue={handleSubmit}
+                                        />
+                                    </div>
                                 </div>
                             </div>
                         </MainTable>
                     </main>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
