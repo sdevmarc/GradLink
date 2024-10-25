ac590f552e9af3ad91d080f3752e9157cd734cf3
sdevmarc <suanieeee@yahoo.com>
2024-10-26 00:34:12 +0800

    Refactor: Update breadcrumb navigation for creating a new student

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -132,6 +132,28 @@ const HeaderDashboard = () => {
                                                 </BreadcrumbItem>
                                             </BreadCrumbs>
                                         )
+                                    case ROUTES.NEW_STUDENT:
+                                        return (
+                                            <BreadCrumbs>
+                                                <BreadcrumbItem>
+                                                    <BreadcrumbPage className='text-md'>LOGO</BreadcrumbPage>
+                                                </BreadcrumbItem>
+                                                <BreadcrumbSeparator>
+                                                    <Slash />
+                                                </BreadcrumbSeparator>
+                                                <BreadcrumbItem>
+                                                    <BreadcrumbItem className='text-md font-light'>Gradlink</BreadcrumbItem>
+                                                </BreadcrumbItem>
+                                                <BreadcrumbSeparator>
+                                                    <Slash />
+                                                </BreadcrumbSeparator>
+                                                <BreadcrumbItem>
+                                                    <BreadcrumbPage className='text-md font-medium'>
+                                                        Create a student
+                                                    </BreadcrumbPage>
+                                                </BreadcrumbItem>
+                                            </BreadCrumbs>
+                                        )
                                     case `/program/details/${id}`:
                                         return (
                                             <BreadCrumbs>
