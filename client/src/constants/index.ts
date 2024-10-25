ffee462cc372293340d129b1c4929704a7247e2c
sdevmarc <suanieeee@yahoo.com>
2024-10-26 00:34:16 +0800

    Refactor: Update routes for creating a new student and enrollment-related pages

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -6,18 +6,28 @@ export const ROUTES = {
     HOME: '/',
     LOGIN: '/login',
     OVERVIEW: '/overview',
+
+    //TRACER
     TRACER: '/tracer',
+
+    //ENROLLMENT
     ENROLLMENT: '/enrollment',
     CREATE_COURSE_OFFERED: '/enrollment/create-course-offered',
     CURRENTLY_ENROLLED: '/enrollment/current-enrolled',
+    NEW_STUDENT: '/enrollment/new-student',
+
+    //STUDENT
     LIST_OF_STUDENTS: '/student',
     ALUMNI_GRADUATES: '/student/alumni',
     CREATE_STUDENT: '/student/create',
+
+    //PROGRAM
     AVAILABLE_PROGRAMS: '/program',
     AVAILABLE_COURSES: '/program/courses',
     CREATE_PROGRAM: '/program/program/create',
     CREATE_COURSE: '/program/courses/create',
     CURRICULUM: '/program/curriculum',
     CREATE_CURRICULUM: '/program/curriculum/create',
+
     GOOGLE_FORM: '/form'
 }
