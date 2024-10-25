21b900e1f742418303db304fb8da8b7e14a35c39
sdevmarc <suanieeee@yahoo.com>
2024-10-25 14:26:15 +0800

    Refactor: Add current enrolled student page and table component

---
+++ client/src/pages/dashboard/enrollment/current-enrolled.tsx
@@ -0,0 +1,60 @@
+import MainTable from "@/components/main-table"
+import HeadSection, { SubHeadSectionDetails } from "@/components/head-section"
+import { Sidebar, SidebarNavs } from "@/components/sidebar"
+import { useQuery } from "@tanstack/react-query"
+import { API_STUDENT_FINDALL_ENROLLED } from "@/api/student"
+import { ROUTES } from "@/constants"
+import { useState } from "react"
+import { DataTableStudentCurrentEnrolled } from "./enrollment-data-table-components/current-enrolled/data-table-current-enrolled"
+import { StudentCurrentEnrolledColumns } from "./enrollment-data-table-components/current-enrolled/columns-current-enrolled"
+import { IAPIStudents } from "@/interface/student.interface"
+import Loading from "@/components/loading"
+
+export default function CurrentEnrolledStudent() {
+    const [resetSelection, setResetSelection] = useState(false)
+
+    const { data: students, isLoading: studentLoading, isFetched: studentFetched } = useQuery({
+        queryFn: () => API_STUDENT_FINDALL_ENROLLED(),
+        queryKey: ['students']
+    })
+
+    const handleStudentChange = (selectedStudent: IAPIStudents[]) => {
+        console.log(selectedStudent)
+    }
+
+    return (
+        <>
+            {
+                studentLoading ? <Loading />
+                    :
+                    <div className="flex flex-col min-h-screen items-center">
+                        <div className="w-full max-w-[90rem] flex flex-col">
+                            <aside className="px-4 pb-4 pt-[8rem]">
+                                <HeadSection>
+                                    <SubHeadSectionDetails
+                                        title="CURRENTLY ENROLLED STUDENTS"
+                                        description="Here's a list of currently enrolled students this semester."
+                                    />
+                                </HeadSection>
+                            </aside>
+                            <main className="flex">
+                                <Sidebar>
+                                    <SidebarNavs title="Courses Offered" link={ROUTES.ENROLLMENT} />
+                                    <SidebarNavs bg='bg-muted' title="Current Enrolled" link={ROUTES.CURRENTLY_ENROLLED} />
+                                </Sidebar>
+                                <MainTable>
+                                    {studentFetched && <DataTableStudentCurrentEnrolled
+                                        columns={StudentCurrentEnrolledColumns}
+                                        data={students.data || []}
+                                        fetchCheck={handleStudentChange}
+                                        resetSelection={resetSelection}
+                                        onResetComplete={() => setResetSelection(false)}
+                                    />}
+                                </MainTable>
+                            </main>
+                        </div>
+                    </div>
+            }
+        </>
+    )
+}
