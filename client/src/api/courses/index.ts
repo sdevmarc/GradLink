bf344eb2499e8175b2f49c0be07999ffbccd864b
sdevmarc <suanieeee@yahoo.com>
2024-10-26 00:33:58 +0800

    Refactor: Update API endpoints for course search and creation
    - Updated the API endpoints for course search and creation in the `client/src/api/courses/index.ts` file.
    - Added new API functions for retrieving all courses offered and all active courses in the curriculum.
    - Modified the API function for retrieving a single course to use the course number as a parameter.
    - Removed the API function for deleting a course, as it is no longer needed.

--- client/src/api/courses/index.ts
+++ client/src/api/courses/index.ts
@@ -1,10 +1,28 @@
 import { HOST } from '@/constants'
-import { IAPICourse } from '@/interface/course.interface'
+import { IAPICourse, IRequestUpdateCourse } from '@/interface/course.interface'
 import axios from 'axios'
 
 export const API_COURSE_FINDALL = async () => {
     try {
         const response = await axios.get(`${HOST}/courses`)
+        return response.data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const API_COURSE_FINDALL_COURSES_OFFERED = async () => {
+    try {
+        const response = await axios.get(`${HOST}/courses/courses-offered`)
+        return response.data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const API_COURSE_FINDALL_ACTIVE_IN_CURRICULUM = async () => {
+    try {
+        const response = await axios.get(`${HOST}/courses/courses-active-curriculum`)
         return response.data
     } catch (error) {
         console.error(error)
@@ -32,21 +50,12 @@ export const API_COURSE_CREATE = async ({ code, courseno, descriptiveTitle, unit
     }
 }
 
-export const API_COURSE_UPDATE = async ({ _id, courseno, descriptiveTitle, programs, units }: IAPICourse) => {
+export const API_COURSE_UPDATE_COURSES_OFFERED = async ({ id }: IRequestUpdateCourse) => {
     try {
-        const response = await axios.post(`${HOST}/courses/update`, {
-            _id, courseno, descriptiveTitle, programs, units
+        const response = await axios.post(`${HOST}/courses/update-courses-offered`, {
+            id
         })
 
-        return response.data
-    } catch (error) {
-        console.error(error)
-    }
-}
-
-export const API_COURSE_DELETE = async ({ _id }: IAPICourse) => {
-    try {
-        const response = await axios.get(`${HOST}/course/${_id}`)
         return response.data
     } catch (error) {
         console.error(error)
