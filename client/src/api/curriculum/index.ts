b73a93658e47bd557a822a07c41717869ea240ca
sdevmarc <suanieeee@yahoo.com>
2024-10-25 14:04:13 +0800

    Refactor curriculum API: Update findAll method

--- client/src/api/curriculum/index.ts
+++ client/src/api/curriculum/index.ts
@@ -2,23 +2,23 @@ import { HOST } from '@/constants'
 import { IRequestCurriculum } from '@/interface/curriculum.interface'
 import axios from 'axios'
 
-export const API_CURRICULUM_FINDALL_ACTIVE = async () => {
+export const API_CURRICULUM_FINDALL = async () => {
     try {
-        const response = await axios.get(`${HOST}/curriculum/active`)
+        const response = await axios.get(`${HOST}/curriculum`)
         return response.data
     } catch (error) {
         console.error(error)
     }
 }
 
-export const API_CURRICULUM_FINDALL_LEGACY = async () => {
-    try {
-        const response = await axios.get(`${HOST}/curriculum/legacy`)
-        return response.data
-    } catch (error) {
-        console.error(error)
-    }
-}
+// export const API_CURRICULUM_FINDALL_LEGACY = async () => {
+//     try {
+//         const response = await axios.get(`${HOST}/curriculum/legacy`)
+//         return response.data
+//     } catch (error) {
+//         console.error(error)
+//     }
+// }
 
 export const API_NEW_CURRICULUM = async ({ name, programCode, major, categories }: IRequestCurriculum) => {
     try {
