a951a56da06d03ca1fdbcaccd3b31664be90cac4
sdevmarc <suanieeee@yahoo.com>
2024-10-26 16:52:35 +0800

    Refactor: Update column name in CoursesOfferedInEnrollmentColumns component

--- client/src/pages/dashboard/program/available-courses.tsx
+++ client/src/pages/dashboard/program/available-courses.tsx
@@ -20,7 +20,7 @@ export default function Courses() {
                     <aside className="px-4 pb-4 pt-[8rem]">
                         <HeadSection>
                             <SubHeadSectionDetails
-                                title="REGISTERED COURSES"
+                                title="COURSES"
                                 description="Here's a list of registered courses."
                             />
                         </HeadSection>
