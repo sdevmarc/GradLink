119b24ba160bcc7017d4b729a9684b3184f8288c
sdevmarc <suanieeee@yahoo.com>
2024-10-26 16:52:59 +0800

    Refactor: Update column name in SelectCoursesInCurriculumColumns component

--- client/src/pages/dashboard/program/program-data-table-components/curriculum/create-curriculum/courses/columns-select-courses-in-curriculum.tsx
+++ client/src/pages/dashboard/program/program-data-table-components/curriculum/create-curriculum/courses/columns-select-courses-in-curriculum.tsx
@@ -62,7 +62,7 @@ export const SelectCoursesInCurriculumColumns: ColumnDef<IAPICourse>[] = [
         cell: ({ row }) => {
             return (
                 <div className="flex space-x-2">
-                    <span className="max-w-[500px] truncate capitalize font-medium">
+                    <span className="max-w-[500px] truncate normal-case">
                         {row.getValue("descriptiveTitle")}
                     </span>
                 </div>
