8f985a8f10a99a7f30cc36546b35674af3009264
sdevmarc <suanieeee@yahoo.com>
2024-10-26 16:51:24 +0800

    Refactor: Update header component to include new routes for student enrollees and list of enrolled students

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -12,12 +12,13 @@ export const ROUTES = {
 
     //ENROLLMENT
     ENROLLMENT: '/enrollment',
+    STUDENT_ENROLLEE: '/enrollment/student-enrollees',
     CREATE_COURSE_OFFERED: '/enrollment/create-course-offered',
     CURRENTLY_ENROLLED: '/enrollment/current-enrolled',
     NEW_STUDENT: '/enrollment/new-student',
 
     //STUDENT
-    LIST_OF_STUDENTS: '/student',
+    LIST_OF_ENROLLED_STUDENTS: '/student',
     ALUMNI_GRADUATES: '/student/alumni',
     CREATE_STUDENT: '/student/create',
 
