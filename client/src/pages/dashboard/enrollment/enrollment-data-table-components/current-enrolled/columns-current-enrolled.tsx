4e92d3387f9ebce320fd21b8666b5857cb255e74
sdevmarc <suanieeee@yahoo.com>
2024-10-27 19:42:16 +0800

    Refactor: Capitalize student names in current enrolled columns

--- client/src/pages/dashboard/enrollment/enrollment-data-table-components/current-enrolled/columns-current-enrolled.tsx
+++ client/src/pages/dashboard/enrollment/enrollment-data-table-components/current-enrolled/columns-current-enrolled.tsx
@@ -49,7 +49,7 @@ export const StudentCurrentEnrolledColumns: ColumnDef<IAPICourse>[] = [
         cell: ({ row }) => {
             return (
                 <div className="flex space-x-2">
-                    <span className="max-w-[400px] truncate normal-case">
+                    <span className="max-w-[400px] truncate capitalize">
                         {row.getValue("lastname")}
                     </span>
                 </div>
@@ -64,7 +64,7 @@ export const StudentCurrentEnrolledColumns: ColumnDef<IAPICourse>[] = [
         cell: ({ row }) => {
             return (
                 <div className="flex space-x-2">
-                    <span className="max-w-[400px] truncate normal-case">
+                    <span className="max-w-[400px] truncate capitalize">
                         {row.getValue("firstname")}
                     </span>
                 </div>
@@ -79,7 +79,7 @@ export const StudentCurrentEnrolledColumns: ColumnDef<IAPICourse>[] = [
         cell: ({ row }) => {
             return (
                 <div className="flex space-x-2">
-                    <span className="max-w-[400px] truncate normal-case">
+                    <span className="max-w-[400px] truncate capitalize">
                         {row.getValue("middlename")}
                     </span>
                 </div>
