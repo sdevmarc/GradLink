7e9e271cbceea5707ddadc2b34bab7010424b766
sdevmarc <suanieeee@yahoo.com>
2024-10-27 19:43:24 +0800

    Refactor: Add ProgramSchema to forms module imports

--- server/src/forms/forms.module.ts
+++ server/src/forms/forms.module.ts
@@ -7,12 +7,14 @@ import { ConstantsService } from 'src/constants/constants.service'
 import { StudentSchema } from 'src/student/student.schema'
 import { StudentService } from 'src/student/student.service'
 import { CoursesSchema } from 'src/courses/courses.schema'
+import { ProgramSchema } from 'src/programs/programs.schema'
 
 @Module({
     imports: [MongooseModule.forFeature([
         { name: 'Form', schema: FormSchema },
         { name: 'Student', schema: StudentSchema },
         { name: 'Course', schema: CoursesSchema },
+        { name: 'Program', schema: ProgramSchema },
     ])],
     providers: [
         FormsService,
