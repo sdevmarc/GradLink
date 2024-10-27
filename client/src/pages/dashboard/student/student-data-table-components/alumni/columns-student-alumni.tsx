f63c63ab8c860377b826e8fa8c46d88582cd5484
sdevmarc <suanieeee@yahoo.com>
2024-10-27 19:43:07 +0800

    Refactor: Capitalize student names in StudentAlumniColumns

--- client/src/pages/dashboard/student/student-data-table-components/alumni/columns-student-alumni.tsx
+++ client/src/pages/dashboard/student/student-data-table-components/alumni/columns-student-alumni.tsx
@@ -49,7 +49,7 @@ export const StudentAlumniColumns: ColumnDef<IAPICourse>[] = [
         cell: ({ row }) => {
             return (
                 <div className="flex space-x-2">
-                    <span className=" truncate normal-case">
+                    <span className=" truncate capitalize">
                         {row.getValue("lastname")}
                     </span>
                 </div>
@@ -64,7 +64,7 @@ export const StudentAlumniColumns: ColumnDef<IAPICourse>[] = [
         cell: ({ row }) => {
             return (
                 <div className="flex space-x-2">
-                    <span className=" truncate normal-case">
+                    <span className=" truncate capitalize">
                         {row.getValue("firstname")}
                     </span>
                 </div>
@@ -79,7 +79,7 @@ export const StudentAlumniColumns: ColumnDef<IAPICourse>[] = [
         cell: ({ row }) => {
             return (
                 <div className="flex space-x-2">
-                    <span className=" truncate normal-case">
+                    <span className=" truncate capitalize">
                         {row.getValue("middlename")}
                     </span>
                 </div>
