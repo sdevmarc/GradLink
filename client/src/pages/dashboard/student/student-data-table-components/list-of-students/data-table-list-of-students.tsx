5c036b2a4233ce5ffe2a9165a061b72b59dddb6c
sdevmarc <suanieeee@yahoo.com>
2024-10-28 01:24:05 +0800

    Refactor: Update DataTableStudentListOfStudent component to include column visibility options

--- client/src/pages/dashboard/student/student-data-table-components/list-of-students/data-table-list-of-students.tsx
+++ client/src/pages/dashboard/student/student-data-table-components/list-of-students/data-table-list-of-students.tsx
@@ -37,7 +37,11 @@ export function DataTableStudentListOfStudent<TData, TValue>({
 }: DataTableProps<TData, TValue>) {
     const [sorting, setSorting] = React.useState<SortingState>([])
     const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([])
-    const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({})
+    const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({
+        program: false,
+        department: false,
+        createdAt: false
+    })
     const [rowSelection, setRowSelection] = React.useState({})
 
     const table = useReactTable({
