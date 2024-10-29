85947f593ac5b43a4e44a5f59348413f3b672a1d
sdevmarc <suanieeee@yahoo.com>
2024-10-29 20:41:58 +0800

    Refactor: Update curriculum API endpoints and interfaces

--- client/src/api/curriculum/index.ts
+++ client/src/api/curriculum/index.ts
@@ -1,5 +1,5 @@
 import { HOST } from '@/constants'
-import { IRequestCurriculum } from '@/interface/curriculum.interface'
+import { ICurriculum } from '@/interface/curriculum.interface'
 import axios from 'axios'
 
 export const API_CURRICULUM_FINDALL = async () => {
@@ -11,30 +11,20 @@ export const API_CURRICULUM_FINDALL = async () => {
     }
 }
 
-// export const API_CURRICULUM_FINDALL_LEGACY = async () => {
-//     try {
-//         const response = await axios.get(`${HOST}/curriculum/legacy`)
-//         return response.data
-//     } catch (error) {
-//         console.error(error)
-//     }
-// }
-
-export const API_NEW_CURRICULUM = async ({ name, programCode, major, categories }: IRequestCurriculum) => {
+export const API_CURRICULUM_FINDALL_ACTIVE = async () => {
     try {
-        const response = await axios.post(`${HOST}/curriculum/create`, { name, programCode, major, categories })
+        const response = await axios.get(`${HOST}/curriculum/active`)
         return response.data
     } catch (error) {
         console.error(error)
     }
 }
 
-export const API_CURRICULUM_ISEXISTS = async () => {
-    const response = await axios.get(`${HOST}/curriculum/check-curriculum`)
-    return response.data
-}
-
-export const API_SEMESTER_ISEXISTS = async () => {
-    const response = await axios.get(`${HOST}/semester/check-semester`)
-    return response.data
+export const API_NEW_CURRICULUM = async ({ name, programid, major, categories }: ICurriculum) => {
+    try {
+        const response = await axios.post(`${HOST}/curriculum/create`, { name, programid, major, categories })
+        return response.data
+    } catch (error) {
+        console.error(error)
+    }
 }
