d4c83846a96b82fae58345d9ecad88e4108d6c56
sdevmarc <suanieeee@yahoo.com>
2024-11-03 23:23:13 +0800

    Enhance ListOfStudents: add tabs for Students and Alumni, update layout and loading states

--- client/src/pages/dashboard/student/list-of-students.tsx
+++ client/src/pages/dashboard/student/list-of-students.tsx
@@ -1,15 +1,21 @@
-import MainTable from "@/components/main-table"
 import HeadSection, { SubHeadSectionDetails } from "@/components/head-section"
-import { Sidebar, SidebarNavs } from "@/components/sidebar"
 import { useQuery } from "@tanstack/react-query"
 import { API_STUDENT_FINDALL } from "@/api/student"
-import { ROUTES } from "@/constants"
 import { StudentListOfStudentsColumns } from "./student-data-table-components/list-of-students/columns-student-list-of-students"
 import { DataTableStudentListOfStudent } from "./student-data-table-components/list-of-students/data-table-list-of-students"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { DataTableStudentAlumni } from "./student-data-table-components/alumni/data-table-alumni"
+import { StudentAlumniColumns } from "./student-data-table-components/alumni/columns-student-alumni"
+import { API_STUDENT_FINDALL_ALUMNI } from "@/api/alumni"
 
 export default function ListOfStudents() {
     const { data: students, isLoading: studentLoading, isFetched: studentFetched } = useQuery({
         queryFn: () => API_STUDENT_FINDALL(),
+        queryKey: ['students']
+    })
+
+    const { data: dataAlumni, isLoading: alumniLoading, isFetched: alumniFetched } = useQuery({
+        queryFn: () => API_STUDENT_FINDALL_ALUMNI(),
         queryKey: ['students']
     })
 
@@ -20,26 +26,39 @@ export default function ListOfStudents() {
                     <aside className="px-4 pb-4 pt-[8rem]">
                         <HeadSection>
                             <SubHeadSectionDetails
-                                title="LIST OF REGISTERED STUDENTS"
-                                description="Here's a list of registered students."
+                                title="STUDENTS"
+                                description="View and manage students."
                             />
                         </HeadSection>
                     </aside>
-                    <main className="flex">
-                        <Sidebar>
-                            <SidebarNavs bg='bg-muted' title="Student Information" link={ROUTES.LIST_OF_STUDENTS} />
-                            <SidebarNavs title="Alumni Graduates" link={ROUTES.ALUMNI_GRADUATES} />
-                        </Sidebar>
-                        <MainTable>
-                            {studentLoading && <div>Loading...</div>}
-                            {
-                                (!studentLoading && studentFetched) &&
-                                <DataTableStudentListOfStudent
-                                    columns={StudentListOfStudentsColumns}
-                                    data={students?.data || []}
-                                />
-                            }
-                        </MainTable>
+                    <main className="flex flex-col px-8">
+                        <Tabs defaultValue="students" className="w-full">
+                            <TabsList className="bg-background">
+                                <TabsTrigger value="students">Students</TabsTrigger>
+                                <TabsTrigger value="alumni">Alumni</TabsTrigger>
+                            </TabsList>
+                            <TabsContent value="students">
+                                {studentLoading && <div>Loading...</div>}
+                                {
+                                    (!studentLoading && studentFetched) &&
+                                    <DataTableStudentListOfStudent
+                                        columns={StudentListOfStudentsColumns}
+                                        data={students?.data || []}
+                                    />
+                                }
+                            </TabsContent>
+                            <TabsContent value="alumni">
+                                {alumniLoading && <div>Loading...</div>}
+                                {
+                                    (!alumniLoading && alumniFetched) &&
+                                    <DataTableStudentAlumni
+                                        columns={StudentAlumniColumns}
+                                        data={[]}
+                                    />
+                                }
+                            </TabsContent>
+                        </Tabs>
+
                     </main>
                 </div>
             </div>
