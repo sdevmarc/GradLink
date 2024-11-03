ee5fe8d5eea26f9794373e279e88bcb5af0f6ea2
sdevmarc <suanieeee@yahoo.com>
2024-11-03 23:24:19 +0800

    Refactor CoursesSchema: remove unused prerequisites field and isoffered property

--- server/src/courses/courses.schema.ts
+++ server/src/courses/courses.schema.ts
@@ -9,26 +9,16 @@ export const CoursesSchema = new mongoose.Schema({
     courseno: {
         type: String,
         required: true,
-        unique: true,  // Ensure course number is unique
-        index: true  // Index for faster lookups
+        unique: true,
+        index: true
     },
     descriptiveTitle: {
         type: String,
         required: true
     },
-    prerequisites: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Course',
-        default: 'None'
-    }],
     units: {
         type: Number,
         required: true,
         default: 0
-    },
-    isoffered: {
-        type: Boolean,
-        required: true,
-        default: false
     }
 }, { timestamps: true })
