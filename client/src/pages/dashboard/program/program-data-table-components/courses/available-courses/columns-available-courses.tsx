5724b2643d8e6649ecd9bfe4c44acf869aac891c
sdevmarc <suanieeee@yahoo.com>
2024-11-03 23:22:30 +0800

    Enhance AvailableCoursesColumns: add icons for courses, improve layout with badges, and update button design

--- client/src/pages/dashboard/program/program-data-table-components/courses/available-courses/columns-available-courses.tsx
+++ client/src/pages/dashboard/program/program-data-table-components/courses/available-courses/columns-available-courses.tsx
@@ -6,19 +6,35 @@ import {
 
 import { IAPICourse } from '@/interface/course.interface'
 import { DataTableColumnHeader } from "@/components/data-table-components/data-table-column-header"
-import { DataTableRowActionsAvailableCourses } from "./data-table-row-actions-available-courses"
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { RightSheetModal } from "@/components/right-sheet-modal"
+import { BookOpen, TableOfContents } from "lucide-react"
+import { Badge } from "@/components/ui/badge"
 
 export const AvailableCoursesColumns: ColumnDef<IAPICourse>[] = [
+    {
+        id: "icon",
+        cell: () => {
+            return (
+                <div className="w-0">
+                    <BookOpen color="#000000" size={18} />
+                </div>
+            )
+
+        }
+    },
     {
         accessorKey: "code",
         header: ({ column }) => (
             <DataTableColumnHeader column={column} title="Code" className="text-text" />
         ),
         cell: ({ row }) => (
-            <div className="w-[100px] capitalize">{row.getValue("code")}</div>
+            <div className="flex justify-start items-center capitalize gap-4 ">
+                <Badge variant={`default`}>
+                    {row.getValue("code")}
+                </Badge>
+            </div>
         ),
         enableHiding: false
     },
@@ -77,9 +93,9 @@ export const AvailableCoursesColumns: ColumnDef<IAPICourse>[] = [
                 setIsOpen(open)
             }
             return (
-                <>
-                    <Button onClick={handleViewDetails} variant={`outline`} size={`sm`}>
-                        View Details
+                <div className="flex justify-end">
+                    <Button onClick={handleViewDetails} variant={`outline`} size={`sm`} className="flex items-center gap-4">
+                        <TableOfContents color="#000000" size={18} />   View Details
                     </Button>
                     <RightSheetModal
                         className="w-[60%]"
@@ -89,8 +105,7 @@ export const AvailableCoursesColumns: ColumnDef<IAPICourse>[] = [
                         description="View details of the selected course."
                         content={''}
                     />
-                </>
-
+                </div>
             )
         }
     }
