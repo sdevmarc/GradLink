49be26d6d0f9ce3586563e800a89b23f5d266d41
sdevmarc <suanieeee@yahoo.com>
2024-11-04 01:37:38 +0800

    Enhance program details modal with residency and credits information

--- client/src/pages/dashboard/program/program-data-table-components/program/available-programs/columns-available-programs.tsx
+++ client/src/pages/dashboard/program/program-data-table-components/program/available-programs/columns-available-programs.tsx
@@ -10,7 +10,8 @@ import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { RightSheetModal } from "@/components/right-sheet-modal"
 import { Badge } from "@/components/ui/badge"
-import { BookCopy, TableOfContents } from "lucide-react"
+import { BookCopy, BookOpen, Clock, GraduationCap, TableOfContents } from "lucide-react"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 
 export const AvailableProgramsColumns: ColumnDef<IAPIPrograms>[] = [
     {
@@ -85,6 +86,13 @@ export const AvailableProgramsColumns: ColumnDef<IAPIPrograms>[] = [
         cell: ({ row }) => {
             const [isOpen, setIsOpen] = useState<boolean>(false)
 
+            const code = row?.original?.code || 'Invalid Code'
+            const descriptiveTitle = row?.original?.descriptiveTitle || 'Invalid Descriptive Title'
+            const residency = row?.original?.residency || 'Invalid Descriptive Title'
+            const totalUnits = row?.original?.totalUnits || 0
+            const { name, major, categories } = row?.original?.curriculum || {}
+
+            // console.log(test)
             const handleViewDetails = () => {
                 setIsOpen(true)
             }
@@ -103,7 +111,80 @@ export const AvailableProgramsColumns: ColumnDef<IAPIPrograms>[] = [
                         onOpenChange={handleOpenChange}
                         title="Program Details"
                         description="View details of the selected program."
-                        content={''}
+                        content={
+                            <div className="flex flex-col min-h-screen items-center">
+                                <div className="w-full max-w-[90rem] flex flex-col">
+                                    <main className="flex justify-center items-center py-4">
+                                        <div className="min-h-screen w-full max-w-[70rem]">
+                                            <Card className="w-full mx-auto">
+                                                <CardHeader>
+                                                    <div className="flex justify-between items-start">
+                                                        <div>
+                                                            <CardTitle className="capitalize text-3xl font-bold flex flex-col">
+                                                                {descriptiveTitle}
+                                                                <span className="font-normal text-md">{major || null}</span>
+                                                            </CardTitle>
+                                                            <CardDescription className="mt-2">
+                                                                <Badge variant="default" className="mr-2">
+                                                                    {code}
+                                                                </Badge>
+                                                                <span className="text-muted-foreground">
+                                                                    {name || 'No Curriculum'}
+                                                                </span>
+                                                            </CardDescription>
+                                                        </div>
+                                                        {/* <Button>Apply Now</Button> */}
+                                                    </div>
+                                                </CardHeader>
+                                                <CardContent className="space-y-4">
+                                                    <section className="space-y-4">
+                                                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                                                            <div className="flex items-center">
+                                                                <Clock className="h-5 w-5 mr-2 text-muted-foreground" />
+                                                                <span>Residency: {residency} years</span>
+                                                            </div>
+                                                            <div className="flex items-center">
+                                                                <BookOpen className="h-5 w-5 mr-2 text-muted-foreground" />
+                                                                <span>Credits: {totalUnits}</span>
+                                                            </div>
+                                                        </div>
+                                                    </section>
+
+                                                    <section className="space-y-4">
+                                                        {
+                                                            categories?.map((item, i) => (
+                                                                <div key={i} className="flex flex-col">
+                                                                    <h3 className="font-medium text-md">
+                                                                        {item.categoryName}
+                                                                    </h3>
+                                                                    {
+                                                                        item?.courses?.map((course, l) => (
+                                                                            <ul key={l} className="flex items-center gap-4 mt-2">
+                                                                                <li className="w-full flex items-center justify-between text-sm">
+                                                                                    <div className="flex items-center gap-2">
+                                                                                        <GraduationCap size={18} className="h-5 w-5 mr-2 text-muted-foreground" />
+                                                                                        {(course as any)?.descriptiveTitle || 'No Title'}
+                                                                                    </div>
+                                                                                    <h1 className="flex items-center gap-2">
+                                                                                        {(course as any)?.units}<span>Units</span>
+                                                                                    </h1>
+                                                                                </li>
+                                                                            </ul>
+                                                                        ))
+                                                                    }
+
+                                                                </div>
+                                                            ))
+                                                        }
+
+                                                    </section>
+                                                </CardContent>
+                                            </Card>
+                                        </div>
+                                    </main>
+                                </div>
+                            </div>
+                        }
                     />
                 </div>
             )
