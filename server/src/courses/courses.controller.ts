e8f2e6664482990c6fd1902408447abbf0a41f51
sdevmarc <suanieeee@yahoo.com>
2024-11-03 23:23:43 +0800

    Refactor CoursesController: remove unused methods and parameters, clean up commented code

--- server/src/courses/courses.controller.ts
+++ server/src/courses/courses.controller.ts
@@ -1,7 +1,6 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
-import { CoursesService } from './courses.service';
-import { ICourses, IRequestCourses } from './courses.interface';
-import mongoose from 'mongoose';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common'
+import { CoursesService } from './courses.service'
+import { ICourses } from './courses.interface'
 
 @Controller('courses')
 export class CoursesController {
@@ -14,38 +13,18 @@ export class CoursesController {
         return this.courseService.findAll()
     }
 
-    @Get('courses-offered')
-    async findAllCoursesOffered() {
-        return this.courseService.findAllCoursesOffered()
-    }
-
     @Get('courses-active-curriculum')
     async findAllCoursesInActiveCurriculum() {
         return this.courseService.findAllCoursesInActiveCurricullum()
     }
 
     @Get('courses-additional/:curriculumid')
-    async findAllCoursesAddtional(@Param('curriculumid') curriculumid: string ) {
+    async findAllCoursesAddtional(@Param('curriculumid') curriculumid: string) {
         return this.courseService.findAllCoursesForAdditional({ curriculumid })
     }
 
-    // @Get(':courseno')
-    // async findOneCourse(@Param() { courseno }: ICourses) {
-    //     return this.courseService.findOne({ courseno })
-    // }
-
     @Post('create')
-    async createCourse(@Body() { code, courseno, descriptiveTitle, units, prerequisites }: ICourses) {
-        return this.courseService.create({ code, courseno, descriptiveTitle, units, prerequisites })
+    async createCourse(@Body() { code, courseno, descriptiveTitle, units }: ICourses) {
+        return this.courseService.create({ code, courseno, descriptiveTitle, units })
     }
-
-    @Post('update-courses-offered')
-    async updateCourse(@Body() { id }: IRequestCourses) {
-        return this.courseService.updateToCourseOffered({ id })
-    }
-
-    // @Post(':cid')
-    // async deleteCourse(@Param() { cid }: ICourses) {
-    //     return this.courseService.delete({ cid })
-    // }
 }
