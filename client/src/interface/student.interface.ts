8a3f00d7d9c8210b19e686f38e8446b227ab517a
sdevmarc <suanieeee@yahoo.com>
2024-11-04 02:50:29 +0800

    Add interfaces for academic year and enrolled courses in student interface

--- client/src/interface/student.interface.ts
+++ client/src/interface/student.interface.ts
@@ -2,6 +2,29 @@ interface IEnrollments {
     course?: string
 }
 
+interface IAcademicYear {
+    startDate?: number
+    endDate?: number
+}
+
+interface ICourses {
+    _id?: string
+    code?: number
+    courseno?: string
+    descriptiveTitle?: string
+    units?: 3,
+    enrollmentDate?: string
+    status?: string
+    semester?: number
+    academicYear?: IAcademicYear
+}
+
+
+interface IEnrolledCourses {
+    courses?: ICourses[]
+    academicYear?: IAcademicYear
+}
+
 export interface IAPIStudents {
     _id?: string
     idNumber?: string
@@ -17,6 +40,12 @@ export interface IAPIStudents {
     units?: string
     totalOfUnitsEarned?: number
     totalOfUnitsEnrolled?: number
+    enrolledCourses?: IEnrolledCourses[]
+    progress?: string
+    programCode?: string
+    programName?: string
+    department?: string
+    isenrolled?: string
 }
 
 export interface IRequestStudents {
