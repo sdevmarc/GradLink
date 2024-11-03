de337a2bcb19da14523b74291c0bfebf3c1929ef
sdevmarc <suanieeee@yahoo.com>
2024-11-04 01:37:14 +0800

    Add totalUnits and curriculum properties to IAPIPrograms interface

--- client/src/interface/program.interface.ts
+++ client/src/interface/program.interface.ts
@@ -1,7 +1,12 @@
+import { ICurriculum } from "./curriculum.interface"
+
 export interface IAPIPrograms {
     _id?: string
     code?: string
     descriptiveTitle?: string
     residency?: string
     department?: string
+
+    totalUnits?: number
+    curriculum?: ICurriculum
 }
