a998a2100ff5115cd344d91797b6be12223e6f9c
sdevmarc <suanieeee@yahoo.com>
2024-11-03 23:22:49 +0800

    Enhance DataTableToolbarSelectProgramsInCurriculum: update search placeholder to "Search program code..."

--- client/src/pages/dashboard/program/program-data-table-components/curriculum/create-curriculum/programs/data-table-toolbar-select-programs-in-curriculum.tsx
+++ client/src/pages/dashboard/program/program-data-table-components/curriculum/create-curriculum/programs/data-table-toolbar-select-programs-in-curriculum.tsx
@@ -14,18 +14,19 @@ export function DataTableToolbarSelectProgramsInCurriculum<TData>({
     table,
 }: DataTableToolbarProps<TData>) {
     const isFiltered = table.getState().columnFilters.length > 0
-    
+
     return (
         <div className="flex flex-wrap items-center justify-between">
             <div className="flex flex-1 flex-wrap items-center gap-2">
                 <Input
-                    placeholder="Search course number..."
+                    placeholder="Search program code..."
                     value={(table.getColumn("code")?.getFilterValue() as string) ?? ""}
                     onChange={(event) => {
                         table.getColumn("code")?.setFilterValue(event.target.value)
                     }}
                     className="h-8 w-[20rem] lg:w-[25rem]"
                 />
+
                 {isFiltered && (
                     <Button
                         variant="ghost"
