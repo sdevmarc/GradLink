1f1c00fda6113fb7d86097e03679d7a871451b01
sdevmarc <suanieeee@yahoo.com>
2024-11-03 23:22:53 +0800

    Enhance CurriculumColumns: add icons for courses, include creation date column, and improve action button layout

--- client/src/pages/dashboard/program/program-data-table-components/curriculum/curriculum/columns-curriculum.tsx
+++ client/src/pages/dashboard/program/program-data-table-components/curriculum/curriculum/columns-curriculum.tsx
@@ -10,8 +10,20 @@ import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { RightSheetModal } from "@/components/right-sheet-modal"
 import { useState } from "react"
+import { Bookmark, TableOfContents } from "lucide-react"
 
 export const CurriculumColumns: ColumnDef<IAPICourse>[] = [
+    {
+        id: "icon",
+        cell: () => {
+            return (
+                <div className="w-0">
+                    <Bookmark color="#000000" size={18} />
+                </div>
+            )
+
+        }
+    },
     {
         accessorKey: "name",
         header: ({ column }) => (
@@ -30,23 +42,9 @@ export const CurriculumColumns: ColumnDef<IAPICourse>[] = [
         ),
         cell: ({ row }) => (
             <div className="w-full capitalize">{row.getValue("program")}</div>
-        )
-    },
-    {
-        accessorKey: "major",
-        header: ({ column }) => (
-            <DataTableColumnHeader column={column} title="Major" className="text-text" />
         ),
-        cell: ({ row }) => {
-            return (
-                <div className="flex space-x-2">
-                    <span className="max-w-[500px] truncate capitalize">
-                        {row.getValue("major") === '' ? 'None' : row.getValue("major")}
-                    </span>
-                </div>
-            )
-        },
-        enableSorting: false
+        enableSorting: false,
+        enableHiding: false
     },
     {
         accessorKey: "isActive",
@@ -71,6 +69,18 @@ export const CurriculumColumns: ColumnDef<IAPICourse>[] = [
         enableSorting: false
     },
     {
+        accessorKey: "createdAt",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Date Created" className="text-text" />
+        ),
+        cell: ({ row }) => {
+            return (
+                <div className="w-full capitalize">{row.getValue("createdAt")}</div>
+            )
+        },
+        enableColumnFilter: true,
+    },
+    {
         id: "actions",
         cell: ({ row }) => {
             const [isOpen, setIsOpen] = useState<boolean>(false)
@@ -83,9 +93,9 @@ export const CurriculumColumns: ColumnDef<IAPICourse>[] = [
                 setIsOpen(open)
             }
             return (
-                <>
-                    <Button onClick={handleViewDetails} variant={`outline`} size={`sm`}>
-                        View Details
+                <div className="flex justify-end">
+                    <Button onClick={handleViewDetails} variant={`outline`} size={`sm`} className="flex items-center gap-4">
+                        <TableOfContents color="#000000" size={18} />  View Details
                     </Button>
                     <RightSheetModal
                         className="w-[60%]"
@@ -95,8 +105,7 @@ export const CurriculumColumns: ColumnDef<IAPICourse>[] = [
                         description="View details of the selected curriculum."
                         content={''}
                     />
-                </>
-
+                </div>
             )
         }
     }
