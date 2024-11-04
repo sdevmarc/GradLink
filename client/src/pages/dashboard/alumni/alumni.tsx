479fb064734d8fe0d0706ac76d9553a5c2cb3e2b
sdevmarc <suanieeee@yahoo.com>
2024-11-04 19:46:08 +0800

    Add Alumni page with data fetching and loading state

---
+++ client/src/pages/dashboard/alumni/alumni.tsx
@@ -0,0 +1,39 @@
+import HeadSection, { SubHeadSectionDetails } from "@/components/head-section"
+import { useQuery } from "@tanstack/react-query"
+import { API_STUDENT_FINDALL_ALUMNI } from "@/api/alumni"
+import { DataTableStudentAlumni } from "./alumni-data-table-components/alumni/data-table-alumni"
+import { StudentAlumniColumns } from "./alumni-data-table-components/alumni/columns-student-alumni"
+
+export default function Alumni() {
+    const { data: dataAlumni, isLoading: alumniLoading, isFetched: alumniFetched } = useQuery({
+        queryFn: () => API_STUDENT_FINDALL_ALUMNI(),
+        queryKey: ['students']
+    })
+
+    return (
+        <>
+            <div className="flex flex-col min-h-screen items-center">
+                <div className="w-full max-w-[90rem] flex flex-col">
+                    <aside className="px-4 pb-4 pt-[8rem]">
+                        <HeadSection>
+                            <SubHeadSectionDetails
+                                title="ALUMNI"
+                                description="View and manage alumni."
+                            />
+                        </HeadSection>
+                    </aside>
+                    <main className="flex px-8">
+                        {alumniLoading && <div>Loading...</div>}
+                        {
+                            (!alumniLoading && alumniFetched) &&
+                            <DataTableStudentAlumni
+                                columns={StudentAlumniColumns}
+                                data={[]}
+                            />
+                        }
+                    </main>
+                </div>
+            </div>
+        </>
+    )
+}
