3d0b9934be72d7177e538572f2ba62eecd68924e
sdevmarc <suanieeee@yahoo.com>
2024-11-05 18:25:44 +0800

    Add API methods for fetching and creating offered courses

--- client/src/api/offered/index.ts
+++ client/src/api/offered/index.ts
@@ -1,9 +1,22 @@
 import { HOST } from '@/constants'
+import { IAPIOffered } from '@/interface/offered.interface'
 import axios from 'axios'
 
 export const API_FINDALL_COURSES_OFFERED = async () => {
     try {
         const response = await axios.get(`${HOST}/offered`)
+        return response.data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const API_CREATE_COURSES_OFFERED = async ({ courses, semester, academicYear }: IAPIOffered) => {
+    try {
+        const response = await axios.post(`${HOST}/offered/create`, {
+            courses, semester, academicYear
+        })
+
         return response.data
     } catch (error) {
         console.error(error)
