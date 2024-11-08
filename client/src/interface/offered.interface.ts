5782131dfe9e8479e62ffc98c62a5ab4e84e5145
Marc Edison <suanieeee@yahoo.com>
2024-11-08 13:18:43 +0800

    feat: add curriculum interface and department field to API offered interface

--- client/src/interface/offered.interface.ts
+++ client/src/interface/offered.interface.ts
@@ -3,11 +3,18 @@ interface academicYear {
     endDate: number
 }
 
+interface ICurriculum {
+    name?: string
+    major?: string
+}
+
 export interface IAPIOffered {
     _id?: string
     code?: string
     courseno?: string
     descriptiveTitle?: string
+    department?: string
+    curriculum?: ICurriculum
     semester?: number
     academicYear?: academicYear
     courses?: string[]
