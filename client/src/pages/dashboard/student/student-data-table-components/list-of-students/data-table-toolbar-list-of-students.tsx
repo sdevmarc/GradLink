ede9e68981e3bd5e31f53ab09afe9d1159140fad
Marc Edison <suanieeee@yahoo.com>
2024-11-08 20:11:11 +0800

    fix: update btnIcon color in DataTableToolbarListOfStudents to use Tailwind's primary color for consistency

--- client/src/pages/dashboard/student/student-data-table-components/list-of-students/data-table-toolbar-list-of-students.tsx
+++ client/src/pages/dashboard/student/student-data-table-components/list-of-students/data-table-toolbar-list-of-students.tsx
@@ -154,7 +154,7 @@ export function DataTableToolbarListOfStudent<TData>({
                     className="flex items-center gap-2"
                     type={`default`}
                     variant={'outline'}
-                    btnIcon={<Plus color="#000000" size={18} />}
+                    btnIcon={<Plus className="text-primary" size={18} />}
                     btnTitle="New Student"
                     title="Are you sure?"
                     description={`You will be redirect to page for creating new student.`}
