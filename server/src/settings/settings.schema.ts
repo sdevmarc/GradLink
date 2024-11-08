b955a3a6454955561cd2852b31b36fd92499fc72
sdevmarc <suanieeee@yahoo.com>
2024-11-09 04:00:11 +0800

    feat: add index field to SettingsSchema with default value

--- server/src/settings/settings.schema.ts
+++ server/src/settings/settings.schema.ts
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
 export const SettingsSchema = new mongoose.Schema({
+    index: {
+        type: Number,
+        required: true,
+        default: 0
+    },
     isenroll: {
         type: Boolean,
         required: true,
