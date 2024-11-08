f07c8a4ca34b3c79cbf6d58676c9796bea7870b1
sdevmarc <suanieeee@yahoo.com>
2024-11-09 04:00:17 +0800

    feat: implement findAll and upsertSettings methods in SettingsService for settings management

--- server/src/settings/settings.service.ts
+++ server/src/settings/settings.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { IPromiseSettings, ISettings } from './settings.interface';
 
 @Injectable()
-export class SettingsService {}
+export class SettingsService {
+    constructor(
+        @InjectModel('Settings') private readonly SettingsModel: Model<ISettings>
+    ) { }
+
+    async findAll(): Promise<IPromiseSettings> {
+        try {
+            const response = await this.SettingsModel.findOne({ index: 1 })
+            return { success: true, message: 'Settings fetched successfully.', data: response }
+        } catch (error) {
+            throw new HttpException(
+                { success: false, message: 'Failed to fetch settings.', error },
+                HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    async upsertSettings({ isenroll }: ISettings): Promise<IPromiseSettings> {
+        try {
+            await this.SettingsModel.findOneAndUpdate(
+                { index: 1 },
+                { isenroll },
+                { new: true, upsert: true }
+            )
+            return { success: true, message: 'Settings updated successfully.' }
+        } catch (error) {
+            throw new HttpException(
+                { success: false, message: 'Failed to update settings.', error },
+                HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+}
