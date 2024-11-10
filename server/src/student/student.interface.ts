da46f2cce0350a7c83c831f12b2d924ef79346cc
sdevmarc <suanieeee@yahoo.com>
2024-11-11 04:15:58 +0800

    feat: update IStudentEnrollments interface to use course object and add graduation_date field

--- server/src/student/student.interface.ts
+++ server/src/student/student.interface.ts
@@ -1,6 +1,6 @@
 
 interface IStudentEnrollments {
-    course?: string[]
+    course?: { _id: string } 
     enrollmentDate?: string
     ispass?: 'pass' | 'fail' | 'inc' | 'ongoing' | 'drop' | 'discontinue'
 }
@@ -38,6 +38,7 @@ export interface IStudent {
     courses?: string[]
     undergraduateInformation?: IUndergraduateInformation
     achievements?: IAchievements
+    graduation_date?: Date
 }
 
 export interface IEvaluationItem {
