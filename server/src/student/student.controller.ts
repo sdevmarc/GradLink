da8d787c54740fd87ede4571692a5d99e1632301
sdevmarc <suanieeee@yahoo.com>
2024-11-11 06:14:22 +0800

    feat: comment out createStudentEnrollee and enrollStudentEnrollee methods in StudentController

--- server/src/student/student.controller.ts
+++ server/src/student/student.controller.ts
@@ -51,51 +51,51 @@ export class StudentController {
         return this.studentService.findAllYearrGraduated()
     }
 
-    @Post('new-student')
-    async createStudentEnrollee(
-        @Body() { userId, idNumber, lastname, firstname, middlename, email, program, courses, undergraduateInformation, achievements }: IStudent
-    ) {
-        try {
-            const issuccess = await this.studentService.createEnrollee({ idNumber, lastname, firstname, middlename, email, program, courses, undergraduateInformation, achievements })
-            if (issuccess.success)
-            {
-                await this.auditlogService.createLog({ userId, action: "create", description: `Student created w/ ID no: ${idNumber}` })
-                return { success: true, message: "Student successfully created." }
-            }
-            await this.auditlogService.createLog({ userId, action: "create", description: 'Error' })
-            return { success: false, message: issuccess.message }
+    // @Post('new-student')
+    // async createStudentEnrollee(
+    //     @Body() { userId, idNumber, lastname, firstname, middlename, email, program, courses, undergraduateInformation, achievements }: IStudent
+    // ) {
+    //     try {
+    //         const issuccess = await this.studentService.createEnrollee({ idNumber, lastname, firstname, middlename, email, program, courses, undergraduateInformation, achievements })
+    //         if (issuccess.success)
+    //         {
+    //             await this.auditlogService.createLog({ userId, action: "create", description: `Student created w/ ID no: ${idNumber}` })
+    //             return { success: true, message: "Student successfully created." }
+    //         }
+    //         await this.auditlogService.createLog({ userId, action: "create", description: 'Error' })
+    //         return { success: false, message: issuccess.message }
 
-        } catch (error) {
-            throw new HttpException(
-                { success: false, message: 'Failed to fetch audit logs.', error },
-                HttpStatus.INTERNAL_SERVER_ERROR
-            );
-        }
+    //     } catch (error) {
+    //         throw new HttpException(
+    //             { success: false, message: 'Failed to fetch audit logs.', error },
+    //             HttpStatus.INTERNAL_SERVER_ERROR
+    //         );
+    //     }
 
-        //return await this.studentService.createEnrollee({ idNumber, lastname, firstname, middlename, email, program, courses, undergraduateInformation, achievements })
-    }
+    //     //return await this.studentService.createEnrollee({ idNumber, lastname, firstname, middlename, email, program, courses, undergraduateInformation, achievements })
+    // }
 
-    @Post('enroll-student')
-    async enrollStudentEnrollee(
-        @Body() { userId, course, id }: IRequestStudent
-    ) {
-        try {
-            const SelectedCourse = await this.coursesService.findOneCourse({course})
-            const issuccess = await this.studentService.enrollStudent({course, id})
-            if(issuccess.success){
-                await this.auditlogService.createLog({userId, action: "Enroll", description: `${id.length} student/s was enrolled in ${SelectedCourse.data.descriptiveTitle}`})
-            }
+    // @Post('enroll-student')
+    // async enrollStudentEnrollee(
+    //     @Body() { userId, course, id }: IRequestStudent
+    // ) {
+    //     try {
+    //         const SelectedCourse = await this.coursesService.findOneCourse({course})
+    //         const issuccess = await this.studentService.enrollStudent({course, id})
+    //         if(issuccess.success){
+    //             await this.auditlogService.createLog({userId, action: "Enroll", description: `${id.length} student/s was enrolled in ${SelectedCourse.data.descriptiveTitle}`})
+    //         }
 
-        } catch (error) {
-                        throw new HttpException(
-                { success: false, message: 'Failed to fetch audit logs.', error },
-                HttpStatus.INTERNAL_SERVER_ERROR
-            );
-        }
+    //     } catch (error) {
+    //                     throw new HttpException(
+    //             { success: false, message: 'Failed to fetch audit logs.', error },
+    //             HttpStatus.INTERNAL_SERVER_ERROR
+    //         );
+    //     }
 
-        //console.log(SelectedCourse.data.descriptiveTitle)
-        return await this.studentService.enrollStudent({ course, id })
-    }
+    //     //console.log(SelectedCourse.data.descriptiveTitle)
+    //     return await this.studentService.enrollStudent({ course, id })
+    // }
 
 
     @Post('evaluate-student')
@@ -117,12 +117,16 @@ export class StudentController {
         const response = await this.formService.mapQuestionsToAnswers(this.constantsService.getFormId())
 
         const updatePromises = response.map(async (item) => {
-            const idNumber = String(item.generalInformation.answers[0].answer)
-            const { generalInformation, educationalBackground, employmentData } = item
-            return await this.studentService.formUpdateStudent({
-                idNumber,
+            const formemail = String(item.generalInformation.answers[6].answer)
+            const {
                 generalInformation,
-                educationalBackground,
+                // educationalBackground, 
+                employmentData
+            } = item
+            return await this.studentService.formUpdateStudent({
+                email: formemail,
+                generalInformation,
+                // educationalBackground,
                 employmentData,
             })
         })
@@ -130,14 +134,22 @@ export class StudentController {
         const updateResults = await Promise.all(updatePromises)
 
         const resultsPromises = updateResults.map(async (item) => {
-            const { success, message, idNumber } = item
-            if (!success) return await this.studentService.insertFormPending({ idNumber })
+            const {
+                success,
+                message,
+                // email 
+            } = item
+            if (!success) {
+                // return await this.studentService.insertFormPending({ idNumber })
+                return { success: false, message }
+            }
 
             //Testing
             return { success, message }
         })
 
-        const pendingResults = await Promise.all(resultsPromises)
-        return pendingResults
+        // const pendingResults = await Promise.all(resultsPromises)
+        // return pendingResults
+        return resultsPromises
     }
 }
