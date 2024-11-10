94ffe266a7160eeee387a9417a46046216cfb792
Jericho Pineda <jericho.pineda@outlook.com>
2024-11-11 06:21:42 +0800

    Program module modifications

--- server/src/programs/programs.module.ts
+++ server/src/programs/programs.module.ts
@@ -6,14 +6,17 @@ import { ProgramSchema } from './programs.schema';
 import { CurriculumSchema } from 'src/curriculum/curriculum.schema';
 import { CurriculumService } from 'src/curriculum/curriculum.service';
 import { CoursesSchema } from 'src/courses/courses.schema';
+import { AuditlogService } from 'src/auditlog/auditlog.service';
+import { AuditSchema } from 'src/auditlog/auditlog.schema';
 
 @Module({
   imports: [MongooseModule.forFeature([
     { name: 'Course', schema: CoursesSchema },
     { name: 'Program', schema: ProgramSchema },
-    { name: 'Curriculum', schema: CurriculumSchema }
+    { name: 'Curriculum', schema: CurriculumSchema },
+    { name: 'Auditlog', schema: AuditSchema },
   ])],
-  providers: [ProgramsService, CurriculumService],
+  providers: [ProgramsService, CurriculumService, AuditlogService],
   controllers: [ProgramsController]
 })
 export class ProgramsModule { }
