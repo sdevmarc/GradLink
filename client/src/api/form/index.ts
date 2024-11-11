41d37763bd193d9e135f176421dc5ee8c84d02c6
sdevmarc <suanieeee@yahoo.com>
2024-11-11 17:41:48 +0800

    feat: add API endpoint for retrieving mapped forms

--- client/src/api/form/index.ts
+++ client/src/api/form/index.ts
@@ -5,3 +5,8 @@ export const API_FORM_FINDALL_UNKNOWN = async () => {
     const response = await axios.get(`${HOST}/forms/unknown-respondents`)
     return response.data
 }
+
+export const API_FORM_MAPPED = async () => {
+    const response = await axios.get(`${HOST}/forms/form-mapped`)
+    return response.data
+}
