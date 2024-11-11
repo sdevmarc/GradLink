7fddeed2f7c78e719e9a69f208d63b65133c3e43
sdevmarc <suanieeee@yahoo.com>
2024-11-12 05:56:52 +0800

    feat: add Cloudinary configuration properties to ConstantsService

--- server/src/constants/constants.service.ts
+++ server/src/constants/constants.service.ts
@@ -7,12 +7,18 @@ export class ConstantsService {
     private readonly mail_username: string
     private readonly mail_password: string
     private readonly mail_host: string
+    private readonly cloudinaryName: string
+    private readonly cloudinaryKey: string
+    private readonly cloudinarySecret: string
 
     constructor(private configService: ConfigService) {
         this.form_id = this.configService.get<string>('FORM_ID')
         this.mail_username = this.configService.get<string>('EMAIL_USERNAME')
         this.mail_password = this.configService.get<string>('EMAIL_PASSWORD')
         this.mail_host = this.configService.get<string>('EMAIL_HOST');
+        this.cloudinaryName = this.configService.get<string>('CLOUDINARY_NAME');
+        this.cloudinaryKey = this.configService.get<string>('CLOUDINARY_API_KEY');
+        this.cloudinarySecret = this.configService.get<string>('CLOUDINARY_API_SECRET');
     }
 
     getFormId(): string {
@@ -29,5 +35,17 @@ export class ConstantsService {
 
     getMailHost(): string {
         return this.mail_host
+    }
+
+    getCloudinaryName(): string {
+        return this.cloudinaryName
+    }
+
+    getCloudinaryKey(): string {
+        return this.cloudinaryKey
+    }
+
+    getCloudinarySecret(): string {
+        return this.cloudinarySecret
     }
 }
