ed432751789235f8ea07a6ef61afde71e030c937
sdevmarc <suanieeee@yahoo.com>
2024-11-11 12:35:47 +0800

    feat: set default column visibility for alumni data table

--- client/src/pages/dashboard/alumni/alumni-data-table-components/alumni/data-table-alumni.tsx
+++ client/src/pages/dashboard/alumni/alumni-data-table-components/alumni/data-table-alumni.tsx
@@ -37,7 +37,16 @@ export function DataTableStudentAlumni<TData, TValue>({
 }: DataTableProps<TData, TValue>) {
     const [sorting, setSorting] = React.useState<SortingState>([])
     const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([])
-    const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({})
+    const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({
+        program: false,
+        totalOfUnitsEnrolled: false,
+        totalOfUnitsEarned: false,
+        email: false,
+        lastname: false,
+        firstname: false,
+        middlename: false,
+        department: false
+    })
     const [rowSelection, setRowSelection] = React.useState({})
 
     const table = useReactTable({
