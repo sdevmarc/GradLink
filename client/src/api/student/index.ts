d0f4e70689cacd7e579c5735ba8b47674a1ff4c6
sdevmarc <suanieeee@yahoo.com>
2024-11-11 12:36:19 +0800

    feat: add API endpoints for retrieving alumni data for tracer map

--- client/src/api/student/index.ts
+++ client/src/api/student/index.ts
@@ -7,6 +7,16 @@ export const API_STUDENT_FINDALL = async () => {
     return response.data
 }
 
+export const API_STUDENT_FINDONE_ALUMNI_FOR_TRACER_MAP = async ({ id }: { id: string }) => {
+    const response = await axios.get(`${HOST}/student/alumni-tracer/${id}`)
+    return response.data
+}
+
+export const API_STUDENT_FINDALL_ALUMNI_FOR_TRACER_MAP = async () => {
+    const response = await axios.get(`${HOST}/student/alumni-for-tracer-map`)
+    return response.data
+}
+
 export const API_STUDENT_FINDALL_FILTERED_ALUMNI = async ({ search, program, yeargraduated }: { search: string, program: string, yeargraduated: string }) => {
     const response = await axios.post(`${HOST}/student/filtered-alumni`, {
         search, program, yeargraduated
