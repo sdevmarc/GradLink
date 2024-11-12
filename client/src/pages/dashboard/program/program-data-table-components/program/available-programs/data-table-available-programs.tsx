7264384e782d889d2bdc00e96139ce51610b3453
sdevmarc <suanieeee@yahoo.com>
2024-11-12 16:00:22 +0800

    feat: update available programs data table to display total row count

--- client/src/pages/dashboard/program/program-data-table-components/program/available-programs/data-table-available-programs.tsx
+++ client/src/pages/dashboard/program/program-data-table-components/program/available-programs/data-table-available-programs.tsx
@@ -116,8 +116,9 @@ export function DataTableAvailablePrograms<TData, TValue>({
             </div>
             <div className="flex items-center justify-end space-x-2 py-4">
                 <div className="flex-1 text-sm text-muted-foreground">
-                    {table.getFilteredSelectedRowModel().rows.length} of{" "}
-                    {table.getFilteredRowModel().rows.length} row(s) selected.
+                    {/* {table.getFilteredSelectedRowModel().rows.length} of{" "}
+                    {table.getFilteredRowModel().rows.length} row(s) selected. */}
+                    {table.getRowModel().rows.length} row(s) total.
                 </div>
                 <div className="space-x-2">
                     <Button
