91238d1437c37952fb35bd4b18955f2d4c8316c3
sdevmarc <suanieeee@yahoo.com>
2024-11-12 16:00:02 +0800

    feat: update attrition rate courses data table to display total row count

--- client/src/pages/dashboard/enrollment/enrollment-data-table-components/attrition-rate-courses/data-table-attrition-rate-courses..tsx
+++ client/src/pages/dashboard/enrollment/enrollment-data-table-components/attrition-rate-courses/data-table-attrition-rate-courses..tsx
@@ -116,10 +116,11 @@ export function DataTableAttritionRateCourses<TData, TValue>({
                 </Table>
             </div>
             <div className="flex items-center justify-end space-x-2 py-4">
-                {/* <div className="flex-1 text-sm text-muted-foreground">
-                    {table.getFilteredSelectedRowModel().rows.length} of{" "}
-                    {table.getFilteredRowModel().rows.length} row(s) selected.
-                </div> */}
+                <div className="flex-1 text-sm text-muted-foreground">
+                    {/* {table.getFilteredSelectedRowModel().rows.length} of{" "}
+                    {table.getFilteredRowModel().rows.length} row(s) selected. */}
+                    {table.getRowModel().rows.length} row(s) total.
+                </div>
                 <div className="space-x-2">
                     <Button
                         variant="outline"
