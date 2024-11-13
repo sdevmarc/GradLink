fc17f8e4da79bd6c12c86a5d9aca1bfc5e35d05e
sdevmarc <suanieeee@yahoo.com>
2024-11-14 04:32:23 +0800

    feat: configure CORS with specific origin and enable cookie parsing

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
-  await app.listen(3002);
+  app.enableCors({
+    origin: 'http://192.168.1.9:3000',
+    credentials: true
+  })
+  app.use(cookieParser());
+  await app.listen(3002, '192.168.1.10');
 }
 bootstrap();
