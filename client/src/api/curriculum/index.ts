4ace146048058bf8379fa19d0b51c715dff17973
sdevmarc <suanieeee@yahoo.com>
2024-11-14 04:31:04 +0800

    feat: enable withCredentials for curriculum API requests

--- client/src/api/curriculum/index.ts
+++ client/src/api/curriculum/index.ts
@@ -2,9 +2,13 @@ import { HOST } from '@/constants'
 import { ICurriculum } from '@/interface/curriculum.interface'
 import axios from 'axios'
 
+axios.defaults.withCredentials = true;
+
 export const API_CURRICULUM_FINDALL = async () => {
     try {
-        const response = await axios.get(`${HOST}/curriculum`)
+        const response = await axios.get(`${HOST}/curriculum`,
+            { withCredentials: true }
+        )
         return response.data
     } catch (error) {
         console.error(error)
@@ -13,7 +17,9 @@ export const API_CURRICULUM_FINDALL = async () => {
 
 export const API_CURRICULUM_FINDALL_ACTIVE = async () => {
     try {
-        const response = await axios.get(`${HOST}/curriculum/active`)
+        const response = await axios.get(`${HOST}/curriculum/active`,
+            { withCredentials: true }
+        )
         return response.data
     } catch (error) {
         console.error(error)
@@ -22,7 +28,9 @@ export const API_CURRICULUM_FINDALL_ACTIVE = async () => {
 
 export const API_NEW_CURRICULUM = async ({ name, programid, major, categories, department }: ICurriculum) => {
     try {
-        const response = await axios.post(`${HOST}/curriculum/create`, { name, programid, major, categories, department })
+        const response = await axios.post(`${HOST}/curriculum/create`, { name, programid, major, categories, department },
+            { withCredentials: true }
+        )
         return response.data
     } catch (error) {
         console.error(error)
