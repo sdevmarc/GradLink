5dc54f77f0fb867cdd3dc73929266da75d50f446
sdevmarc <suanieeee@yahoo.com>
2024-11-14 04:31:26 +0800

    feat: enable withCredentials for student API requests

--- client/src/api/student/index.ts
+++ client/src/api/student/index.ts
@@ -2,87 +2,109 @@ import { HOST } from '@/constants'
 import { IAPIStudents, IRequestStudents } from '@/interface/student.interface'
 import axios from 'axios'
 
+axios.defaults.withCredentials = true;
+
 export const API_STUDENT_FINDALL = async () => {
-    const response = await axios.get(`${HOST}/student`)
+    const response = await axios.get(`${HOST}/student`,
+        { withCredentials: true }
+    )
     return response.data
 }
 
 export const API_STUDENT_FINDONE_ALUMNI_FOR_TRACER_MAP = async ({ id }: { id: string }) => {
-    const response = await axios.get(`${HOST}/student/alumni-tracer/${id}`)
+    const response = await axios.get(`${HOST}/student/alumni-tracer/${id}`,
+        { withCredentials: true }
+    )
     return response.data
 }
 
 export const API_STUDENT_FINDALL_ALUMNI_FOR_TRACER_MAP = async () => {
-    const response = await axios.get(`${HOST}/student/alumni-for-tracer-map`)
+    const response = await axios.get(`${HOST}/student/alumni-for-tracer-map`,
+        { withCredentials: true }
+    )
     return response.data
 }
 
 export const API_STUDENT_FINDALL_FILTERED_ALUMNI = async ({ search, program, yeargraduated }: { search: string, program: string, yeargraduated: string }) => {
     const response = await axios.post(`${HOST}/student/filtered-alumni`, {
         search, program, yeargraduated
-    })
+    }, { withCredentials: true })
     return response.data
 }
 
 export const API_STUDENT_YEARS_GRADUATED = async () => {
-    const response = await axios.get(`${HOST}/student/years-graduated`)
+    const response = await axios.get(`${HOST}/student/years-graduated`,
+        { withCredentials: true }
+    )
     return response.data
 }
 
 export const API_STUDENT_FINDALL_ATTRITION_RATE_COURSES = async (id: string) => {
-    const response = await axios.get(`${HOST}/student/attrition/${id}`)
+    const response = await axios.get(`${HOST}/student/attrition/${id}`,
+        { withCredentials: true }
+    )
     return response.data
 }
 
 export const API_STUDENT_FINDALL_ENROLLEES = async () => {
-    const response = await axios.get(`${HOST}/student/enrollees`)
+    const response = await axios.get(`${HOST}/student/enrollees`,
+        { withCredentials: true }
+    )
     return response.data
 }
 
 export const API_STUDENT_FINDALL_ENROLLEES_IN_COURSE = async (id: string) => {
-    const response = await axios.get(`${HOST}/student/enrollees/${id}`)
+    const response = await axios.get(`${HOST}/student/enrollees/${id}`,
+        { withCredentials: true }
+    )
     return response.data
 }
 
 export const API_STUDENT_FINDALL_EVALUATEES_IN_COURSE = async (id: string) => {
-    const response = await axios.get(`${HOST}/student/evaluation/${id}`)
+    const response = await axios.get(`${HOST}/student/evaluation/${id}`,
+        { withCredentials: true }
+    )
     return response.data
 }
 
 export const API_STUDENT_FINDALL_CURRENTLY_ENROLLED = async () => {
-    const response = await axios.get(`${HOST}/student/currently-enrolled`)
+    const response = await axios.get(`${HOST}/student/currently-enrolled`,
+        { withCredentials: true }
+    )
     return response.data
 }
 
 export const API_STUDENT_NEW_STUDENT = async ({ idNumber, lastname, firstname, middlename, email, program, courses, undergraduateInformation, achievements }: IAPIStudents) => {
     const response = await axios.post(`${HOST}/student/new-student`, {
         idNumber, lastname, firstname, middlename, email, program, courses, undergraduateInformation, achievements
-    })
+    }, { withCredentials: true })
     return response.data
 }
 
 export const API_STUDENT_ENROLL_STUDENT = async ({ course, id }: IRequestStudents) => {
     const response = await axios.post(`${HOST}/student/enroll-student`, {
         course, id
-    })
+    }, { withCredentials: true })
     return response.data
 }
 
 export const API_STUDENT_ACTIVATE_STUDENT = async ({ id }: IRequestStudents) => {
     const response = await axios.post(`${HOST}/student/activate-student`, {
         studentid: id
-    })
+    }, { withCredentials: true })
     return response.data
 }
 
 export const API_STUDENT_DISCONTINUE_STUDENT = async (formData: FormData) => {
-    const response = await axios.post(`${HOST}/student/discontinue-student`, formData)
+    const response = await axios.post(`${HOST}/student/discontinue-student`, formData,
+        { withCredentials: true }
+    )
     return response.data
 }
 
 export const API_STUDENT_EVALUATE_STUDENT = async ({ course, evaluations }: IRequestStudents) => {
     const response = await axios.post(`${HOST}/student/evaluate-student`, {
         course, evaluations
-    })
+    }, { withCredentials: true })
     return response.data
 }
