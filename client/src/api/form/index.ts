6e4ee40be6c2361bce3c3f1825e676409b72a60a
sdevmarc <suanieeee@yahoo.com>
2024-11-14 04:31:08 +0800

    feat: enable withCredentials for form API requests

--- client/src/api/form/index.ts
+++ client/src/api/form/index.ts
@@ -1,12 +1,18 @@
 import { HOST } from '@/constants'
 import axios from 'axios'
 
+axios.defaults.withCredentials = true;
+
 export const API_FORM_FINDALL_UNKNOWN = async () => {
-    const response = await axios.get(`${HOST}/forms/unknown-respondents`)
+    const response = await axios.get(`${HOST}/forms/unknown-respondents`,
+        { withCredentials: true }
+    )
     return response.data
 }
 
 export const API_FORM_MAPPED = async () => {
-    const response = await axios.get(`${HOST}/forms/form-mapped`)
+    const response = await axios.get(`${HOST}/forms/form-mapped`,
+        { withCredentials: true }
+    )
     return response.data
 }
