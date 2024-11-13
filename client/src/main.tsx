addbec8d604939878de5b1ac131957d4329f91f8
sdevmarc <suanieeee@yahoo.com>
2024-11-14 04:30:40 +0800

    feat: wrap application in AuthProvider for authentication context

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,16 +6,19 @@ import Routes from './pages/routes.tsx'
 import { ThemeProvider } from './hooks/useTheme.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from "@/components/ui/sonner"
+import { AuthProvider } from './hooks/AuthContext.tsx'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-            <QueryClientProvider client={queryClient}>
-                <RouterProvider router={Routes} />
-                <Toaster />
-            </QueryClientProvider>
-        </ThemeProvider>
+        <AuthProvider>
+            <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+                <QueryClientProvider client={queryClient}>
+                    <RouterProvider router={Routes} />
+                    <Toaster />
+                </QueryClientProvider>
+            </ThemeProvider>
+        </AuthProvider>
     </React.StrictMode>,
 )
