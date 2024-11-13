5089726956b4dd8147d34c6eb33df188c2b57427
sdevmarc <suanieeee@yahoo.com>
2024-11-14 04:32:10 +0800

    feat: add status endpoint to check authentication using JWT

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(
+        private readonly appService: AppService,
+        private jwtService: JwtService
+    ) { }
 
-  @Get()
-  getHello(): {} {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): {} {
+        return this.appService.getHello();
+    }
+
+    @Get('status')
+    async getStatus(@Req() request: Request) {
+        const token = request.cookies['access_token'];
+
+        if (!token) {
+            return { isAuthenticated: false };
+        }
+
+        try {
+            const payload = this.jwtService.verify(token);
+            return { isAuthenticated: true, user: payload };
+        } catch (error) {
+            return { isAuthenticated: false };
+        }
+    }
 }
