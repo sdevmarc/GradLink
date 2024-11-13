774364890e089c638fdb63e696dbd382141b9956
sdevmarc <suanieeee@yahoo.com>
2024-11-14 04:30:46 +0800

    feat: enable withCredentials for alumni API requests to support authentication

--- client/src/api/alumni/index.ts
+++ client/src/api/alumni/index.ts
@@ -2,26 +2,32 @@ import { HOST } from '@/constants'
 import { IAPIAlumni } from '@/interface/alumni.interface'
 import axios from 'axios'
 
+axios.defaults.withCredentials = true;
+
 export const API_STUDENT_FINDALL_ALUMNI = async () => {
-    const response = await axios.get(`${HOST}/student/alumni`)
+    const response = await axios.get(`${HOST}/student/alumni`,
+        { withCredentials: true })
     return response.data
 }
 
 export const API_STUDENT_FINDONE_ALUMNI = async ({ idNumber }: IAPIAlumni) => {
-    const response = await axios.get(`${HOST}/student/findone/${idNumber}`)
+    const response = await axios.get(`${HOST}/student/findone/${idNumber}`,
+        { withCredentials: true })
     return response.data
 }
 
 export const API_STUDENT_SEND_TRACER = async ({ academicYear, program }: { academicYear: string, program: string }) => {
     const response = await axios.post(`${HOST}/student/send-tracer-to-alumni`, {
         academicYear, program
-    })
+    },
+        { withCredentials: true })
     return response.data
 }
 
 export const API_STUDENT_SEND_TRACER_TO_ONE = async ({ email }: { email: string }) => {
     const response = await axios.post(`${HOST}/mail/one-alumni`, {
         email
-    })
+    },
+        { withCredentials: true })
     return response.data
 }
