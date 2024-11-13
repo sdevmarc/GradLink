46f5235b92a4eaf602c621318d9e3ad35b71f56d
sdevmarc <suanieeee@yahoo.com>
2024-11-14 04:33:00 +0800

    feat: refactor user service methods and improve error handling

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -33,35 +33,35 @@ export class UsersService {
         }
     }
 
-    async findOne({ email }: IUsers)
-        : Promise<IPromiseUser> {
-        try {
-            const response = await this.UserModel.findOne({ email })
-            return { success: true, message: 'User retrieved successfully.', data: response }
-        } catch (error) {
-            throw new HttpException({ success: false, message: 'User email failed to retrieved.' }, HttpStatus.INTERNAL_SERVER_ERROR)
-        }
-    }
+    // async findOne({ email }: IUsers)
+    //     : Promise<IPromiseUser> {
+    //     try {
+    //         const response = await this.UserModel.findOne({ email })
+    //         return { success: true, message: 'User retrieved successfully.', data: response }
+    //     } catch (error) {
+    //         throw new HttpException({ success: false, message: 'User email failed to retrieved.' }, HttpStatus.INTERNAL_SERVER_ERROR)
+    //     }
+    // }
 
-    async InsertFirstUser({ email, password }: IUsers)
-        : Promise<IPromiseUser> {
-        try {
-            const isemail = await this.UserModel.findOne({ email })
-            if (isemail) return { success: false, message: 'User email already exists.' }
+    // async InsertFirstUser({ email, password }: IUsers)
+    //     : Promise<IPromiseUser> {
+    //     try {
+    //         const isemail = await this.UserModel.findOne({ email })
+    //         if (isemail) return { success: false, message: 'User email already exists.' }
 
-            let role: string
-            const hasusers = await this.UserModel.find()
-            hasusers.length <= 0 ? role = 'admin' : role = 'user'
+    //         let role: string
+    //         const hasusers = await this.UserModel.find()
+    //         hasusers.length <= 0 ? role = 'admin' : role = 'user'
 
-            const salt = await bcrypt.genSalt();
-            const hashedpassword = await bcrypt.hash(password, salt);
+    //         const salt = await bcrypt.genSalt();
+    //         const hashedpassword = await bcrypt.hash(password, salt);
 
-            await this.UserModel.create({ email, password: hashedpassword, role })
-            return { success: true, message: 'User successfully created.' }
-        } catch (error) {
-            throw new HttpException({ success: false, message: 'User failed to create.' }, HttpStatus.INTERNAL_SERVER_ERROR)
-        }
-    }
+    //         await this.UserModel.create({ email, password: hashedpassword, role })
+    //         return { success: true, message: 'User successfully created.' }
+    //     } catch (error) {
+    //         throw new HttpException({ success: false, message: 'User failed to create.' }, HttpStatus.INTERNAL_SERVER_ERROR)
+    //     }
+    // }
 
     async InsertUser({ email, password, role }: IUsers)
         : Promise<IPromiseUser> {
@@ -70,7 +70,7 @@ export class UsersService {
             if (isemail) return { success: false, message: 'User email already exists.' }
 
             const salt = await bcrypt.genSalt();
-            const hashedpassword = await bcrypt.hash(password, salt);
+            const hashedpassword = await bcrypt.hash('admin', salt);
 
             await this.UserModel.create({ email, password: hashedpassword, role })
             return { success: true, message: 'User successfully created.' }
@@ -82,18 +82,28 @@ export class UsersService {
     async ReadLoginUser({ email, password }: IUsers)
         : Promise<IPromiseUser> {
         try {
+            const hasusers = await this.UserModel.find()
+
+            if (hasusers.length === 0) {
+                const salt = await bcrypt.genSalt();
+                const hashedpassword = await bcrypt.hash(password, salt);
+                await this.UserModel.create({ role: 'root', password: hashedpassword, isactive: true })
+            }
+
             const isemail = await this.UserModel.findOne({ email })
 
             if (!isemail) return { success: false, message: 'User email does not exist.' }
+
             const ispassword = await bcrypt.compare(password, isemail.password);
 
             if (!ispassword) return { success: false, message: 'Password is incorrect.' }
+
             const payload = { sub: isemail._id }
             const access_token = await this.jwtService.signAsync(payload)
 
             return { success: true, message: 'Logged in successful.', access_token }
         } catch (error) {
-            throw new HttpException({ success: false, message: 'User failed to login.' }, HttpStatus.INTERNAL_SERVER_ERROR)
+            throw new HttpException({ success: false, message: 'User failed to login.', error }, HttpStatus.INTERNAL_SERVER_ERROR)
         }
     }
 }
