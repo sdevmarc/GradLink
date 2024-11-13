e777340045bc01db08cd6791d2d218139b8f331f
sdevmarc <suanieeee@yahoo.com>
2024-11-14 04:30:51 +0800

    feat: enable withCredentials for employment analytics API requests

--- client/src/api/analytics/index.ts
+++ client/src/api/analytics/index.ts
@@ -1,9 +1,11 @@
 import { HOST } from '@/constants'
 import axios from 'axios'
 
+axios.defaults.withCredentials = true;
+
 export const API_ANALYTICS_EMPLOYMENT = async ({ department, program, academicYear }: { department?: string, program?: string, academicYear?: string }) => {
     const response = await axios.post(`${HOST}/student/employment-analytics`, {
         department, program, academicYear
-    })
+    }, { withCredentials: true })
     return response.data
 }
