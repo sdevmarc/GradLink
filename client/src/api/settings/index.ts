bdd13eb00e25cbebb1a72bfa1bcb9e81a469dc9e
sdevmarc <suanieeee@yahoo.com>
2024-11-14 04:31:22 +0800

    feat: enable withCredentials for settings and audit log API requests

--- client/src/api/settings/index.ts
+++ client/src/api/settings/index.ts
@@ -1,17 +1,13 @@
 import { HOST } from '@/constants'
 import axios from 'axios'
 
-interface IAuditLog {
-    id: number;
-    timestamp: string;
-    user: string;
-    action: string;
-    details: string;
-}
+axios.defaults.withCredentials = true;
 
 export const API_FINDONE_SETTINGS = async () => {
     try {
-        const response = await axios.get(`${HOST}/settings`)
+        const response = await axios.get(`${HOST}/settings`,
+            { withCredentials: true }
+        )
         return response.data
     } catch (error) {
         console.error(error)
@@ -22,7 +18,7 @@ export const API_UPDATE_SETTINGS = async ({ isenroll }: IAPISettings) => {
     try {
         const response = await axios.post(`${HOST}/settings/update-settings`, {
             isenroll
-        })
+        }, { withCredentials: true })
         return response.data
     } catch (error) {
         console.error(error)
@@ -31,10 +27,23 @@ export const API_UPDATE_SETTINGS = async ({ isenroll }: IAPISettings) => {
 
 export const API_GET_AUDIT_LOGS = async () => {
     try {
-        const response = await axios.get(`${HOST}/auditlog`)
+        const response = await axios.get(`${HOST}/auditlog`,
+            { withCredentials: true }
+        )
         return response.data
     } catch (error) {
         console.error(error)
-        return { data: [] }
     }
 }
+
+export const checkAuthStatus = async () => {
+    try {
+        const response = await axios.get(`http://${HOST}/auth/status`,
+            { withCredentials: true }
+        );
+        return response.data;
+    } catch (error) {
+        console.error(error)
+        return { isAuthenticated: false };
+    }
+};
