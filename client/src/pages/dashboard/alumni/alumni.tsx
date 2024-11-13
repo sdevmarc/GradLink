f018db15f15344f0f2169673f9939ceb36cbeb8b
Marc Edison <suanieeee@yahoo.com>
2024-11-14 02:08:51 +0800

    refactor: clean up Alumni component structure and improve loading state handling

--- client/src/pages/dashboard/alumni/alumni.tsx
+++ client/src/pages/dashboard/alumni/alumni.tsx
@@ -8,7 +8,6 @@ import MainTable from "@/components/main-table"
 import { ROUTES } from "@/constants"
 import { API_FORM_MAPPED } from "@/api/form"
 import { useEffect } from "react"
-import Loading from "@/components/loading"
 
 export default function Alumni() {
     const { data: dataAlumni, isLoading: alumniLoading, isFetched: alumniFetched } = useQuery({
@@ -16,51 +15,46 @@ export default function Alumni() {
         queryKey: ['students']
     })
 
-    const { data: form, isLoading: formLoading, isFetched: formFetched } = useQuery({
+    const { data: formdata, isLoading: formLoading, isFetched: formFetched } = useQuery({
         queryFn: () => API_FORM_MAPPED(),
         queryKey: ['form']
     })
 
     useEffect(() => {
-        if (formFetched) {
-            console.log(form?.message)
-        }
-    }, [form])
+        if (formFetched) { console.log(formdata?.message) }
+    }, [formdata, formLoading])
 
     const isLoading = formLoading || alumniLoading
 
     return (
-        <>
-            {isLoading && <Loading />}
-            <div className="flex flex-col min-h-screen items-center">
-                <div className="w-full max-w-[90rem] flex flex-col">
-                    <aside className="px-4 pb-4 pt-[8rem]">
-                        <HeadSection>
-                            <SubHeadSectionDetails
-                                title="Alumni Information"
-                                description="View and manage alumni."
+        <div className="flex flex-col min-h-screen items-center">
+            <div className="w-full max-w-[90rem] flex flex-col">
+                <aside className="px-4 pb-4 pt-[8rem]">
+                    <HeadSection>
+                        <SubHeadSectionDetails
+                            title="Alumni Information"
+                            description="View and manage alumni."
+                        />
+                    </HeadSection>
+                </aside>
+                <main className="flex">
+                    <Sidebar>
+                        <SidebarNavs bg='bg-muted' title="Alumni Information" link={ROUTES.ALUMNI} />
+                        <SidebarNavs title="Tracer Map" link={ROUTES.TRACER_MAP} />
+                    </Sidebar>
+                    <MainTable>
+                        {alumniLoading && <div>Loading...</div>}
+                        {
+                            (!alumniLoading && alumniFetched) &&
+                            <DataTableStudentAlumni
+                                isSync={isLoading}
+                                columns={StudentAlumniColumns}
+                                data={dataAlumni?.data || []}
                             />
-                        </HeadSection>
-                    </aside>
-                    <main className="flex">
-                        <Sidebar>
-                            <SidebarNavs bg='bg-muted' title="Alumni Information" link={ROUTES.ALUMNI} />
-                            <SidebarNavs title="Tracer Map" link={ROUTES.TRACER_MAP} />
-                            {/* <SidebarNavs title="Google Form" link={ROUTES.GOOGLE_FORM} /> */}
-                        </Sidebar>
-                        <MainTable>
-                            {alumniLoading && <div>Loading...</div>}
-                            {
-                                (!alumniLoading && alumniFetched) &&
-                                <DataTableStudentAlumni
-                                    columns={StudentAlumniColumns}
-                                    data={dataAlumni?.data || []}
-                                />
-                            }
-                        </MainTable>
-                    </main>
-                </div>
+                        }
+                    </MainTable>
+                </main>
             </div>
-        </>
+        </div>
     )
 }
