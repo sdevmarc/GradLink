73589a34ef93fc0cd08f828000959d857b82c478
sdevmarc <suanieeee@yahoo.com>
2024-11-19 15:25:53 +0800

    feat: add pie chart component to visualize alumni job landing times

---
+++ client/src/components/charts/pie-chart-land-job.tsx
@@ -0,0 +1,127 @@
+"use client"
+
+import * as React from "react"
+import { TrendingUp } from "lucide-react"
+import { Label, Pie, PieChart } from "recharts"
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "@/components/ui/chart"
+const chartData = [
+    { browser: "Less than a month", visitors: 3, fill: "var(--color-chrome)" },
+    { browser: "1 to 6 months", visitors: 4, fill: "var(--color-safari)" },
+    { browser: "7 to 11 months", visitors: 7, fill: "var(--color-firefox)" },
+    { browser: "1 year to less than 2 years", visitors: 2, fill: "var(--color-chrome)" },
+    { browser: "2 year to less than 2 years", visitors: 7, fill: "var(--color-safari)" },
+    { browser: "3 year to less than 2 years", visitors: 12, fill: "var(--color-firefox)" },
+    { browser: "More than 4 years", visitors: 9, fill: "var(--color-edge)" },
+]
+
+const chartConfig = {
+    visitors: {
+        label: "Visitors",
+    },
+    chrome: {
+        label: "Chrome",
+        color: "hsl(var(--chart-1))",
+    },
+    safari: {
+        label: "Safari",
+        color: "hsl(var(--chart-2))",
+    },
+    firefox: {
+        label: "Firefox",
+        color: "hsl(var(--chart-3))",
+    },
+    edge: {
+        label: "Edge",
+        color: "hsl(var(--chart-4))",
+    },
+    other: {
+        label: "Other",
+        color: "hsl(var(--chart-5))",
+    },
+} satisfies ChartConfig
+
+export function PieChartLandJob() {
+    const totalVisitors = React.useMemo(() => {
+        return chartData.reduce((acc, curr) => acc + curr.visitors, 0)
+    }, [])
+
+    return (
+        <Card className="flex flex-col shadow-none border-none">
+            <CardHeader className="items-center pb-0">
+                <CardTitle>
+                    Time of Alumni landing their job.
+                </CardTitle>
+                <CardDescription>2024 - 2025</CardDescription>
+            </CardHeader>
+            <CardContent className="flex-1 pb-0">
+                <ChartContainer
+                    config={chartConfig}
+                    className="mx-auto aspect-square max-h-[250px]"
+                >
+                    <PieChart>
+                        <ChartTooltip
+                            cursor={false}
+                            content={<ChartTooltipContent hideLabel />}
+                        />
+                        <Pie
+                            data={chartData}
+                            dataKey="visitors"
+                            nameKey="browser"
+                            innerRadius={60}
+                            strokeWidth={5}
+                        >
+                            <Label
+                                content={({ viewBox }) => {
+                                    if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                                        return (
+                                            <text
+                                                x={viewBox.cx}
+                                                y={viewBox.cy}
+                                                textAnchor="middle"
+                                                dominantBaseline="middle"
+                                            >
+                                                <tspan
+                                                    x={viewBox.cx}
+                                                    y={viewBox.cy}
+                                                    className="fill-foreground text-3xl font-bold"
+                                                >
+                                                    {totalVisitors.toLocaleString()}
+                                                </tspan>
+                                                <tspan
+                                                    x={viewBox.cx}
+                                                    y={(viewBox.cy || 0) + 24}
+                                                    className="fill-muted-foreground"
+                                                >
+                                                    Respondents
+                                                </tspan>
+                                            </text>
+                                        )
+                                    }
+                                }}
+                            />
+                        </Pie>
+                    </PieChart>
+                </ChartContainer>
+            </CardContent>
+            <CardFooter className="flex-col gap-2 text-sm">
+                <div className="leading-none text-muted-foreground text-center">
+                    Showing analytics from alumni responses in landing their job.
+                </div>
+            </CardFooter>
+        </Card>
+    )
+}
