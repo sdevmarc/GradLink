0d0c2eaa64397111053755c7dbeb3b10d0ab4ff8
sdevmarc <suanieeee@yahoo.com>
2024-11-24 23:15:50 +0800

    feat: extend action field in AuditSchema to include specific action types

--- server/src/auditlog/auditlog.schema.ts
+++ server/src/auditlog/auditlog.schema.ts
@@ -8,6 +8,7 @@ export const AuditSchema = new mongoose.Schema({
     },
     action: {
         type: String,
+        enum: ['user_login', 'settings_changed', 'user_created', 'password_changed', 'program_changed', 'course_changed', 'curriculum_changed', 'semester_changed', 'student_changed'],
         required: true,
     },
     description: {
