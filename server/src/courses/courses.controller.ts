c25c7efa86194f3b973d2da3230fd640737898af
sdevmarc <suanieeee@yahoo.com>
2024-11-24 23:16:00 +0800

    feat: enhance createCourse method to include JWT verification and audit logging

--- server/src/courses/courses.controller.ts
+++ server/src/courses/courses.controller.ts
@@ -1,16 +1,19 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Param, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Get, HttpException, HttpStatus, Param, Post, Req, UseGuards } from '@nestjs/common'
 import { CoursesService } from './courses.service'
 import { ICourses } from './courses.interface'
 import { privateDecrypt } from 'crypto'
 import { AuditlogService } from 'src/auditlog/auditlog.service'
 import { AuthGuard } from 'src/auth/auth.guard'
+import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('courses')
 @UseGuards(AuthGuard)
 export class CoursesController {
     constructor(
         private readonly courseService: CoursesService,
-        private readonly auditlogService: AuditlogService
+        private readonly auditlogService: AuditlogService,
+        private jwtService: JwtService
     ) { }
 
     @Get()
@@ -29,20 +32,26 @@ export class CoursesController {
     }
 
     @Post('create')
-    async createCourse(@Body() { userId, code, courseno, descriptiveTitle, units }: ICourses) {
-       
-        try {
-            return await this.courseService.create({ code, courseno, descriptiveTitle, units })
-            // const issuccess = await this.courseService.create({ code, courseno, descriptiveTitle, units })
+    async createCourse(
+        @Req() request: Request,
+        @Body() { code, courseno, descriptiveTitle, units }: ICourses
+    ) {
+        const token = request.cookies['access_token'];
+        if (!token) return { isAuthenticated: false }
 
-            // if(issuccess.success){
-            //     await this.auditlogService.createLog({ userId, action: "create", description: `Course created: ${descriptiveTitle}` })
-            //     return { success: true, message: "course successfully created." }
-            // }
-            // await this.auditlogService.createLog({ userId, action: "create", description: 'Error' })
-            // return { success: false, message: issuccess.message }
-        } catch (error) 
-        {
+        try {
+            const payload = await this.jwtService.verify(token);
+            const userId = await payload.sub
+
+            const issuccess = await this.courseService.create({ code, courseno, descriptiveTitle, units })
+
+            if (issuccess.success) {
+                await this.auditlogService.createLog({ userId, action: "course_changed", description: `Course created: ${descriptiveTitle}` })
+                return { success: true, message: "course successfully created." }
+            }
+
+            return { success: false, message: issuccess.message }
+        } catch (error) {
             throw new HttpException(
                 { success: false, message: 'Failed to fetch audit logs.', error },
                 HttpStatus.INTERNAL_SERVER_ERROR
