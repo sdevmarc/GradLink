f24786313a1209337271c5f77f1820a843490c03
sdevmarc <suanieeee@yahoo.com>
2024-11-24 23:15:44 +0800

    feat: update IAuditlog interface to include specific action types and add IPromiseAudit interface for promise handling

--- server/src/auditlog/auditlog.interface.ts
+++ server/src/auditlog/auditlog.interface.ts
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 export interface IAuditlog {
     userId: mongoose.Schema.Types.ObjectId
-    action: string;
+    action: 'user_login' | 'settings_changed' | 'user_created' | 'password_changed' | 'program_changed' | 'course_changed' | 'curriculum_changed' | 'semester_changed' | 'student_changed'
     description: string;
+}
+
+export interface IPromiseAudit {
+    success: boolean
+    message: string
 }
