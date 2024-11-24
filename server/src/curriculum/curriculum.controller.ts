a4bbe090c451d42a57f65f0e82ce25c851dda6ca
sdevmarc <suanieeee@yahoo.com>
2024-11-24 23:16:05 +0800

    feat: implement JWT verification and audit logging in insertCurriculum method

--- server/src/curriculum/curriculum.controller.ts
+++ server/src/curriculum/curriculum.controller.ts
@@ -1,15 +1,18 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
 import { CurriculumService } from './curriculum.service';
 import { ICurriculum } from './curriculum.interface';
 import { AuditlogService } from 'src/auditlog/auditlog.service'
 import { AuthGuard } from 'src/auth/auth.guard';
+import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('curriculum')
 @UseGuards(AuthGuard)
 export class CurriculumController {
     constructor(
         private readonly CurriculumService: CurriculumService,
-        private readonly auditlogService: AuditlogService
+        private readonly auditlogService: AuditlogService,
+        private jwtService: JwtService
     ) { }
 
     @Get()
@@ -23,16 +26,26 @@ export class CurriculumController {
     }
 
     @Post('create')
-    async insertCurriculum(@Body() { userId, name, programid, major, categories }: ICurriculum) {
+    async insertCurriculum(
+        @Req() request: Request,
+        @Body() { name, programid, major, categories }: ICurriculum) {
+
+        const token = request.cookies['access_token'];
+        if (!token) return { isAuthenticated: false }
+
         try {
-            return await this.CurriculumService.insertNew({ name, programid, major, categories })
-            // const issuccess = await this.CurriculumService.insertNew({ name, programid, major, categories })
-            // if(issuccess.success){
-            //     await this.auditlogService.createLog({ userId, action: "create", description: `${name} created` })
-            //     return { success: true, message: "Curriculum successfully created." }
-            // }
-            // await this.auditlogService.createLog({ userId, action: "create", description: 'Error creating curriculum' })
-            // return { success: false, message: issuccess.message }
+            const payload = await this.jwtService.verify(token);
+            const userId = await payload.sub
+
+            // return await this.CurriculumService.insertNew({ name, programid, major, categories })
+            const issuccess = await this.CurriculumService.insertNew({ name, programid, major, categories })
+
+            if (issuccess.success) {
+                await this.auditlogService.createLog({ userId, action: "curriculum_changed", description: `Curriculum ${name} created successfully.` })
+                return { success: true, message: "Curriculum successfully created." }
+            }
+
+            return { success: false, message: issuccess.message }
         } catch (error) {
             throw new HttpException(
                 { success: false, message: 'Failed to fetch audit logs.', error },
