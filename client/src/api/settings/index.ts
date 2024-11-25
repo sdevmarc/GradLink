5a6584677ed8160e5a76bffb05dea0990384eba1
Marc Edison <suanieeee@yahoo.com>
2024-11-26 03:37:29 +0800

    feat: add OTP sending and verification functions to settings API

--- client/src/api/settings/index.ts
+++ client/src/api/settings/index.ts
@@ -38,12 +38,35 @@ export const API_GET_AUDIT_LOGS = async () => {
 
 export const checkAuthStatus = async () => {
     try {
-        const response = await axios.get(`http://${HOST}/auth/status`,
+        const response = await axios.get(`${HOST}/auth/status`,
             { withCredentials: true }
         );
         return response.data;
     } catch (error) {
         console.error(error)
         return { isAuthenticated: false };
+    }
+};
+
+export const API_SETTINGS_SEND_OTP = async ({ email }: { email: string }) => {
+    try {
+        const response = await axios.post(`${HOST}/users/otp`, {
+            email
+        }, { withCredentials: true })
+        return response.data
+    } catch (error) {
+        console.error(error)
+    }
+};
+
+export const VerifyOtp = async ({ otp }: { otp: number }) => {
+    try {
+        const response = await axios.post(`${HOST}/users/verify-otp`, {
+            otp
+        }, { withCredentials: true });
+
+        return response.data;
+    } catch (error) {
+        console.error(error)
     }
 };
