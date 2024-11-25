24c9354f59b8dfcfd6a671710b200ecc4e262fb5
Marc Edison <suanieeee@yahoo.com>
2024-11-26 03:38:28 +0800

    feat: add MailService and ConstantsService to UsersModule for enhanced functionality

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -3,14 +3,18 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersSchema } from './users.schema';
+import { MailService } from 'src/mail/mail.service';
+import { MailSchema } from 'src/mail/mail.schema';
+import { ConstantsService } from 'src/constants/constants.service';
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             { name: 'User', schema: UsersSchema },
+            { name: 'Mail', schema: MailSchema },
         ]),
     ],
-    providers: [UsersService],
+    providers: [UsersService, MailService, ConstantsService],
     controllers: [UsersController]
 })
 export class UsersModule { }
