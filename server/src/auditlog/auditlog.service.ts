785956e773d2227b86aff00e980fe1aac6a83c9c
sdevmarc <suanieeee@yahoo.com>
2024-11-27 03:13:49 +0800

    fix: implement findAll method in AuditlogService for retrieving audit logs

--- server/src/auditlog/auditlog.service.ts
+++ server/src/auditlog/auditlog.service.ts
@@ -9,6 +9,39 @@ export class AuditlogService {
         @InjectModel('Auditlog') private readonly AuditlogModel: Model<IAuditlog> //Check mo audit log interface, dun ka maglagay ng types niya. Pagbasehan mo yung sa schema, make your own schema kapag.
     ) { }
 
+    async findAll(): Promise<IPromiseAudit> {
+        try {
+            const response = await this.AuditlogModel.aggregate([
+                {
+                    $lookup: {
+                        from: 'users',
+                        localField: 'userId',
+                        foreignField: '_id',
+                        as: 'user'
+                    }
+                },
+                {
+                    $unwind: '$user'
+                },
+                {
+                    $project: {
+                        _id: 1,
+                        name: '$user.name',
+                        action: 1,
+                        description: 1,
+                        updatedAt: 1
+                    }
+                }
+            ])
+            return { success: true, message: 'Audit retrieved successfully.', data: response }
+        } catch (error) {
+            throw new HttpException(
+                { success: false, message: 'Failed to find all audit log.', error },
+                HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
     async createLog({ userId, action, description }: IAuditlog): Promise<IPromiseAudit> {
         try {
             await this.AuditlogModel.create({ userId, action, description });
@@ -16,17 +49,6 @@ export class AuditlogService {
         } catch (error) {
             throw new HttpException(
                 { success: false, message: 'Failed to create audit log.', error },
-                HttpStatus.INTERNAL_SERVER_ERROR
-            );
-        }
-    }
-
-    async getAllLogs(): Promise<IAuditlog[]> {
-        try {
-            return await this.AuditlogModel.find().populate('userId', 'email').sort({ createdAt: -1 }).exec();
-        } catch (error) {
-            throw new HttpException(
-                { success: false, message: 'Failed to fetch audit logs.', error },
                 HttpStatus.INTERNAL_SERVER_ERROR
             );
         }
