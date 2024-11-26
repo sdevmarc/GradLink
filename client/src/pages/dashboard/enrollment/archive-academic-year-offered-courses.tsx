c41a9ae78ff3bae1ae27af621f6707182b5c6a48
sdevmarc <suanieeee@yahoo.com>
2024-11-27 01:10:18 +0800

    fix: correct indentation in ArchivedAcademicYearOfferedCourses component for improved readability

--- client/src/pages/dashboard/enrollment/archive-academic-year-offered-courses.tsx
+++ client/src/pages/dashboard/enrollment/archive-academic-year-offered-courses.tsx
@@ -15,7 +15,7 @@ export default function ArchivedAcademicYearOfferedCourses() {
 
     return (
         <div className="flex flex-col min-h-screen items-center">
- <div className="w-full max-w-[90rem] flex flex-col pb-[20rem]">
+            <div className="w-full max-w-[90rem] flex flex-col pb-[20rem]">
                 <aside className="px-4 pb-4 pt-[8rem]">
                     <HeadSection>
                         <BackHeadSection />
