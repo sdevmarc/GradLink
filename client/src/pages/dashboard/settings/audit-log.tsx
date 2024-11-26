15b599a63810c585adb561b131ad921bc0db42f5
Marc Edison <suanieeee@yahoo.com>
2024-11-27 04:57:32 +0800

    refactor: remove unused state and logic in AuditLog component

--- client/src/pages/dashboard/settings/audit-log.tsx
+++ client/src/pages/dashboard/settings/audit-log.tsx
@@ -1,72 +1,17 @@
 import { API_GET_AUDIT_LOGS } from "@/api/settings"
 import HeadSection, { SubHeadSectionDetails } from "@/components/head-section"
 import MainTable from "@/components/main-table"
-import { Badge } from "@/components/ui/badge"
 import { Card, CardContent } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
-import { IAPIAuditlog } from "@/interface/settings.interface"
 import { useQuery } from "@tanstack/react-query"
-import { Search } from "lucide-react"
-import { useEffect, useState } from "react"
 import { DataTableAudit } from "./settings-data-table-components/data-table-list-of-audit"
 import { AuditColumns } from "./settings-data-table-components/columns-student-list-of-audit"
 
 export default function AuditLog() {
-    const [auditlogs, setAuditlogs] = useState<IAPIAuditlog[]>([])
 
     const { data: auditlog, isLoading: auditlogLoading, isFetched: auditlogFetched } = useQuery({
         queryFn: () => API_GET_AUDIT_LOGS(),
         queryKey: ['audit']
     })
-
-    const ACTION_LABELS = {
-        user_login: 'User Login',
-        settings_changed: 'Settings Change',
-        user_created: 'User Change',
-        password_changed: 'Password Change',
-        program_changed: 'Program Change',
-        course_changed: 'Course Change',
-        curriculum_changed: 'Curriculum Change',
-        semester_changed: 'Semester Change',
-        student_changed: 'Student Change'
-    } as const
-
-    useEffect(() => {
-        if (auditlogFetched) {
-            const filterlogs = auditlog?.data?.map((item: IAPIAuditlog) => {
-                const { _id, action, description, updatedAt, name } = item;
-
-                return {
-                    id: _id,
-                    action: ACTION_LABELS[action as keyof typeof ACTION_LABELS] || action, // Fallback to original action if not found
-                    details: description,
-                    timestamp: updatedAt,
-                    user: name
-                }
-            }) || [];
-
-            setAuditlogs(filterlogs);
-        }
-    }, [auditlog, auditlogFetched]);
-
-    // const [auditLogs, setAuditLogs] = useState([
-    //     { id: 1, timestamp: '2024-11-06T02:30:00Z', user: 'John Doe', action: 'User Login', details: 'Successful login from IP 192.168.1.1' },
-    //     { id: 2, timestamp: '2024-11-06T03:15:00Z', user: 'Jane Smith', action: 'Settings Changed', details: 'Updated email notifications preferences' },
-    //     { id: 3, timestamp: '2024-11-06T04:00:00Z', user: 'Admin', action: 'User Created', details: 'New user account created for Alice Johnson' },
-    //     { id: 4, timestamp: '2024-11-06T05:30:00Z', user: 'John Doe', action: 'Password Changed', details: 'User changed their account password' },
-    //     { id: 5, timestamp: '2024-11-06T06:45:00Z', user: 'System', action: 'Backup Created', details: 'Automatic system backup completed' },
-    // ])
-    const [auditLogFilter, setAuditLogFilter] = useState('')
-    const [auditLogActionFilter, setAuditLogActionFilter] = useState('')
-
-    const filteredAuditLogs = auditlogs.filter(log =>
-        ((log.user ?? '').toLowerCase().includes(auditLogFilter.toLowerCase()) ||
-            (log.action ?? '').toLowerCase().includes(auditLogFilter.toLowerCase()) ||
-            (log.details ?? '').toLowerCase().includes(auditLogFilter.toLowerCase())) &&
-        (auditLogActionFilter === '' || log.action === auditLogActionFilter)
-    )
 
     return (
         <div className="flex flex-col min-h-screen items-center">
