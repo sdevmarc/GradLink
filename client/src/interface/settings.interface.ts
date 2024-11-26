3e7b05064b27e5ff2925b35bc41a2fa9390fe00b
Marc Edison <suanieeee@yahoo.com>
2024-11-27 04:15:43 +0800

    feat: add IAPIAuditlog interface for audit log structure definition

--- client/src/interface/settings.interface.ts
+++ client/src/interface/settings.interface.ts
@@ -1,3 +1,15 @@
 interface IAPISettings {
     isenroll?: boolean
 }
+
+export interface IAPIAuditlog {
+    _id?: string;
+    action?: string;
+    description?: string;
+    updatedAt?: string;
+    name?: string;
+    id?: string
+    user?: string
+    details?: string
+    timestamp?: string
+}
