f83c68751e69ac187ee5268c95ccee4ab0ecae08
Marc Edison <suanieeee@yahoo.com>
2024-12-02 01:46:44 +0800

    feat: add authentication check and redirect to login in ListOfStudents component

--- client/src/pages/dashboard/student/list-of-students.tsx
+++ client/src/pages/dashboard/student/list-of-students.tsx
@@ -3,8 +3,21 @@ import { useQuery } from "@tanstack/react-query"
 import { API_STUDENT_FINDALL } from "@/api/student"
 import { StudentListOfStudentsColumns } from "./student-data-table-components/list-of-students/columns-student-list-of-students"
 import { DataTableStudentListOfStudent } from "./student-data-table-components/list-of-students/data-table-list-of-students"
+import { AuthContext } from "@/hooks/AuthContext"
+import { useContext, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 
 export default function ListOfStudents() {
+    const { isAuthenticated } = useContext(AuthContext);
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (!isAuthenticated) {
+            // Redirect to login page if not authenticated
+            navigate("/", { replace: true });
+        }
+    }, [isAuthenticated, navigate])
+
     const { data: students, isLoading: studentLoading, isFetched: studentFetched } = useQuery({
         queryFn: () => API_STUDENT_FINDALL(),
         queryKey: ['students']
