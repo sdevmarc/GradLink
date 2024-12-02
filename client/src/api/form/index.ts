2fae5a7298cc122d89dae61c13672007fbe9a46f
Marc Edison <suanieeee@yahoo.com>
2024-12-03 01:44:57 +0800

    feat: add API function to retrieve Google Form link

--- client/src/api/form/index.ts
+++ client/src/api/form/index.ts
@@ -3,6 +3,13 @@ import axios from 'axios'
 
 axios.defaults.withCredentials = true;
 
+export const API_FORM_GET_GOOGLE_FORM_LINK = async () => {
+    const response = await axios.get(`${HOST}/forms/google-link`,
+        { withCredentials: true }
+    )
+    return response.data
+}
+
 export const API_FORM_FINDALL_UNKNOWN = async () => {
     const response = await axios.get(`${HOST}/forms/unknown-respondents`,
         { withCredentials: true }
