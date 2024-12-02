ff31ef74727bd587ab5cd7acba4d1cb647e40c8b
sdevmarc <suanieeee@yahoo.com>
2024-12-02 21:08:53 +0800

    feat: add API_AUTHENTICATION_STATUS function to check user authentication status

--- client/src/api/settings/index.ts
+++ client/src/api/settings/index.ts
@@ -4,6 +4,17 @@ import axios from 'axios'
 
 axios.defaults.withCredentials = true;
 
+export const API_AUTHENTICATION_STATUS = async () => {
+    try {
+        const response = await axios.get(`${HOST}/status`,
+            { withCredentials: true }
+        )
+        return response.data.isAuthenticated
+    } catch (error) {
+        console.error(error)
+    }
+}
+
 export const API_FINDONE_SETTINGS = async () => {
     try {
         const response = await axios.get(`${HOST}/settings`,
