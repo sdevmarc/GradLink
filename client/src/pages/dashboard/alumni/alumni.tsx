63257ea6dd154c3bcfd14d51ef0c433dec311065
Marc Edison <suanieeee@yahoo.com>
2024-12-03 03:10:13 +0800

    feat: add password check functionality in Alumni component and update sidebar link title

--- client/src/pages/dashboard/alumni/alumni.tsx
+++ client/src/pages/dashboard/alumni/alumni.tsx
@@ -10,6 +10,7 @@ import { API_FORM_MAPPED } from "@/api/form"
 import { useContext, useEffect } from "react"
 import { AuthContext } from "@/hooks/AuthContext"
 import { useNavigate } from "react-router-dom"
+import { API_USER_CHECK_DEFAULT_PASSWORD } from "@/api/user"
 
 export default function Alumni() {
     const { isAuthenticated } = useContext(AuthContext);
@@ -21,6 +22,19 @@ export default function Alumni() {
             navigate("/", { replace: true });
         }
     }, [isAuthenticated, navigate])
+
+    const { data: checkpassword, isLoading: checkpasswordLoading, isFetched: checkpasswordFetched } = useQuery({
+        queryFn: () => API_USER_CHECK_DEFAULT_PASSWORD(),
+        queryKey: ['check-password']
+    })
+
+    useEffect(() => {
+        if (checkpasswordFetched) {
+            if (!checkpassword.success) {
+               navigate('/overview')
+            } 
+        }
+    }, [checkpassword])
 
     const { data: dataAlumni, isLoading: alumniLoading, isFetched: alumniFetched } = useQuery({
         queryFn: () => API_STUDENT_FINDALL_ALUMNI(),
@@ -36,7 +50,7 @@ export default function Alumni() {
         if (formFetched) { console.log(formdata?.message) }
     }, [formdata, formLoading])
 
-    const isLoading = formLoading || alumniLoading
+    const isLoading = formLoading || alumniLoading || checkpasswordLoading
 
     return (
         <div className="flex flex-col min-h-screen items-center">
@@ -53,7 +67,7 @@ export default function Alumni() {
                     <Sidebar>
                         <SidebarNavs bg='bg-muted' title="Alumni Information" link={ROUTES.ALUMNI} />
                         <SidebarNavs title="Tracer Map" link={ROUTES.TRACER_MAP} />
-                        <SidebarNavs title="Google Form" link={ROUTES.GOOGLE_FORM} />
+                        <SidebarNavs title="Tracer Respondents" link={ROUTES.GOOGLE_FORM} />
                     </Sidebar>
                     <MainTable>
                         {alumniLoading && <div>Loading...</div>}
