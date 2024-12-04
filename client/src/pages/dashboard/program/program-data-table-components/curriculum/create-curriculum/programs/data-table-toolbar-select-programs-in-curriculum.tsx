56345a0a8a0859fc7f4a1e6e6857f458d23f73f0
sdevmarc <suanieeee@yahoo.com>
2024-12-04 19:17:11 +0800

    feat: enhance program selection data table toolbar with global search and reset functionality

--- client/src/pages/dashboard/program/program-data-table-components/curriculum/create-curriculum/programs/data-table-toolbar-select-programs-in-curriculum.tsx
+++ client/src/pages/dashboard/program/program-data-table-components/curriculum/create-curriculum/programs/data-table-toolbar-select-programs-in-curriculum.tsx
@@ -13,16 +13,16 @@ interface DataTableToolbarProps<TData> {
 export function DataTableToolbarSelectProgramsInCurriculum<TData>({
     table,
 }: DataTableToolbarProps<TData>) {
-    const isFiltered = table.getState().columnFilters.length > 0
+    const isFiltered = table.getState().columnFilters.length > 0 || !!table.getState().globalFilter;
 
     return (
         <div className="flex flex-wrap items-center justify-between">
             <div className="flex flex-1 flex-wrap items-center gap-2">
                 <Input
-                    placeholder="Search program code..."
-                    value={(table.getColumn("code")?.getFilterValue() as string) ?? ""}
+                    placeholder="Search programs..."
+                    value={(table.getState().globalFilter as string) ?? ""}
                     onChange={(event) => {
-                        table.getColumn("code")?.setFilterValue(event.target.value)
+                        table.setGlobalFilter(event.target.value);
                     }}
                     className="h-8 w-[20rem] lg:w-[25rem]"
                 />
@@ -30,7 +30,10 @@ export function DataTableToolbarSelectProgramsInCurriculum<TData>({
                 {isFiltered && (
                     <Button
                         variant="ghost"
-                        onClick={() => table.resetColumnFilters()}
+                        onClick={() => {
+                            table.resetColumnFilters()
+                            table.setGlobalFilter('')
+                        }}
                         className="h-8 px-2 lg:px-3"
                     >
                         Reset
