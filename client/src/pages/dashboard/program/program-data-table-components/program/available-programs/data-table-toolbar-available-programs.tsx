1704b2199d51f01e75e7f9391b2088c244e5874a
sdevmarc <suanieeee@yahoo.com>
2024-12-04 19:17:31 +0800

    feat: enhance available programs data table toolbar with global search functionality

--- client/src/pages/dashboard/program/program-data-table-components/program/available-programs/data-table-toolbar-available-programs.tsx
+++ client/src/pages/dashboard/program/program-data-table-components/program/available-programs/data-table-toolbar-available-programs.tsx
@@ -18,13 +18,13 @@ interface DataTableToolbarProps<TData> {
 export function DataTableToolbarAvailablePrograms<TData>({
     table,
 }: DataTableToolbarProps<TData>) {
-    const isFiltered = table.getState().columnFilters.length > 0;
+    const isFiltered = table.getState().columnFilters.length > 0 || !!table.getState().globalFilter;
     const navigate = useNavigate()
     const department_options = [
         { value: 'SEAIT', label: "Eng'g, Dev't. Arts & Design, Library Science & IT" },
         { value: 'SHANS', label: "Science and Mathematics" },
         { value: 'SAB', label: "Business and Accountancy" },
-        { value: 'STEH', label: "Teacher Education and Humanities"}
+        { value: 'STEH', label: "Teacher Education and Humanities" }
     ]
 
     return (
@@ -32,9 +32,9 @@ export function DataTableToolbarAvailablePrograms<TData>({
             <div className="flex flex-1 flex-wrap items-center gap-2">
                 <Input
                     placeholder="Search program..."
-                    value={(table.getColumn("descriptiveTitle")?.getFilterValue() as string) ?? ""}
+                    value={(table.getState().globalFilter as string) ?? ""}
                     onChange={(event) => {
-                        table.getColumn("descriptiveTitle")?.setFilterValue(event.target.value);
+                        table.setGlobalFilter(event.target.value);
                     }}
                     className="h-8 w-[20rem] lg:w-[25rem]"
                 />
@@ -45,29 +45,19 @@ export function DataTableToolbarAvailablePrograms<TData>({
                         options={department_options}
                     />
                 )}
-                {/* {table.getColumn("type") && (
-                    <DataTableFacetedFilter
-                        column={table.getColumn("type")}
-                        title="Type"
-                        options={incomeType}
-                    />
-                )} */}
                 {isFiltered && (
                     <Button
                         variant="ghost"
-                        onClick={() => table.resetColumnFilters()}
+                        onClick={() => {
+                            table.resetColumnFilters()
+                            table.setGlobalFilter('')
+                        }}
                         className="h-8 px-2 lg:px-3"
                     >
                         Reset
                         <Cross2Icon className="ml-2 h-4 w-4" />
                     </Button>
                 )}
-                {/* <CalendarDatePicker
-                    date={dateRange}
-                    onDateSelect={handleDateSelect}
-                    className="w-[250px] h-8"
-                    variant="outline"
-                /> */}
             </div>
             <div className="flex gap-2 items-center">
                 <AlertDialogConfirmation
