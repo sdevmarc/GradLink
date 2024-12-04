5a103da8d39b800bc9cb8ced4aa44aef8dc940c8
sdevmarc <suanieeee@yahoo.com>
2024-12-04 19:15:20 +0800

    fix: update query key for password check to improve consistency

--- client/src/pages/dashboard/enrollment/attrition-rate-courses.tsx
+++ client/src/pages/dashboard/enrollment/attrition-rate-courses.tsx
@@ -24,7 +24,7 @@ export default function AttritionRateCourses() {
 
     const { data: checkpassword, isFetched: checkpasswordFetched } = useQuery({
         queryFn: () => API_USER_CHECK_DEFAULT_PASSWORD(),
-        queryKey: ['check-password']
+        queryKey: ['checkpassword']
     })
 
     useEffect(() => {
