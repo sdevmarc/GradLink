0042179e1f35dd122c0302ab145b1f519e6dfd20
sdevmarc <suanieeee@yahoo.com>
2024-12-04 19:14:54 +0800

    fix: update query key for password check to improve consistency

--- client/src/pages/dashboard/alumni/alumni.tsx
+++ client/src/pages/dashboard/alumni/alumni.tsx
@@ -25,7 +25,7 @@ export default function Alumni() {
 
     const { data: checkpassword, isLoading: checkpasswordLoading, isFetched: checkpasswordFetched } = useQuery({
         queryFn: () => API_USER_CHECK_DEFAULT_PASSWORD(),
-        queryKey: ['check-password']
+        queryKey: ['checkpassword']
     })
 
     useEffect(() => {
