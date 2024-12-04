e141b3a9c4333c19ef4c396cd708aee07307ecb0
sdevmarc <suanieeee@yahoo.com>
2024-12-04 19:15:12 +0800

    fix: update query key for password check to enhance clarity

--- client/src/pages/dashboard/enrollment/achived-semesters-in-academic-year.tsx
+++ client/src/pages/dashboard/enrollment/achived-semesters-in-academic-year.tsx
@@ -26,7 +26,7 @@ export default function ArchivedSemestersInAcademicYear() {
 
     const { data: checkpassword, isFetched: checkpasswordFetched } = useQuery({
         queryFn: () => API_USER_CHECK_DEFAULT_PASSWORD(),
-        queryKey: ['check-password']
+        queryKey: ['checkpassword']
     })
 
     useEffect(() => {
