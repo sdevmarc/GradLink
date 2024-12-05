983271fb1ebf00e2c09aa61d12d3d4cdac17504b
Marc Edison <suanieeee@yahoo.com>
2024-12-05 08:41:36 +0800

    feat: add IEvaluationForm interface to define structure for evaluation forms

--- server/src/forms/forms.interface.ts
+++ server/src/forms/forms.interface.ts
@@ -1,3 +1,24 @@
+export interface IEvaluationForm {
+    email: string
+    generalInformation: {
+        title: string
+        description: string
+        questions: FormGoogle[]
+    }
+    employmentData: {
+        title: string
+        description: string
+        questions: FormGoogle[]
+    }
+    isApproved: boolean
+    isActive: boolean
+}
+
+interface FormGoogle {
+    question: string
+    answer: string
+}
+
 export interface IModelForm {
     idNumber?: string
     notes?: string
