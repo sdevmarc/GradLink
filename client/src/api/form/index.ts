db7b2ec37da5d14c8481d63c4c28a618fc7a5cb3
Marc Edison <suanieeee@yahoo.com>
2024-12-05 08:40:57 +0800

    feat: add API endpoints for retrieving and evaluating tracer forms

--- client/src/api/form/index.ts
+++ client/src/api/form/index.ts
@@ -10,6 +10,13 @@ export const API_FORM_GET_GOOGLE_FORM_LINK = async () => {
     return response.data
 }
 
+export const API_FORM_FINDALL_TRACER = async () => {
+    const response = await axios.get(`${HOST}/forms`,
+        { withCredentials: true }
+    )
+    return response.data
+}
+
 export const API_FORM_FINDALL_UNKNOWN = async () => {
     const response = await axios.get(`${HOST}/forms/unknown-respondents`,
         { withCredentials: true }
@@ -19,6 +26,14 @@ export const API_FORM_FINDALL_UNKNOWN = async () => {
 
 export const API_FORM_MAPPED = async () => {
     const response = await axios.get(`${HOST}/forms/form-mapped`,
+        { withCredentials: true }
+    )
+    return response.data
+}
+
+export const API_FORM_EVALUATE_TRACER = async ({ id, isApproved }: {id: string, isApproved: boolean}) => {
+    const response = await axios.post(`${HOST}/forms/evaluate-form-tracer`,
+        { id, isApproved },
         { withCredentials: true }
     )
     return response.data
