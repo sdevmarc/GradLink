bd7779ccc5ef0e7cef97eaa7a85f1b927919ebb2
sdevmarc <suanieeee@yahoo.com>
2024-12-13 06:29:37 +0800

    feat: add Tailwind CSS integration and configure build settings in vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,28 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+  build: {
+    outDir: 'dist', // Default output directory
+    // sourcemap: true, // Ensure sourcemaps are generated
+    sourcemap: process.env.NODE_ENV !== 'production', // Disable sourcemaps in production
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // server: {
+  //   host: "192.168.1.26",  // Allows access from any IP on the local network
+  //   port: 3000,       // Customize the port if needed (default is 3000)
+  // },
 })
