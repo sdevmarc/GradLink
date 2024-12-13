104624059c65252117bf822f399f4d51022055f6
sdevmarc <suanieeee@yahoo.com>
2024-12-13 11:18:17 +0800

    refactor: remove unused date range state and filters from DataTableToolbar component

--- client/src/components/data-table-components/data-table-toolbar.tsx
+++ client/src/components/data-table-components/data-table-toolbar.tsx
@@ -5,10 +5,6 @@ import { Table } from "@tanstack/react-table";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { incomeType, categories } from "@/components/data-table-components/data";
-import { DataTableFacetedFilter } from "@/components/data-table-components/data-table-faceted-filter";
-import { CalendarDatePicker } from "@/components/calendar-date-picker";
-import { useState } from "react";
 import { DataTableViewOptions } from "@/components/data-table-components/data-table-view-options";
 
 interface DataTableToolbarProps<TData> {
@@ -19,17 +15,6 @@ export function DataTableToolbar<TData>({
     table
 }: DataTableToolbarProps<TData>) {
     const isFiltered = table.getState().columnFilters.length > 0;
-
-    const [dateRange, setDateRange] = useState<{ from: Date; to: Date }>({
-        from: new Date(new Date().getFullYear(), 0, 1),
-        to: new Date()
-    });
-
-    const handleDateSelect = ({ from, to }: { from: Date; to: Date }) => {
-        setDateRange({ from, to });
-        // Filter table data based on selected date ranges
-        table.getColumn("date")?.setFilterValue([from, to]);
-    };
 
     return (
         <div className="flex flex-wrap items-center justify-between">
@@ -42,20 +27,7 @@ export function DataTableToolbar<TData>({
                     }}
                     className="h-8 w-[150px] lg:w-[250px] placeholder:text-muted"
                 />
-                {table.getColumn("category") && (
-                    <DataTableFacetedFilter
-                        column={table.getColumn("category")}
-                        title="Category"
-                        options={categories}
-                    />
-                )}
-                {table.getColumn("type") && (
-                    <DataTableFacetedFilter
-                        column={table.getColumn("type")}
-                        title="Type"
-                        options={incomeType}
-                    />
-                )}
+             
                 {isFiltered && (
                     <Button
                         variant="ghost"
@@ -66,12 +38,6 @@ export function DataTableToolbar<TData>({
                         <Cross2Icon className="ml-2 h-4 w-4" />
                     </Button>
                 )}
-                <CalendarDatePicker
-                    date={dateRange}
-                    onDateSelect={handleDateSelect}
-                    className="w-[250px] h-8"
-                    variant="outline"
-                />
             </div>
 
             <DataTableViewOptions table={table} />
