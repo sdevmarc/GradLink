4d82f150be1aea134c2bd19533aed8f114585e83
sdevmarc <suanieeee@yahoo.com>
2024-12-13 16:41:47 +0800

    feat: add backup script for MongoDB database with automated cleanup

--- server/src/settings/settings.controller.ts
+++ server/src/settings/settings.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SettingsService } from './settings.service';
+import { exec } from 'child_process';
 
 @Controller('settings')
 export class SettingsController {
@@ -17,9 +18,18 @@ export class SettingsController {
         return await this.settingsService.upsertSettings({ isenroll })
     }
 
-    @Get('restore')
-    async restoreDatabase() {
-        return await this.settingsService.restoreDatabase()
+    @Post('restore')
+    async restoreDatabase(): Promise<{ success: boolean, message: string }> {
+        return new Promise((resolve, reject) => {
+            exec('/usr/local/bin/restore.sh', (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Error object: ${error}`);
+                    console.error(`Restore stderr: ${stderr}`);
+                    return reject({ success: false, message: 'Restore Failed', error });
+                }
+                console.log(stdout);
+                resolve({ success: true, message: 'Restore Successful.' });
+            });
+        });
     }
-
 }
