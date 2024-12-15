5fcee8c22f7900ea572222ac3233de591ec9286e
sdevmarc <suanieeee@yahoo.com>
2024-12-15 22:36:23 +0800

    feat: make CORS origin configurable via environment variable

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
+const HOST = process.env.HOST || '*'
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: HOST,
     credentials: true
   })
   await app.listen(3002);
