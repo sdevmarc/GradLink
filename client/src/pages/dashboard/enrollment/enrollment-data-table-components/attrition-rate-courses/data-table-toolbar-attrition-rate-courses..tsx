4c99a4b70744c2f3946cbcc907aeaacd49b9442a
Marc Edison <suanieeee@yahoo.com>
2024-12-16 05:12:50 +0800

    feat: implement global search functionality in attrition rate courses data table toolbar

--- client/src/pages/dashboard/enrollment/enrollment-data-table-components/attrition-rate-courses/data-table-toolbar-attrition-rate-courses..tsx
+++ client/src/pages/dashboard/enrollment/enrollment-data-table-components/attrition-rate-courses/data-table-toolbar-attrition-rate-courses..tsx
@@ -10,85 +10,29 @@ interface DataTableToolbarProps<TData> {
     table: Table<TData>
 }
 
-// interface ProgramOption {
-//     value: string;
-//     label: string;
-//     department: string;
-// }
-
 export function DataTableToolbarAttritionRateCourses<TData>({
     table,
 }: DataTableToolbarProps<TData>) {
-    const isFiltered = table.getState().columnFilters.length > 0
-    // const [formattedprogram, setFormattedProgram] = useState<ProgramOption[]>([]);
-    // const [filteredPrograms, setFilteredPrograms] = useState<ProgramOption[]>([]);
-
-    // const department_options = [
-    //     { value: 'SEAIT', label: "Eng'g, Dev't. Arts & Design, Library Science & IT" },
-    //     { value: 'SHANS', label: "Science and Mathematics" },
-    //     { value: 'SAB', label: "Business and Accountancy" },
-    //     { value: 'STEH', label: "Teacher Education and Humanities"}
-    // ]
-
-    // const { data: program, isLoading: programLoading, isFetched: programFetched } = useQuery({
-    //     queryFn: () => API_PROGRAM_FINDALL(),
-    //     queryKey: ['programs']
-    // })
-
-    // useEffect(() => {
-    //     if (!programLoading && programFetched) {
-    //         const formatprogram = program.data.map((item: IAPIPrograms) => {
-    //             const { _id, code, department } = item
-    //             return {
-    //                 value: _id, label: code, department: department // Make sure your API returns this
-    //             }
-    //         })
-
-    //         setFormattedProgram(formatprogram)
-    //         setFilteredPrograms(formatprogram)
-    //     }
-    // }, [program])
-
-    // useEffect(() => {
-    //     const selectedDepartment = table.getColumn("department")?.getFilterValue() as string[];
-
-    //     if (selectedDepartment && selectedDepartment.length > 0) {
-    //         const filtered = formattedprogram.filter((prog: any) =>
-    //             selectedDepartment.includes(prog.department)
-    //         );
-    //         setFilteredPrograms(filtered);
-
-    //         // Clear program filter if selected program is not in filtered list
-    //         const currentProgramFilter = table.getColumn("program")?.getFilterValue() as string[];
-    //         if (currentProgramFilter && currentProgramFilter.length > 0) {
-    //             const validPrograms = filtered.map(p => p.value);
-    //             const newProgramFilter = currentProgramFilter.filter(p =>
-    //                 validPrograms.includes(p)
-    //             );
-    //             if (newProgramFilter.length !== currentProgramFilter.length) {
-    //                 table.getColumn("program")?.setFilterValue(newProgramFilter);
-    //             }
-    //         }
-    //     } else {
-    //         setFilteredPrograms(formattedprogram);
-    //     }
-    // }, [table.getColumn("department")?.getFilterValue()]);
+    const isFiltered = table.getState().columnFilters.length > 0 || !!table.getState().globalFilter;
 
     return (
         <div className="flex flex-wrap items-center justify-between">
             <div className="flex flex-1 flex-wrap items-center gap-2">
                 <Input
                     placeholder="Search course..."
-                    value={(table.getColumn("courseno")?.getFilterValue() as string) ?? ""}
+                    value={(table.getState().globalFilter as string) ?? ""}
                     onChange={(event) => {
-                        table.getColumn("courseno")?.setFilterValue(event.target.value);
+                        table.setGlobalFilter(event.target.value);
                     }}
-                    className="h-8 w-[17rem] lg:w-[20rem]"
+                    className="h-8 w-[20rem] lg:w-[25rem]"
                 />
                 {isFiltered && (
                     <Button
                         variant="ghost"
-                        onClick={() => table.resetColumnFilters()}
+                        onClick={() => {
+                            table.resetColumnFilters()
+                            table.setGlobalFilter('')
+                        }}
                         className="h-8 px-2 lg:px-3"
                         size={`sm`}
                     >
@@ -96,30 +40,6 @@ export function DataTableToolbarAttritionRateCourses<TData>({
                         <Cross2Icon className="ml-2 h-4 w-4" />
                     </Button>
                 )}
-                {/* {table.getColumn("department") && (
-                    <DataTableFacetedFilter
-                        column={table.getColumn("department")}
-                        title="Department"
-                        options={department_options}
-                    />
-                )}
-                {(table.getColumn("program") && programFetched) && (
-                    <DataTableFacetedFilter
-                        column={table.getColumn("program")}
-                        title="Program"
-                        options={filteredPrograms}
-                    />
-                )} */}
-            </div>
-            <div className="flex gap-2 items-center">
-                {/* <AlertDialogConfirmation
-                    type={`default`}
-                    variant={'outline'}
-                    btnTitle="Export"
-                    title="Are you sure?"
-                    description={`This will export the current data you are viewing.`}
-                    btnContinue={() => navigate('/program')}
-                /> */}
             </div>
         </div>
     )
