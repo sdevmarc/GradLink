9509f58957d7dc6203a353e315febfe5ee796ec4
Marc Edison <suanieeee@yahoo.com>
2024-12-16 04:45:01 +0800

    feat: set default HOST to http://localhost:5173 in main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
-const HOST = process.env.HOST || '*'
+const HOST = process.env.HOST || 'http://localhost:5173'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
